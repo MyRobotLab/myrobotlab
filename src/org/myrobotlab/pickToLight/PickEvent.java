24d3cec21cdce0413834275dd0541e62252b6505
GroG <grog@myrobotlab.org>
2014-05-05 07:28:54 -0700

    pick event added

---
+++ src/org/myrobotlab/pickToLight/PickEvent.java
@@ -0,0 +1,15 @@
+package org.myrobotlab.pickToLight;
+
+public class PickEvent {
+	
+	public String ipAddress;
+	public String macAddress;
+	public int i2CAddress;
+	
+	public PickEvent(Controller controller, Module m){
+		ipAddress = controller.getIpAddress();
+		macAddress = controller.getMacAddress();
+		i2CAddress = m.getI2CAddress();
+	}
+
+}
