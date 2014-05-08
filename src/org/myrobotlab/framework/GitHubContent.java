262ab4b25d6e85a0ee990060e5d87e75efa65c51
GroG <grog@myrobotlab.org>
2014-05-07 18:58:02 -0700

    GitHubcontent and other updates

---
+++ src/org/myrobotlab/framework/GitHubContent.java
@@ -0,0 +1,14 @@
+package org.myrobotlab.framework;
+
+public class GitHubContent {
+	
+	String name;
+	String path; 
+	String sha; 
+	Integer size; 
+	String url; 
+	String html_url;
+	String git_url;
+	String type;
+
+}
