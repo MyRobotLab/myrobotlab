e5a1224f3eb4d82dcdc9cc6fe91bdc001db8bd87
GroG <grog@myrobotlab.org>
2014-06-20 15:20:29 -0700

    small fix to test build

--- build/resource/root/myrobotlab.bat
+++ build/resource/root/myrobotlab.bat
@@ -1,6 +1,6 @@
 REM Proxy configuration
 REM the following will allow mrl to update and check the repo if its behind a firewall
-REM -Dhttp.proxyHost=webproxy -Dhttp.proxyPort=8080 -Dhttp.proxyUserName="myusername" -Dhttp.proxyPassword="mypassword" -Dhttps.proxyHost=webproxy -Dhttps.proxyPort=8080 
+REM -Dhttp.proxyHost=webproxy -Dhttp.proxyPort=80 -Dhttps.proxyHost=webproxy -Dhttps.proxyPort=80 -Dhttp.proxyUserName="myusername" -Dhttp.proxyPassword="mypassword"  
 
 REM start javaw starts java without another shell window on windows
 REM to display system out messages enable logging or run simply as java ..<parameters>..
