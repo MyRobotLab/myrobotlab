94622314223c73f4cc5032fe8d322d47a1fb5ff3
U-neptune\kwatters <kwatters@kmwllc.com>
2014-08-14 09:14:37 -0400

    Adding ProgramAB service and a GUI for it.  AIML 2.0 reference impl more info at https://code.google.com/p/program-ab/

---
+++ src/org/myrobotlab/control/ProgramABGUI.java
@@ -0,0 +1,117 @@
+package org.myrobotlab.control;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.myrobotlab.logging.LoggerFactory;
+import org.myrobotlab.service.GUIService;
+import org.myrobotlab.service.OpenCV;
+import org.myrobotlab.service.ProgramD;
+import org.myrobotlab.service.Servo;
+import org.slf4j.Logger;
+
+/**
+ * A UI for controlling and interacting with ProgramD
+ * 
+ * @author kwatters
+ *
+ */
+public class ProgramABGUI extends ServiceGUI implements ActionListener {
+
+	public final static Logger log = LoggerFactory.getLogger(ProgramABGUI.class.toString());
+	static final long serialVersionUID = 1L;
+	public final String boundServiceName;
+	
+	// TODO: make this auto-resize when added to gui..
+	private JTextField text = new JTextField("Say Hello:", 60);
+	private JTextArea response = new JTextArea("Program AB Response:");
+	private JButton askButton = new JButton("Ask Program AB");
+	private JScrollPane scrollResponse = new JScrollPane(response);
+	
+	private JTextField progABPath = new JTextField("C:\\tools\\ProgramAB", 16);
+	private JTextField botName = new JTextField("alice2", 16);
+	
+	private JButton startSessionButton = new JButton("Start Session");
+	
+	public ProgramABGUI(String boundServiceName, GUIService myService,JTabbedPane tabs) {
+		super(boundServiceName, myService, tabs);
+		this.boundServiceName = boundServiceName;
+	}
+	
+	@Override
+	public void init() {
+		//
+		scrollResponse.setAutoscrolls(true);
+		display.setLayout(new BorderLayout());
+		
+		JPanel inputControl = new JPanel();
+		
+		inputControl.add(text);
+		inputControl.add(askButton);
+		
+		display.add(inputControl, BorderLayout.PAGE_START);
+		
+		display.add(scrollResponse, BorderLayout.CENTER);
+		
+		JPanel botControl = new JPanel();
+		
+		botControl.add(progABPath);
+		botControl.add(botName);
+		botControl.add(startSessionButton);
+		
+		display.add(botControl, BorderLayout.PAGE_END);
+		
+		text.addActionListener(this);
+		askButton.addActionListener(this);
+		
+		startSessionButton.addActionListener(this);
+		
+		
+	}
+
+	@Override
+	public void attachGUI() {
+		// 
+		subscribe("publishState", "getState", ProgramD.class);
+		myService.send(boundServiceName, "publishState");
+	}
+
+	@Override
+	public void detachGUI() {
+		// 
+		unsubscribe("publishState", "getState", ProgramD.class);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		Object o = event.getSource();
+		if (o == askButton || o == text) {
+			//myService.send(boundServiceName, "getResponse", new String(text.getText()), "1", "SampleBot");
+			String answer=(String) myService.sendBlocking(boundServiceName, "getResponse", text.getText());
+			// response.setText(response.getText() + "<br/>\n\r" + answer);
+			if (answer != null) {
+				response.append("\n" + answer.trim());
+			} else {
+				response.append("\nERROR: NULL Response");
+			}
+			// clear out the original question.
+			text.setText("");			
+		} else if (o == startSessionButton) {
+			myService.send(boundServiceName, "startSession", progABPath.getText().trim(), botName.getText().trim());
+		} else {
+			log.info(o.toString());
+			log.info("Unknown action!");
+		}
+		// TODO Auto-generated method stub
+	}
+	
+}
---
+++ src/org/myrobotlab/service/ProgramAB.java
@@ -0,0 +1,91 @@
+package org.myrobotlab.service;
+
+import org.alicebot.ab.*;
+import org.myrobotlab.framework.Service;
+import org.myrobotlab.logging.LoggingFactory;
+
+/**
+ * Program AB service for MyRobotLab
+ * Uses AIML 2.0 to create a ChatBot
+ * This is a reboot of the Old AIML spec to be more 21st century.
+ * 
+ * More Info at http://aitools.org/ProgramAB 
+ * 
+ * @author kwatters
+ *
+ */
+public class ProgramAB extends Service {
+
+	
+	private Bot bot=null;
+	private Chat chatSession=null;
+	
+	public ProgramAB(String reservedKey) {
+		super(reservedKey);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return "AIML 2.0 Reference interpreter based on Program AB";
+	}
+
+	@Override
+	public void stopService() {
+		// TODO Auto-generated method stub
+		super.stopService();
+	}
+
+	@Override
+	public void startService() {
+		// TODO Auto-generated method stub
+		super.startService();
+
+	}
+
+	/**
+	 * Load the AIML 2.0 Bot config and start a chat session.  This must be called after the service is created.
+	 * 
+	 * @param path - should be the full path to the ProgramAB root
+	 * @param botName - The name of the bot to load.
+	 */
+	public void startSession(String path, String botName) {
+		bot = new Bot(botName, path);
+		chatSession = new Chat(bot);				
+	}
+
+	/**
+	 * 
+	 * @param text - the query string to the bot brain
+	 * @param userId - the user that is sending the query
+	 * @param robotName - the name of the bot you which to get the response from
+	 * @return
+	 */
+	public String getResponse(String text) {
+		if (bot == null || chatSession == null) {
+			return "ERROR: Core not loaded, please load core before chatting.";
+		}
+		String res = chatSession.multisentenceRespond(text);
+		System.out.println(res);
+		return res;
+	}
+
+	public static void main(String s[]) {
+		LoggingFactory.getInstance().configure();
+		LoggingFactory.getInstance().setLevel("INFO");
+		Runtime.createAndStart("gui", "GUIService");
+		Runtime.createAndStart("python", "Python");
+		ProgramAB alice = (ProgramAB) Runtime.createAndStart("alice", "ProgramAB");
+		String progABPath = "C:/tools/ProgramAB";
+		String botName = "alice2";
+		alice.startSession(progABPath, botName); 
+		String response = alice.getResponse("How are you?");
+		System.out.println("Alice" + response);		
+	}
+
+}
