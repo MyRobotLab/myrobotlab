16dfe19651d7ab4e44db7d2b1f24d756fdc397eb
GroG <supertick@gmail.com>
2014-08-23 09:14:52 -0700

    create publish & addListener & modify gui to establish msg routes

--- src/org/myrobotlab/control/OculusRiftGUI.java
+++ src/org/myrobotlab/control/OculusRiftGUI.java
@@ -10,6 +10,7 @@ import org.myrobotlab.logging.LoggerFactory;
 import org.myrobotlab.service.GUIService;
 import org.myrobotlab.service.OculusRift;
 import org.myrobotlab.service.OpenCV;
+import org.myrobotlab.service.OculusRift.RiftFrame;
 import org.myrobotlab.service.interfaces.VideoGUISource;
 import org.slf4j.Logger;
 
@@ -58,23 +59,25 @@ public class OculusRiftGUI extends ServiceGUI implements VideoGUISource, ActionL
 		display.add(rightVideoPanel);
 		
 	}
+	
+	public void onRiftFrame(RiftFrame frame){
+		leftEye.displayFrame(frame.left);
+		rightEye.displayFrame(frame.right);
+	}
 
 	@Override
 	public void attachGUI() {
-		// 
+		// gui msg routes created
 		subscribe("publishState", "getState", OculusRift.class);
 		myService.send(boundServiceName, "publishState");
-		// TODO: anything else special here?
-		leftEye.attachGUI(); 
-		rightEye.attachGUI();
+		subscribe("publishRiftFrame","onRiftFrame", RiftFrame.class);
 	}
 
 	@Override
 	public void detachGUI() {
-		// 
-		unsubscribe("publishState", "getState", OpenCV.class);
-		leftEye.detachGUI();
-		rightEye.detachGUI();
+		// gui msg routes removed
+		unsubscribe("publishState", "getState", OculusRift.class);
+		unsubscribe("publishRiftFrame","onRiftFrame", RiftFrame.class);
 	}
 
 }
