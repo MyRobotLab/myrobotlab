d817229ddf8a290affd4ab646da84b678c1d826c
Alessandruino <alessandro.didonna@gmail.com>
2014-09-17 15:34:27 +0200

    Update Serial.mpu6050.py

--- src/resource/Python/examples/Serial.mpu6050.py
+++ src/resource/Python/examples/Serial.mpu6050.py
@@ -3,18 +3,19 @@ from __future__ import division
 arduino = Runtime.start("arduino","Arduino")
 i01 = Runtime.createAndStart("i01","InMoov")
 i01.startHead("COM5")
+i01.startLeftArm("COM5")
 arduino.addCustomMsgListener(python)
 arduino.connect("COM3")
+i01.leftArm.bicep.setMinMax(5,80)
 
-counter = 0
 servo = 90
+bicep = 5
 
 
-def onCustomMsg(ax ,ay ,az):
-  global counter
-  global servo
-  counter += 1
-  servo = (20 +(((ax - 20000)/(-20000 - 20000))*(160 - 20)))
-  if (counter == 25):
-      i01.head.neck.moveTo(int(servo))
-      counter = 0
+def onCustomMsg(ax ,ay2 ,az):
+  global head
+  global bicep
+  head = (20 +(((ax - 16000)/(-16000 - 16000))*(160 - 20)))
+  bicep = (85 +(((ay2 - 20)/(-250 - 20))*(5 - 85)))
+  i01.head.neck.moveTo(int(head))
+  i01.leftArm.bicep.moveTo(int(bicep))
