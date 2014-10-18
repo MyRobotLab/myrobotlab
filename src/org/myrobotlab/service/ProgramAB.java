0f015dd1aa1d025732bbbd46966fa85b4d9b789b
Kevin Watters <kwatters@kmwllc.com>
2014-10-18 19:30:51 -0400

    Adding OOB support to program ab.  OOB tags can now define a <mrl> tag that specifies the service and method to invoke in MRL.

--- src/org/myrobotlab/service/ProgramAB.java
+++ src/org/myrobotlab/service/ProgramAB.java
@@ -1,12 +1,23 @@
 package org.myrobotlab.service;
 
 import java.io.File;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
+import org.myrobotlab.framework.Message;
 import org.myrobotlab.framework.Service;
 import org.myrobotlab.logging.LoggingFactory;
+import org.myrobotlab.programab.OOBPayload;
+import org.myrobotlab.service.interfaces.ServiceInterface;
 import org.myrobotlab.service.interfaces.TextListener;
 
 /**
@@ -26,7 +37,8 @@ public class ProgramAB extends Service {
 	private String botName = "alice2";
 	//private Chat chatSession=null;
 	private HashMap<String, Chat> sessions = new HashMap<String, Chat>();
-	
+	private Pattern oobPattern = Pattern.compile("<oob>(.*?)</oob>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+
 	public ProgramAB(String reservedKey) {
 		super(reservedKey);
 	}
@@ -49,7 +61,7 @@ public class ProgramAB extends Service {
 		if (session == null){
 			session = "default";
 		}
-		
+
 		if (sessions.containsKey(session)){
 			warn("session %s already created", session);
 			return;
@@ -61,16 +73,16 @@ public class ProgramAB extends Service {
 		}
 		
 		sessions.put(session, new Chat(bot));
-		
+
 		if (!"default".equals(session)){
 			getResponse(session, String.format("my name is %s", session));
 		}
 	}
-	
+
 	public static class Response {
 		public String session;
 		public String msg;
-		
+
 		public Response(String session, String msg){
 			this.session = session;
 			this.msg = msg;
@@ -80,7 +92,7 @@ public class ProgramAB extends Service {
 	public Response getResponse(String text){
 		return getResponse(null, text);
 	}
-	
+
 	/**
 	 * 
 	 * @param text - the query string to the bot brain
@@ -97,25 +109,65 @@ public class ProgramAB extends Service {
 			error(error);
 			return new Response(session, error);
 		}
-		
+
 		if (!sessions.containsKey(session)){
 			startSession(path, session, botName);
 		}
-		
+
 		String res = sessions.get(session).multisentenceRespond(text);
+		// Check the AIML response to see if there is OOB (out of band data) 
+		// If so, publish that data independent of the text response.
+		processOOB(res);
 		Response response = new Response(session, res);
 		invoke("publishResponse", response);
 		invoke("publishResponseText", response);
 		info("to: %s - %s", session, res);
 		return response;
 	}
-	
+
+	private void processOOB(String text) {		
+		Matcher oobMatcher = oobPattern.matcher(text);
+		while (oobMatcher.find()) {
+			// We found some OOB text.
+			// assume only one OOB in the text?
+			String oobPayload = oobMatcher.group();
+			OOBPayload payload = parseOOB(oobPayload);
+			// TODO: maybe we dont' want this? 
+			// Notifiy endpoints
+			invoke("publishOOBText", oobPayload);
+			// grab service and invoke method.
+			ServiceInterface s = Runtime.getService(payload.getServiceName());
+			if (payload.getParams()!=null) {
+				s.invoke(payload.getMethodName(), payload.getParams().toArray());
+			} else {
+				s.invoke(payload.getMethodName());				
+			}
+		}		
+	}
+
+	private OOBPayload parseOOB(String oobPayload) {
+		JAXBContext jaxbContext;
+		try {
+			jaxbContext = JAXBContext.newInstance(OOBPayload.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Reader r = new StringReader(oobPayload);
+			OOBPayload oobMsg = (OOBPayload) jaxbUnmarshaller.unmarshal(r);
+			return oobMsg;
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		log.info("OOB tag found, but it's not an MRL tag. {}", oobPayload );
+		return null;
+	}
+
 
 	public void startSession(String progABPath, String botName) {
 		startSession(progABPath	, null, botName);
 	}
 
-	
+
 	/**
 	 * publishing method of the pub sub pair - with addResponseListener allowing subscriptions
 	 * pub/sub routines have the following pattern
@@ -130,7 +182,7 @@ public class ProgramAB extends Service {
 	public Response publishResponse(Response response){
 		return response;
 	}
-	
+
 	/**
 	 * Test only publishing point - for simple consumers
 	 * @param response
@@ -140,14 +192,29 @@ public class ProgramAB extends Service {
 		return response.msg;
 	}
 
+	/**
+	 * If a response comes back that has an OOB Message, publish that separately
+	 * 
+	 * @param response
+	 * @return
+	 */
+	public String publishOOBText(String oobText){
+		return oobText;
+	}
+
+
 	public void addResponseListener(Service service){
 		addListener("publishResponse", service.getName(), "onResponse", Response.class);
 	}
-	
+
 	public void addTextListener(TextListener service){
 		addListener("publishResponseText", service.getName(), "onText", String.class);
 	}
-	
+
+	public void addOOBTextListener(TextListener service){
+		addListener("publishOOBText", service.getName(), "onOOBText", String.class);
+	}
+
 	public void startSession() {
 		startSession(null);
 	}
@@ -156,25 +223,22 @@ public class ProgramAB extends Service {
 		startSession(path, session, botName);
 	}
 
-	
 	public static void main(String s[]) {
 		LoggingFactory.getInstance().configure();
 		LoggingFactory.getInstance().setLevel("INFO");
-		
 		Runtime.createAndStart("gui", "GUIService");
 		Runtime.createAndStart("python", "Python");
 		ProgramAB alice = (ProgramAB) Runtime.createAndStart("alice", "ProgramAB");
 		// File f = new File("ProgramAB");
 		// String progABPath = f.getAbsolutePath();
 		// String botName = "alice2";
-		
 		alice.startSession(); 
 		Response response = alice.getResponse("How are you?");
-		
 		alice.startSession("GroG"); 
 		response = alice.getResponse("GroG", "How are you?");
 		log.info("Alice " + response.msg);		
+		response = alice.getResponse("GroG", "dial 1234566");
+		log.info("Alice " + response.msg);		
 	}
-
-
+	
 }
