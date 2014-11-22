0a40bd835851c6e7b42438d4a73e601a54abe97c
LunDev <vogt-marvin@web.de>
2014-11-22 22:16:47 +0100

    ServoOrchestrator - clean up

--- src/org/myrobotlab/service/ServoOrchestrator.java
+++ src/org/myrobotlab/service/ServoOrchestrator.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
