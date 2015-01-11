fff2c8c49a36e8a166f04dabb80eaeed9ad5519b
Kevin Watters <kwatters@kmwllc.com>
2015-01-11 11:04:19 -0500

    removing old http ant task dependency from the build.

--- build/resource/root/scripts/update.bat
+++
@@ -1,7 +0,0 @@
-rem Proxy configuration
-rem the following will allow mrl to update and check the repo if its behind a firewall
-rem -Dhttp.proxyHost=webproxy -Dhttp.proxyPort=8080 -Dhttp.proxyUserName="myusername" -Dhttp.proxyPassword="mypassword" -Dhttps.proxyHost=webproxy -Dhttps.proxyPort=8080 
-
-rem start javaw starts java without another shell window on windows
-rem to display system out messages enable logging or run simply as java ..<parameters>..
-java -Djava.library.path="libraries/native/x86.32.windows;libraries/native/x86.64.windows"  -cp "libraries/jar/*" org.myrobotlab.service.Runtime -update -logLevel INFO -logToConsole
