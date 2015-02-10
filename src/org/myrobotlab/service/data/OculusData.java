7ea9b7b5b6f2ba3abc17f3b911e58be3ba30dcdb
Alessandruino <alessandro.didonna@gmail.com>
2015-02-10 02:21:56 +0100

    OculusDIY package

---
+++ src/org/myrobotlab/service/data/OculusData.java
@@ -0,0 +1,14 @@
+package org.myrobotlab.service.data;
+
+/**
+ * @author GroG
+ * 
+ *         
+ * 
+ */
+public class OculusData {
+
+	public Integer roll;
+	public Integer pitch;
+	public Integer yaw;
+}
