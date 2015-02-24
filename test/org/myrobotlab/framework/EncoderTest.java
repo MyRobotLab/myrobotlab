a95a8bd877b0b1f2834ae4562352db48268c2cae
kwatters <kwatters@kmwllc.com>
2015-02-23 21:33:59 -0500

    fixing the encoder test to create a test service and decode a url for that service into a message

--- test/org/myrobotlab/framework/EncoderTest.java
+++ test/org/myrobotlab/framework/EncoderTest.java
@@ -1,5 +1,6 @@
 package org.myrobotlab.framework;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.net.URI;
@@ -12,9 +13,14 @@ public class EncoderTest {
 	@Test
 	public void testEncoderDecodeURI() {
 		Encoder e = new Encoder();
+		
 		try {
+			// create and start a service named foo, decode a url for that service.
+			org.myrobotlab.service.Test testService = new org.myrobotlab.service.Test("foo");
+			testService.startService();
 			Message m = e.decodeURI(new URI("http://www.myrobotlab.org/api/foo/bar/a/b/d?baz=bam&bap=bop"));
 			assertNotNull(m);
+			assertEquals("foo",m.getName());			
 		} catch (URISyntaxException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
