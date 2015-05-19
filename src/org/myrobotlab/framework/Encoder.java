283dcf3e722dc42ca6847405ff58938d1840591b
GroG <supertick@gmail.com>
2015-05-19 08:54:51 -0700

    testing API branch again

--- src/org/myrobotlab/framework/Encoder.java
+++ src/org/myrobotlab/framework/Encoder.java
@@ -39,7 +39,7 @@ import com.google.gson.GsonBuilder;
  * [<scheme>:]<scheme-specific-part>[#<fragment>]
  * http://stackoverflow.com/questions/3641722/valid-characters-for-uri-schemes
  * 
- * branch API test 3
+ * branch API test 4
  */
 public class Encoder {
 
