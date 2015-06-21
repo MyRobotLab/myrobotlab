d04eea44dcc25b1ebc9172e8134f71d6970dafb0
LunDev <vogt-marvin@web.de>
2015-06-21 23:05:37 +0200

    WebUI - restructuring -> move all directly service related files to an seperate folder -> split some files

---
+++ src/resource/WebGUI/main/reverseFilter.js
@@ -0,0 +1,7 @@
+angular.module('mrlapp.main.reversefilter', [])
+
+        .filter('reverse', function () {
+            return function (items) {
+                return items.slice().reverse();
+            };
+        });
