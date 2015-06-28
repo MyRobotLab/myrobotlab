60a24d28266fbb172a1d37cda41a6df157d11ee5
GroG <supertick@gmail.com>
2015-06-27 19:30:23 -0700

    small clock updates

--- src/resource/WebGUI/service/js/clockgui.js
+++ src/resource/WebGUI/service/js/clockgui.js
@@ -1,52 +1,55 @@
 angular.module('mrlapp.service.clockgui', [])
-        .controller('ClockGuiCtrl', ['$scope', 'mrl', function ($scope, mrl) {
-                console.log('ClockGuiCtrl');
+.controller('ClockGuiCtrl', ['$scope', 'mrl', function($scope, mrl) {
+        console.log('ClockGuiCtrl');
+        $scope.service = mrl.getService($scope.service.name);
+        
+        $scope.interval = $scope.service.interval;
+        if ($scope.service.isClockRunning == true){
+                $scope.label = "Stop";
+        } else {
                 $scope.label = "Start";
-                $scope.interval = 1000;
-                $scope.gui.setPanelCount(1);
-                // load data bindings for this type
-                $scope.service.pulseData = '';
-                $scope.service.onMsg = function (msg) {
-                    console.log("Clock Msg ! - ");
-                    if (msg.method == "onPulse") {
-                        var pulseData = msg.data[0];
-                        $scope.service.pulseData = pulseData;
-                        console.log('pulseData', $scope.service.pulseData);
-                        $scope.$apply();
-                    };
-                    if (msg.method == "onClockStarted") {
-                    	// track the clock state
-                    	$scope.service.isClockRunning = true;
-                    	$scope.$apply();
-                    };
+        }
 
-                    if (msg.method == "onClockStopped") {
-                    	// track the clock state
-                    	$scope.service.isClockRunning = false;
-                    	$scope.$apply();
-                    };
+        $scope.gui.setPanelCount(1);
+        // load data bindings for this type
+        $scope.pulseData = '';
 
-                };
-                $scope.toggle = function() {
-                	// just to make sure we have the right scope?
-                	$scope.service = mrl.getService($scope.service.name);
-                	if ($scope.service.isClockRunning == false) {
-                		// start the clock and update the label
-                		mrl.sendTo($scope.service.name, "startClock");
-                		$scope.label = "Stop";
-                	} else {
-                		// stop the clock and update the label
-                		mrl.sendTo($scope.service.name, "stopClock");
-                		$scope.label = "Start";
-                	};
-                };
-                
-                $scope.changeInterval = function() {
-                	mrl.sendTo($scope.service.name, "setInterval", $scope.interval);
-                }
-                
-                mrl.subscribe($scope.service.name, 'pulse');
-                mrl.subscribe($scope.service.name, 'clockStarted');
-                mrl.subscribe($scope.service.name, 'clockStopped');
-                $scope.gui.initDone();
-            }]);
+        $scope.service.onMsg = function(msg) {
+           
+            switch (msg.method) {
+                case 'onPulse':
+                    $scope.pulseData = msg.data[0];
+                    $scope.$apply();
+                    break;
+                case 'onClockStarted':
+                    $scope.label = "Stop";
+                    $scope.$apply();
+                    break;
+                case 'onClockStopped':
+                    $scope.label = "Start";
+                    $scope.$apply();
+                    break;
+                default:
+                    console.log("ERROR - unhandled method " + msg.method);
+                    break;
+            }
+        };
+        
+        $scope.toggle = function() {
+            if ($scope.label == "Start") {
+                mrl.sendTo($scope.service.name, "startClock");
+            } else {
+                mrl.sendTo($scope.service.name, "stopClock");
+            }
+        };
+        
+        $scope.changeInterval = function() {
+            mrl.sendTo($scope.service.name, "setInterval", $scope.interval);
+        }
+        
+        mrl.subscribe($scope.service.name, 'pulse');
+        mrl.subscribe($scope.service.name, 'clockStarted');
+        mrl.subscribe($scope.service.name, 'clockStopped');
+        
+        $scope.gui.initDone();
+    }]);
