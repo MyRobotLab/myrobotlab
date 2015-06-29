1c4e1cc7ac5f596f5da4e7a9629f350a9f44d9fd
LunDev <vogt-marvin@web.de>
2015-06-29 22:37:08 +0200

    WebUI - fix child-scope in service-templates

--- src/resource/WebGUI/service/serviceBodyDirective.js
+++ src/resource/WebGUI/service/serviceBodyDirective.js
@@ -8,10 +8,20 @@ angular.module('mrlapp.service')
                         panelindex: '='
                     },
                     link: function (scope, elem, attr) {
-                        //inject template & controller into the service-panel
-                        var html = '<div ng-controller=\"' + attr.simplename + 'GuiCtrl\"><div ng-include="\'service/views/' + attr.type + 'gui.html\'"></div></div>';
+                        //inject controller into the service-panel
+//                        var html = '<div ng-controller=\"' + attr.simplename + 'GuiCtrl\"><div ng-include="\'service/views/' + attr.type + 'gui.html\'"></div></div>';
+                        var html = '<div ng-controller=\"' + attr.simplename + 'GuiCtrl\"><div service-body-second type="' + attr.type + '"></div></div>';
                         elem.html(html).show();
                         $compile(elem.contents())(scope);
+                    }
+                };
+            }])
+
+        .directive('serviceBodySecond', [function () {
+                return {
+                    //inject template into the service-panel
+                    templateUrl: function (element, attr) {
+                        return 'service/views/' + attr.type + 'gui.html';
                     }
                 };
             }]);
