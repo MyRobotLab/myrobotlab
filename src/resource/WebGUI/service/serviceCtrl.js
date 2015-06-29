894737881b21b0e5c6bfe8353b5876b0442b5edf
LunDev <vogt-marvin@web.de>
2015-06-29 15:07:20 +0200

    WebUI: - service panels can be added - displaying error-message when service can't be loaded

--- src/resource/WebGUI/service/serviceCtrl.js
+++ src/resource/WebGUI/service/serviceCtrl.js
@@ -3,88 +3,76 @@ angular.module('mrlapp.service')
         .controller('ServiceCtrl', ['$scope', '$modal', '$ocLazyLoad', 'mrl', 'ServiceSvc',
             function ($scope, $modal, $ocLazyLoad, mrl, ServiceSvc) {
                 console.log('testing', $scope);
-                
-               $scope.anker = $scope.spawndata.name + '_-_' + $scope.spawndata.panelindex + '_-_';
 
-                var isUndefinedOrNull = function (val) {
-                    return angular.isUndefined(val) || val === null;
-                };
+                $scope.anker = $scope.spawndata.name + '_-_' + $scope.spawndata.panelindex + '_-_';
 
-//                //make sure $scope.spawndata is there
-//                var listener = $scope.$watch(function () {
-//                    return $scope.spawndata;
-//                }, function () {
-//                    if (!isUndefinedOrNull($scope.spawndata.name)) {
-//                        listener();
-//                        init();
-//                    }
-//                });
+                console.log('serviceShouldBeReady', $scope.spawndata);
 
-//                var init = function () {
-                    console.log('serviceShouldBeReady', $scope.spawndata);
+                //load the service(-module) (lazy) (from the server)
+                //TODO: should this really be done here?
+                console.log('lazy-loading:', $scope.spawndata.type);
+                $ocLazyLoad.load("service/js/" + $scope.spawndata.type + "gui.js").then(function () {
+                    $scope.serviceloaded = true;
+                }, function (e) {
+                    console.log('lazy-loading wasnt successful:', $scope.spawndata.type);
+                    $scope.servicenotfound = true;
+                });
 
-                    //load the service(-module) (lazy) (from the server)
-                    //TODO: should this really be done here?
-                    console.log('lazy-loading:', $scope.spawndata.type);
-                    $ocLazyLoad.load("service/js/" + $scope.spawndata.type + "gui.js").then(function () {
-                        $scope.serviceloaded = true;
-                    });
+                //START_specific Service-Initialisation
+                //"inst" is given to the specific service-UI
+                $scope.inst = ServiceSvc.getServiceInstance($scope.spawndata.name);
+                if ($scope.inst == null) {
+                    $scope.inst = {};
+                    $scope.inst.gui = {}; //framework-section - DO NOT WRITE IN THERE!
+                    $scope.inst.service = mrl.getService($scope.spawndata.name); //mrl-data-section
+                    ServiceSvc.addServiceInstance($scope.spawndata.name, $scope.inst);
+                }
+                $scope.gui = $scope.inst.gui;
+                $scope.service = $scope.inst.service;
 
-                    //START_specific Service-Initialisation
-                    //"inst" is given to the specific service-UI
-                    $scope.inst = ServiceSvc.getServiceInstance($scope.spawndata.name);
-                    if ($scope.inst == null) {
-                        $scope.inst = {};
-                        $scope.inst.gui = {}; //framework-section - DO NOT WRITE IN THERE!
-                        $scope.inst.service = mrl.getService($scope.spawndata.name); //mrl-data-section
-                        ServiceSvc.addServiceInstance($scope.spawndata.name, $scope.inst);
-                    }
-                    $scope.gui = $scope.inst.gui;
-                    $scope.service = $scope.inst.service;
-
-                    //TODO: refactor
-                    console.log("$scope,size", $scope.size);
-                    if ($scope.size != null && $scope.size.lastIndexOf("force", 0) == 0) {
-                        $scope.gui.oldsize = $scope.gui.size;
-                        $scope.gui.size = $scope.size.substring(5, $scope.size.length);
-                        $scope.gui.forcesize = true;
-                    } else {
-                        if ($scope.gui.oldsize != null) {
-                            $scope.gui.size = $scope.gui.oldsize;
-                            $scope.gui.oldsize = null;
-                        }
-                        $scope.gui.forcesize = false;
-                    }
-                    if (!$scope.gui.size) {
-                        $scope.gui.size = "medium";
+                //TODO: refactor
+                console.log("$scope,size", $scope.size);
+                if ($scope.size != null && $scope.size.lastIndexOf("force", 0) == 0) {
+                    $scope.gui.oldsize = $scope.gui.size;
+                    $scope.gui.size = $scope.size.substring(5, $scope.size.length);
+                    $scope.gui.forcesize = true;
+                } else {
+                    if ($scope.gui.oldsize != null) {
+                        $scope.gui.size = $scope.gui.oldsize;
                         $scope.gui.oldsize = null;
                     }
+                    $scope.gui.forcesize = false;
+                }
+                if (!$scope.gui.size) {
+                    $scope.gui.size = "medium";
+                    $scope.gui.oldsize = null;
+                }
 
-                    //TODO: think of something better
-                    var initDone = false;
-                    $scope.gui.initDone = function () {
-                        if (!initDone) {
-                            initDone = true;
-                            // create message bindings
-                            mrl.subscribeToService($scope.gui.onMsg, $scope.service.name);
-                        }
-                    };
-                    $scope.gui.panelcount = $scope.spawndata.panelcount;
-                    $scope.gui.setPanelCount = function (number) {
-                        console.log('setting panelcount', number);
-                        var old = $scope.gui.panelcount;
-                        $scope.gui.panelcount = number;
-                        ServiceSvc.notifyPanelCountChanged($scope.service.name, old, number);
-                    };
-
-                    //TODO: not completly happy
-                    //to be overriden
-                    if ($scope.service.onMsg == null) {
-                        $scope.service.onMsg = function () {
-                        };
+                //TODO: think of something better
+                var initDone = false;
+                $scope.gui.initDone = function () {
+                    if (!initDone) {
+                        initDone = true;
+                        // create message bindings
+                        mrl.subscribeToService($scope.gui.onMsg, $scope.service.name);
                     }
-                    //END_specific Service-Initialisation
-//                };
+                };
+                $scope.gui.panelcount = $scope.spawndata.panelcount;
+                $scope.gui.setPanelCount = function (number) {
+                    console.log('setting panelcount', number);
+                    var old = $scope.gui.panelcount;
+                    $scope.gui.panelcount = number;
+                    ServiceSvc.notifyPanelCountChanged($scope.service.name, old, number);
+                };
+
+                //TODO: not completly happy
+                //to be overriden
+                if ($scope.gui.onMsg == null) {
+                    $scope.gui.onMsg = function () {
+                        console.log('ERR got message to default service endpoint!');
+                    };
+                }
+                //END_specific Service-Initialisation
 
                 //footer-size-change-buttons
                 $scope.changesize = function (size) {
--- src/resource/WebGUI/service/serviceSvc.js
+++ src/resource/WebGUI/service/serviceSvc.js
@@ -1,118 +1,133 @@
 angular.module('mrlapp.service')
-.service('ServiceSvc', ['mrl', function(mrl) {
-        _self = this;
-        
-        var isUndefinedOrNull = function(val) {
-            return angular.isUndefined(val) || val === null;
-        };
-        
-        this.onMsg = function(msg) {
-            switch (msg.method) {
-                case 'onRegistered':
-                    var newService = msg.data[0];
-                    _self.addServiceInstance(newService.name, newService);
-                    break;
-                
-                case 'onReleased':
-                    var name = msg.data[0];
-                    removeServiceInstance(name);
-                    break;
-            }
-        }
+        .service('ServiceSvc', ['mrl', function (mrl) {
 
-        //START_Service Instances
-        var serviceInstances = [];
-        
-        
-        this.addServiceInstance = function(name, service) {
-            serviceInstances[name] = service;
-        };
-        
-        this.getServiceInstance = function(name) {
-            if (isUndefinedOrNull(serviceInstances[name])) {
-                return null;
-            }
-            return serviceInstances[name].servic;
-        };
-        
-        this.removeServiceInstance = function(name) {
-            var index = serviceInstances.indexOf(name);
-            if (index != -1) {
-                serviceInstances.splice(index, 1);
-            }
-        };
-        //END_Service Instances
+                var gateway = mrl.getGateway();
+                var runtime = mrl.getRuntime();
+                var platform = mrl.getPlatform();
+                var registry = mrl.getRegistry();
 
-        //START_Services
-        //TODO: find a way to get notified if a service is started / stopped
-        var gateway = mrl.getGateway();
-        var runtime = mrl.getRuntime();
-        var platform = mrl.getPlatform();
-        var registry = mrl.getRegistry();
-        
-        var guiData = {};
-        var services = {};
-
-        mrl.subscribeToService(_self.onMsg, runtime.name);
-        
-        for (var name in registry) {
-            if (registry.hasOwnProperty(name)) {
-                var temp = registry[name];
-                guiData[name] = {};
-                guiData[name].simpleName = temp.simpleName;
-                guiData[name].name = temp.name;
-                guiData[name].type = temp.simpleName.toLowerCase();
-                guiData[name].panelcount = 1;
-                
-                var serviceexp = guiData[name];
-                var service = {
-                    simpleName: serviceexp.simpleName,
-                    name: serviceexp.name,
-                    type: serviceexp.type,
-                    panelcount: serviceexp.panelcount,
-                    panelindex: 0
+                var isUndefinedOrNull = function (val) {
+                    return angular.isUndefined(val) || val === null;
                 };
-                services[service.name + '_-_' + service.panelindex + '_-_'] = service;
-            }
-        }
-        
-        this.getServices = function() {
-            return services;
-        };
-        
-        this.notifyPanelCountChanged = function(name, oldcount, count) {
-            console.log('notifyPanelCountChanged', name, oldcount, count);
-            guiData[name].panelcount = count;
-            var diff = count - oldcount;
-            
-            if (diff < 0) {
-                //                        service.name+'_-_'+service.panelindex+'_-_'
-                //                        var index = serviceInstances.indexOf(name);
-                //                        if (index != -1) {
-                //                            serviceInstances.splice(index, 1);
-                //                        }
-                for (var i = oldcount - 1; i > count - 1; i++) {
-                    var index = services.indexOf(name + '_-_' + i + '_-_');
+
+                //START_Service Instances
+                var serviceInstances = [];
+
+                this.addServiceInstance = function (name, service) {
+                    serviceInstances[name] = service;
+                };
+
+                this.getServiceInstance = function (name) {
+                    if (isUndefinedOrNull(serviceInstances[name])) {
+                        return null;
+                    }
+                    return serviceInstances[name];
+                };
+
+                this.removeServiceInstance = function (name) {
+                    var index = serviceInstances.indexOf(name);
                     if (index != -1) {
-                        services.splice(index, 1);
+                        serviceInstances.splice(index, 1);
+                    }
+                };
+                //END_Service Instances
+
+                //START_Services
+                var guiData = {};
+                var services = {};
+
+                this.addService = function (name, temp) {
+                    console.log('adding:', name, guiData);
+                    if (isUndefinedOrNull(guiData[name])) {
+                        console.log('adding#2:', name);
+                        guiData[name] = {};
+                        guiData[name].simpleName = temp.simpleName;
+                        guiData[name].name = temp.name;
+                        guiData[name].type = temp.simpleName.toLowerCase();
+                        guiData[name].panelcount = 1;
+
+                        var serviceexp = guiData[name];
+                        var service = {
+                            simpleName: serviceexp.simpleName,
+                            name: serviceexp.name,
+                            type: serviceexp.type,
+                            panelcount: serviceexp.panelcount,
+                            panelindex: 0
+                        };
+                        services[service.name + '_-_' + service.panelindex + '_-_'] = service;
+                    }
+                    console.log('adding#3:', name, guiData, services);
+                };
+
+                this.removeService = function (name) {
+                    var panelcount = guiData[name].panelcount;
+                    var index = guiData.indexOf(name);
+                    if (index != -1) {
+                        guiData.splice(index, 1);
+                    }
+                    for (var i = 0; i < panelcount; i++) {
+                        var index = services.indexOf(name + '_-_' + i + '_-_');
+                        if (index != -1) {
+                            services.splice(index, 1);
+                        }
+                    }
+                };
+
+                this.getServices = function () {
+                    return services;
+                };
+
+                this.notifyPanelCountChanged = function (name, oldcount, count) {
+                    console.log('notifyPanelCountChanged', name, oldcount, count);
+                    guiData[name].panelcount = count;
+                    var diff = count - oldcount;
+
+                    if (diff < 0) {
+                        for (var i = oldcount - 1; i > count - 1; i++) {
+                            var index = services.indexOf(name + '_-_' + i + '_-_');
+                            if (index != -1) {
+                                services.splice(index, 1);
+                            }
+                        }
+                        for (var i = count - 1; i >= 0; i++) {
+                            services[name + '_-_' + i + '_-_'].panelcount = count;
+                        }
+                    } else if (diff > 0) {
+                        var serviceexp = guiData[name];
+                        for (var i = oldcount; i < count; i++) {
+                            var service = {
+                                simpleName: serviceexp.simpleName,
+                                name: serviceexp.name,
+                                type: serviceexp.type,
+                                panelcount: serviceexp.panelcount,
+                                panelindex: i
+                            };
+                            services[service.name + '_-_' + service.panelindex + '_-_'] = service;
+                        }
+                    }
+                };
+                _self = this;
+                this.onMsg = function (msg) {
+                    switch (msg.method) {
+                        case 'onRegistered':
+                            var newService = msg.data[0];
+//                            this.addServiceInstance(newService.name, newService);
+                            _self.addService(newService.name, newService);
+                            break;
+
+                        case 'onReleased':
+                            var name = msg.data[0];
+                            this.removeServiceInstance(name);
+                            this.removeService(name);
+                            break;
+                    }
+                };
+                mrl.subscribeToService(this.onMsg, runtime.name);
+                
+                for (var name in registry) {
+                    if (registry.hasOwnProperty(name)) {
+                        this.addService(name, registry[name]);
                     }
                 }
-                for (var i = count - 1; i >= 0; i++) {
-                    services[name + '_-_' + i + '_-_'].panelcount = count;
-                }
-            } else if (diff > 0) {
-                var serviceexp = guiData[name];
-                for (var i = oldcount; i < count; i++) {
-                    var service = {
-                        simpleName: serviceexp.simpleName,
-                        name: serviceexp.name,
-                        type: serviceexp.type,
-                        panelcount: serviceexp.panelcount,
-                        panelindex: i
-                    };
-                    services[service.name + '_-_' + service.panelindex + '_-_'] = service;
-                }
-            }
-        };
-    //END_Services
-    }]);
+                //END_Services
+            }]);
