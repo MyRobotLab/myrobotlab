dcccbec0ef8311812cebb5967b9e318de230a3fd
LunDev <vogt-marvin@web.de>
2015-07-27 20:32:47 +0200

    WebUI - an alternative service-panel-system - featuring Angular 1.4 - updated AngularUI-directives - the connection between the framework & the service-panel is completly different now, rather than going over scope attributes for interactions with the framework, the attributes are now bount to the controller (accessible using "this") - a data sharing system for the panels of one service was needed, "scope.data", (could also be bound to the controller) - scope.gui & scope.service are gone - some kind of "init"-thing was needed (e.g. for a several-panel setup) - (also fixed some bugs in other sections) clockgui.js is the most interesting part, to show this alternative desgin off, I adapted (and simplified) it also the complexity of ServiceCtrl is now reduced - there is air for some improvements, but this is showing off the principle perfectly

--- src/resource/WebGUI/main/mainCtrl.js
+++ src/resource/WebGUI/main/mainCtrl.js
@@ -1,16 +1,18 @@
 angular.module('mrlapp.main.mainCtrl', ['mrlapp.mrl'])
-        .controller('mainCtrl', ['$scope', '$log', '$filter', 'mrl', 'ServiceSvc', 'mrlLog',
-            function ($scope, $log, $filter, mrl, ServiceSvc, mrlLog) {
+        .controller('mainCtrl', ['$scope', '$log', '$filter', '$timeout', 'mrl', 'ServiceSvc', 'mrlLog',
+            function ($scope, $log, $filter, $timeout, mrl, ServiceSvc, mrlLog) {
 
                 $log.info('is connected: ' + mrl.isConnected());
 
                 //service-panels & update-routine
-                $scope.allpanels = ServiceSvc.getPanelsList();
-                $scope.panels = $filter('panellist')($scope.allpanels, 'main');
                 var panelsUpdated = function () {
-                    $scope.panels = $filter('panellist')($scope.allpanels, 'main');
-                    $scope.$apply();
+                    $timeout(function () {
+                        $scope.allpanels = ServiceSvc.getPanelsList();
+                        $scope.panels = $filter('panellist')($scope.allpanels, 'main');
+                        $log.info('panels-main', $scope.panels);
+                    });
                 };
+                panelsUpdated();
                 ServiceSvc.subscribeToUpdates(panelsUpdated);
 
                 //access the array containing all log-messages logged using $log
--- src/resource/WebGUI/service/serviceBodyDirective.js
+++ src/resource/WebGUI/service/serviceBodyDirective.js
@@ -2,25 +2,31 @@ angular.module('mrlapp.service')
         .directive('serviceBody', ['$compile', function ($compile) {
                 return {
                     scope: {
-                        gui: '=',
-                        service: '=',
+                        data: '=',
                         size: '=',
-                        panelname: '='
+                        panelname: '=',
+                        cb: '='
                     },
                     link: function (scope, elem, attr) {
+//                        console.log('test-scope2', scope);
                         //inject controller into the service-panel
-                        var html = '<div ng-controller=\"' + attr.simplename + 'GuiCtrl\"><div service-body-second type="' + attr.type + '"></div></div>';
+                        var html = '<div ng-controller=\"' + attr.simplename + 'GuiCtrl as guictrl\"><div service-body-second type="' + attr.type + '" cb=' + scope.cb + '></div></div>';
                         elem.html(html).show();
                         $compile(elem.contents())(scope);
+//                        console.log('test-scope3', scope);
                     }
                 };
             }])
-
         .directive('serviceBodySecond', [function () {
                 return {
                     //inject template into the service-panel
                     templateUrl: function (element, attr) {
                         return 'service/views/' + attr.type + 'gui.html';
-                    }
+                    },
+//                    //set controller properties
+//                    link: function (scope, elem, attr) {
+//                        console.log('test-scope', scope, attr);
+//                        scope.cb.notifycontrollerisready(scope.guictrl);
+//                    }
                 };
             }]);
