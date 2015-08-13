54663a8cabe88486aaa1c31053e478132c05678f
LunDev <vogt-marvin@web.de>
2015-08-13 20:32:33 +0200

    WebUI - fix cligui

--- src/resource/WebGUI/service/js/cligui.js
+++ src/resource/WebGUI/service/js/cligui.js
@@ -1,53 +1,53 @@
 angular.module('mrlapp.service.cligui', [])
-.controller('CLIGuiCtrl', ['$scope', '$log', 'mrl', function($scope, $log, mrl) {
-    $log.info('CLIGuiCtrl');
-    
-    // get fresh copy
-    var service = mrl.getService($scope.service.name);
-    var name = $scope.service.name;
-    
-    this.onMsg = function(msg) {
-        switch (msg.method) {
-        case 'onStdout':
-            $scope.cli = $scope.cli + '\n' + msg.data[0];
-            $scope.$apply();
-            break;
-        case 'onClockStarted':
-            $scope.label = "Stop";
-            $scope.$apply();
-            break;
-        case 'onClockStopped':
-            $scope.label = "Start";
-            $scope.$apply();
-            break;
-        default:
-            $log.error("ERROR - unhandled method " + msg.method);
-            break;
-        }
-    }
-    ;
-    
-    //$scope.myStyle = ".boxsizingBorder {-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;}";    
-    // $scope.myStyle = "{'background-color':'black';}";
-    
-    var buffer = "";
-    
-    $scope.keyPress = function(event) {
-        var keyCode = event.keyCode;
-        var c = String.fromCharCode((96 <= keyCode && keyCode <= 105)? keyCode-48 : keyCode);
-        $log.info('keyPress ', keyCode);
-        if (keyCode == 13) {
-            mrl.sendTo(name, 'process', buffer);
-            buffer = '';
-            return;
-        }
-        buffer = buffer + String.fromCharCode(keyCode);
-    }
-    ;
-    
-    mrl.subscribe($scope.service.name, 'stdout');
-    
-    //after you're done with setting up your service-panel, call this method
-    $scope.panel.initDone();
-}
-]);
+        .controller('CLIGuiCtrl', ['$scope', '$log', 'mrl', function ($scope, $log, mrl) {
+                var _self = this;
+                $log.info('CLIGuiCtrl');
+
+                this.init = function () {
+                    // get fresh copy
+                    var service = mrl.getService($scope.service.name);
+
+                    this.onMsg = function (msg) {
+                        switch (msg.method) {
+                            case 'onStdout':
+                                $scope.data.cli = $scope.data.cli + '\n' + msg.data[0];
+                                $scope.$apply();
+                                break;
+                            case 'onClockStarted':
+                                $scope.data.label = "Stop";
+                                $scope.$apply();
+                                break;
+                            case 'onClockStopped':
+                                $scope.data.label = "Start";
+                                $scope.$apply();
+                                break;
+                            default:
+                                $log.error("ERROR - unhandled method " + msg.method);
+                                break;
+                        }
+                    }
+                    ;
+
+                    //$scope.myStyle = ".boxsizingBorder {-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;}";    
+                    // $scope.myStyle = "{'background-color':'black';}";
+
+                    var buffer = "";
+
+                    $scope.data.keyPress = function (event) {
+                        var keyCode = event.keyCode;
+                        var c = String.fromCharCode((96 <= keyCode && keyCode <= 105) ? keyCode - 48 : keyCode);
+                        $log.info('keyPress ', keyCode);
+                        if (keyCode == 13) {
+                            this.send('process', buffer);
+                            buffer = '';
+                            return;
+                        }
+                        buffer = buffer + String.fromCharCode(keyCode);
+                    };
+
+                    this.subscribe('stdout');
+                };
+
+                $scope.cb.notifycontrollerisready(this);
+            }
+        ]);
