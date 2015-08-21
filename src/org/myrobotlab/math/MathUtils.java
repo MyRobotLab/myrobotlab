553ed5993797ef48ce3c9d3bb7d09604d39ce3b4
U-neptune\kwatters <kwatters@kmwllc.com>
2015-08-21 15:11:06 -0400

    updating DH params with actual measurements from my InMoov and ensuring to return degrees, not radians.

--- src/org/myrobotlab/math/MathUtils.java
+++ src/org/myrobotlab/math/MathUtils.java
@@ -12,5 +12,8 @@ public class MathUtils {
 	public static double degToRad(double degrees) {
 		return degrees * Math.PI/180.0;
 	};
+	public static double radToDeg(double radians) {
+		return radians * 57.2957795;
+	}
 	
 }
