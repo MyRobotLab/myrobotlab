e2577dbd8def91794d05459a22de12f70b694fe8
Alessandruino <alessandro.didonna@gmail.com>
2015-08-28 16:27:02 +0200

    Added listener , publisher interfaces for Myo and MyoData

---
+++ src/org/myrobotlab/service/data/MyoData.java
@@ -0,0 +1,66 @@
+package org.myrobotlab.service.data;
+
+import com.thalmic.myo.Pose;
+
+/**
+ * @author GroG
+ * 
+ *         
+ * 
+ */
+public class MyoData {
+
+	public double roll = 0;
+	public double pitch = 0;
+	public double yaw = 0;
+	public Pose currentPose;
+
+	// default constructor (values will be null until set)
+	public MyoData() {
+	}
+	
+	// constructor with initial values for roll/pitch/yaw
+	public MyoData(double roll, double pitch, double yaw, Pose currentPose) {
+		this.roll = roll;
+		this.pitch = pitch;
+		this.yaw = yaw;
+		this.currentPose = currentPose;
+	}
+	
+
+	
+	public double getRoll() {
+		return roll;
+	}
+
+	public double getPitch() {
+		return pitch;
+	}
+	
+	public double getYaw() {
+		return yaw;
+	}
+	
+	public Pose getPose() {
+		return currentPose;
+	}
+
+	public void setRoll(double roll) {
+		this.roll = roll;
+	}
+
+	public void setPitch(double pitch) {
+		this.pitch = pitch;
+	}
+
+	public void setYaw(double yaw) {
+		this.yaw = yaw;
+	}
+
+	@Override
+	public String toString() {
+		return "MyoData [roll=" + roll + ", pitch=" + pitch + ", yaw=" + yaw +", pose=" + currentPose
+				+ "]";
+	}
+
+}
