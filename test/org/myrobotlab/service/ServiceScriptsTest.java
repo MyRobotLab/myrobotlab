73fac00f925a6bab00440ee4ca4c1badcc066ccd
U-neptune\kwatters <kwatters@kmwllc.com>
2015-09-22 15:35:38 -0400

    Auto testing elf to auto test default scripts.

---
+++ test/org/myrobotlab/service/ServiceScriptsTest.java
@@ -0,0 +1,219 @@
+package org.myrobotlab.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.myrobotlab.logging.LoggerFactory;
+import org.slf4j.Logger;
+
+@Ignore
+public class ServiceScriptsTest {
+
+	public final static Logger log = LoggerFactory.getLogger(ServiceScriptsTest.class);
+
+	@Test
+	public final void testAllServices() throws ClassNotFoundException {
+
+		// start up python so we have it available to do some testing with.
+		Python python = (Python)Runtime.createAndStart("python", "Python");
+
+		String testScriptDirectory = "./src/resource/Python/examples/";
+		List<String> servicesToTest = listAllServices();
+		int numServices = servicesToTest.size();
+		int numServicePages = 0;
+		int numScripts = 0;
+		int numScriptsWorky = 0;
+		log.info("----------------------------------------------");
+		for (String service : servicesToTest) {
+			log.info("Testing Service: {}" , service);
+			
+			if (serviceHasWebPage(service)) {
+				log.info("Service {} has a web page..", service);
+				numServicePages++;
+			} else {
+				log.warn("Service {} does not have a web page..", service);
+			}
+			
+			// validate that a script exists
+			File script = new File(testScriptDirectory + service + ".py");
+			if (script.exists()) {
+				log.info("Service PASS: {}" , service); 
+				numScripts++;
+			} else {
+				log.warn("Missing Script for Service {}" , service);
+			}
+
+			if (testServiceScript(python, testScriptDirectory, service)) {
+				//log.info("Default script for {} executes ok!", service);
+				numScriptsWorky++;
+			} else {
+				//log.warn("Default script for {} blows up!", service);
+			}
+			log.info("----------------------------------------------");
+
+		}
+
+		log.info("----------------------------------------------");
+		log.info("Service Report");
+		log.info("Number of Services:      {}", numServices);
+		log.info("Number of Services Pages {}", numServicePages);
+		log.info("Number of Scripts:       {}", numScripts);
+		log.info("Number of Scripts Worky: {}", numScriptsWorky);
+		log.info("----------------------------------------------");
+		log.info("Done...");
+	}
+
+	private boolean testServiceScript(Python python, String testScriptDirectory, String service) {
+		String testScriptFile = testScriptDirectory + service + ".py";
+
+		File script = new File(testScriptFile);
+		if (!script.exists()) {
+			log.warn("No default script for Service {}", script);
+			return false;
+		} else {
+			log.info("Default Script Exists for {}" , service);
+		}
+		try {
+			String test = FileUtils.readFileToString(script);
+			python.eval(test);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			log.warn("Error Running script {}" , script);
+			e.printStackTrace();
+			
+			return false;
+		}
+		return true;
+	}
+
+	private boolean serviceHasWebPage(String service) {
+		String url = "http://www.myrobotlab.org/service/" + service;
+		InputStream in = null;
+		try {
+			in = new URL( url ).openStream();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			return false;
+		}
+
+		try {
+			// read the page (we don't care about contents. (yet))
+			IOUtils.toString( in );
+		} catch (IOException e) {
+			// e.printStackTrace();
+			return false;
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+		return true;
+	}
+
+	private List<String> listAllServices() throws ClassNotFoundException {
+		// TODO: should this be replaced with a call to Runtime ?
+		List<Class> classes = ServiceScriptsTest.getClassesForPackage("org.myrobotlab.service");
+		List<String> services = new ArrayList<String>();
+		for (Class c : classes) {
+			// System.out.println("CLASS:" + c.toString());
+			HashSet<String> superClasses = new HashSet<String>();
+			Class x = c;
+			while (true) {
+				if (x.getSuperclass() != null) {
+					superClasses.add(x.getSuperclass().toString());
+					x = x.getSuperclass();
+				} else {
+					break;
+				}
+
+			}
+			if (superClasses.contains("class org.myrobotlab.framework.Service")) {
+				// Get just the class name.
+				String [] parts = c.toString().split(" ")[1].split("\\.");
+				services.add(parts[parts.length-1]);
+				// System.out.println(parts[parts.length-1]);
+			}
+		}
+		return services;
+	}
+
+
+
+	/**
+	 * Attempts to list all the classes in the specified package as determined
+	 * by the context class loader
+	 * 
+	 * @param pckgname
+	 *            the package name to search
+	 * @return a list of classes that exist within that package
+	 * @throws ClassNotFoundException
+	 *             if something went wrong
+	 *             
+	 * Ref: http://stackoverflow.com/questions/1498122/java-loop-on-all-the-classes-in-the-classpath
+	 */
+	private static List<Class> getClassesForPackage(String pckgname) throws ClassNotFoundException {
+		// This will hold a list of directories matching the pckgname. There may be more than one if a package is split over multiple jars/paths
+		ArrayList<File> directories = new ArrayList<File>();
+		try {
+			ClassLoader cld = Thread.currentThread().getContextClassLoader();
+			if (cld == null) {
+				throw new ClassNotFoundException("Can't get class loader.");
+			}
+			String path = pckgname.replace('.', '/');
+			// Ask for all resources for the path
+			Enumeration<URL> resources = cld.getResources(path);
+			while (resources.hasMoreElements()) {
+				directories.add(new File(URLDecoder.decode(resources.nextElement().getPath(), "UTF-8")));
+			}
+		} catch (NullPointerException x) {
+			throw new ClassNotFoundException(pckgname + " does not appear to be a valid package (Null pointer exception)");
+		} catch (UnsupportedEncodingException encex) {
+			throw new ClassNotFoundException(pckgname + " does not appear to be a valid package (Unsupported encoding)");
+		} catch (IOException ioex) {
+			throw new ClassNotFoundException("IOException was thrown when trying to get all resources for " + pckgname);
+		}
+
+		ArrayList<Class> classes = new ArrayList<Class>();
+		// For every directory identified capture all the .class files
+		for (File directory : directories) {
+			if (directory.exists()) {
+				// Get the list of the files contained in the package
+				String[] files = directory.list();
+				for (String file : files) {
+					// we are only interested in .class files
+					if (file.endsWith(".class")) {
+						// removes the .class extension
+						try
+						{
+							classes.add(Class.forName(pckgname + '.' + file.substring(0, file.length() - 6)));                      
+						}
+						catch (NoClassDefFoundError e)
+						{
+							// do nothing. this class hasn't been found by the loader, and we don't care.
+						}
+					}
+				}
+			} else {
+				throw new ClassNotFoundException(pckgname + " (" + directory.getPath() + ") does not appear to be a valid package");
+			}
+		}
+		return classes;
+	} 
+
+}
