9aa1ba077db52bf14509fb0390983a01ac733fea
kwatters <kwatters@kmwllc.com>
2015-09-30 02:18:36 -0400

    more unit tests... for Pin class

---
+++ test/org/myrobotlab/service/data/PinTest.java
@@ -0,0 +1,33 @@
+package org.myrobotlab.service.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.bytedeco.javacpp.opencv_highgui.Pt2Func_int_byte__;
+import org.junit.Test;
+
+public class PinTest {
+
+	@Test
+	public void testPin() {
+		Pin p = new Pin();
+		assertNotNull(p);
+
+		Pin p2 = new Pin(1,2,3,"foo");
+	
+		assertEquals(1, p2.pin);
+		assertEquals(2, p2.type);
+		assertEquals(3, p2.value);
+		assertEquals("foo", p2.source);
+	
+		Pin p3 = new Pin(p2);
+		
+		p2.pin = 4;
+		assertEquals(p3.pin, 1);
+				
+				
+		
+	}
+	
+	
+}
