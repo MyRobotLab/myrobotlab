d677225b60fbe8d864ac4347cf7ba75fe810ee0c
U-neptune\kwatters <kwatters@kmwllc.com>
2015-10-09 18:59:49 -0400

    making things a little more robust, but hopefully now oculus is 1 click worky on windows. (assuming you have the oculus runtime installed.)

--- src/org/myrobotlab/oculus/lwjgl/renderengine/Renderer.java
+++ src/org/myrobotlab/oculus/lwjgl/renderengine/Renderer.java
@@ -4,8 +4,10 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.OpenGLException;
 import org.myrobotlab.oculus.lwjgl.models.RawModel;
 import org.myrobotlab.oculus.lwjgl.models.TexturedModel;
+import org.newdawn.slick.util.Log;
 
 public class Renderer {
 
@@ -19,15 +21,22 @@ public class Renderer {
 		
 		RawModel model = texturedModel.getRawModel();
 		
-		GL30.glBindVertexArray(model.getVaoID());
-		GL20.glEnableVertexAttribArray(0);
-		GL20.glEnableVertexAttribArray(1);
-		GL13.glActiveTexture(GL13.GL_TEXTURE0);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texturedModel.getTexture().getID());
-		GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
-		GL20.glDisableVertexAttribArray(0);
-		GL20.glDisableVertexAttribArray(1);
-		GL30.glBindVertexArray(0);
+		try {
+			GL30.glBindVertexArray(model.getVaoID());
+			GL20.glEnableVertexAttribArray(0);
+			GL20.glEnableVertexAttribArray(1);
+			GL13.glActiveTexture(GL13.GL_TEXTURE0);
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, texturedModel.getTexture().getID());
+			GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+			GL20.glDisableVertexAttribArray(0);
+			GL20.glDisableVertexAttribArray(1);
+			GL30.glBindVertexArray(0);
+		} catch (OpenGLException e) {
+			// TODO: figure out what the heck is going on sometimes.. we miss the texture.
+			// it seems to become null or isn't bound?!
+			//log.error("OPEN GL EXCEPTION");
+			e.printStackTrace();
+		}
 	};
 	
 }
