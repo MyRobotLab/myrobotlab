700b0a66da1cef2db69d14a56b080f88ea98f7ef
U-neptune\kwatters <kwatters@kmwllc.com>
2015-10-13 23:42:49 -0400

    Adding a default script for Acapela service.

---
+++ src/resource/Python/examples/AcapelaSpeech.py
@@ -0,0 +1,10 @@
+# Example of how to use the Acapela Speech service
+acapelaSpeech = Runtime.createAndStart("speech", "AcapelaSpeech")
+acapelaSpeech.speak("Hello world")
+voices = acapelaSpeech.getVoices()
+for voice in voices:
+    acapelaSpeech.setVoice(voice)
+    print(voice)
+    acapelaSpeech.speak("hello world")
+    
+
