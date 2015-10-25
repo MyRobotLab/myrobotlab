57ac83b84c90f09326d08b0d35837a128bd44617
kwatters <kwatters@kmwllc.com>
2015-10-25 14:15:21 -0400

    fixing the double encoding problem for programAB and  OOB tags...

--- src/org/myrobotlab/service/ProgramAB.java
+++ src/org/myrobotlab/service/ProgramAB.java
@@ -225,7 +225,7 @@ public class ProgramAB extends Service implements TextListener, TextPublisher {
 
 		// OOB text should not be published as part of the response text.
 		Matcher matcher = oobPattern.matcher(res);
-		res = matcher.replaceAll("");
+		res = matcher.replaceAll("").trim();
 
 		Response response = new Response(session, res, payloads, lastResponseTime);
 		// Now that we've said something, lets create a timer task to wait for N
@@ -328,21 +328,52 @@ public class ProgramAB extends Service implements TextListener, TextPublisher {
 	}
 
 	private OOBPayload parseOOB(String oobPayload) {
-		JAXBContext jaxbContext;
-		try {
-			jaxbContext = JAXBContext.newInstance(OOBPayload.class);
-			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			log.info("OOB PAYLOAD :" + oobPayload);
-			Reader r = new StringReader(oobPayload);
-			OOBPayload oobMsg = (OOBPayload) jaxbUnmarshaller.unmarshal(r);
-			return oobMsg;
-		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-		log.info("OOB tag found, but it's not an MRL tag. {}", oobPayload);
-		return null;
+		// TODO: fix the damn double encoding issue.
+		// we have user entered text in the service/method 
+		// and params values.
+		// grab the service
+		Pattern servicePattern = Pattern.compile("<service>(.*?)</service>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+		Matcher serviceMatcher = servicePattern.matcher(oobPayload);
+		serviceMatcher.find();
+		String serviceName = serviceMatcher.group(1);
+		
+		Pattern methodPattern = Pattern.compile("<method>(.*?)</method>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+		Matcher methodMatcher = methodPattern.matcher(oobPayload);
+		methodMatcher.find();
+		String methodName = methodMatcher.group(1);
+
+		
+		Pattern paramPattern = Pattern.compile("<param>(.*?)</param>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+		Matcher paramMatcher = paramPattern.matcher(oobPayload);
+		ArrayList<String> params = new ArrayList<String>();
+		while (paramMatcher.find()) {
+			// We found some OOB text.
+			// assume only one OOB in the text?
+			String param = paramMatcher.group(1);
+			params.add(param);
+		}
+		OOBPayload payload = new OOBPayload(serviceName, methodName, params);
+		// log.info(payload.toString());
+		return payload;
+		
+		// JAXB stuff blows up because the response from program ab is already xml decoded!  
+		//
+//		JAXBContext jaxbContext;
+//		try {
+//			jaxbContext = JAXBContext.newInstance(OOBPayload.class);
+//			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+//			log.info("OOB PAYLOAD :" + oobPayload);
+//			Reader r = new StringReader(oobPayload);
+//			OOBPayload oobMsg = (OOBPayload) jaxbUnmarshaller.unmarshal(r);
+//			return oobMsg;
+//		} catch (JAXBException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+//		log.info("OOB tag found, but it's not an MRL tag. {}", oobPayload);
+//		return null;
 	}
 
 	private List<OOBPayload> processOOB(String text) {
