58491e20d495930f5ea42994f4a55ed46d5ac301
LunDev <vogt-marvin@web.de>
2015-10-27 17:13:10 +0100

    reverting

--- src/resource/WebGui/main/statesvc.js
+++ src/resource/WebGui/main/statesvc.js
@@ -1,5 +1,7 @@
 /*
     mis-named statesvc should be named statussvs
+    new file is put in
+    TODO - unused - could be removed now
 */
 angular.module('mrlapp.main.statesvc', ['mrlapp.mrl'])
 .service('StateSvc', [function() {
--- src/resource/WebGui/service/js/RuntimeGui.js
+++ src/resource/WebGui/service/js/RuntimeGui.js
@@ -1,49 +1,50 @@
 angular.module('mrlapp.service.RuntimeGui', [])
+        .controller('RuntimeGuiCtrl', ['$scope', '$log', 'mrl', function ($scope, $log, mrl) {
+                $log.info('RuntimeGuiCtrl');
+                var _self = this;
+                var msg = this.msg;
 
-.controller('RuntimeGuiCtrl', ['$scope', '$log', 'mrl', function($scope, $log, mrl) {
-        $log.info('RuntimeGuiCtrl');
-                
-        $scope.service = mrl.getService($scope.service.name);
-        var platform = $scope.service.platform;
-        // make the platform string
-        $scope.platform = platform.arch + "." + platform.bitness + "." + platform.os;
-        $scope.version = platform.mrlVersion;
-        
-        $scope.panel.onMsg = function(msg) {
-            
-            switch (msg.method) {
-                case 'onPulse':
-                    $scope.pulseData = msg.data[0];
-                    $scope.$apply();
-                    break;
-                case 'onClockStarted':
-                    $scope.label = "Stop";
-                    $scope.intervalDisabled = true;
-                    $scope.$apply();
-                    break;
-                case 'onClockStopped':
-                    $scope.label = "Start";
-                    $scope.intervalDisabled = false;
-                    $scope.$apply();
-                    break;
-                default:
-                    $log.error("ERROR - unhandled method " + $scope.name + " " + msg.method);
-                    break;
-            }
-        };
-        
-        $scope.localServiceData = $scope.service.repo.localServiceData.serviceTypes;
-        //mrl.subscribe($scope.service.name, 'pulse');            
-        
-        $scope.newType = undefined;
-        $scope.possibleServices = mrl.getPossibleServices();
-   
-        $scope.start = function(newName, newTypeModel) {
-            mrl.sendTo($scope.service.name, "start", newName, newTypeModel.name);
-        }
-         
-        $scope.install = function() {
-            mrl.sendTo($scope.service.name, "updateAll");
-        }
-    
-    }]);
+                $scope.service = mrl.getService($scope.service.name);
+                var platform = $scope.service.platform;
+                // make the platform string
+                $scope.platform = platform.arch + "." + platform.bitness + "." + platform.os;
+                $scope.version = platform.mrlVersion;
+
+                this.onMsg = function (msg) {
+
+                    switch (msg.method) {
+                        case 'onPulse':
+                            $scope.pulseData = msg.data[0];
+                            $scope.$apply();
+                            break;
+                        case 'onClockStarted':
+                            $scope.label = "Stop";
+                            $scope.intervalDisabled = true;
+                            $scope.$apply();
+                            break;
+                        case 'onClockStopped':
+                            $scope.label = "Start";
+                            $scope.intervalDisabled = false;
+                            $scope.$apply();
+                            break;
+                        default:
+                            $log.error("ERROR - unhandled method " + $scope.name + " " + msg.method);
+                            break;
+                    }
+                };
+
+                $scope.localServiceData = $scope.service.repo.localServiceData.serviceTypes;
+                //mrl.subscribe($scope.service.name, 'pulse');            
+
+                $scope.newType = undefined;
+                $scope.possibleServices = mrl.getPossibleServices();
+
+                $scope.start = function (newName, newTypeModel) {
+                    mrl.sendTo($scope.service.name, "start", newName, newTypeModel.name);
+                }
+
+                $scope.install = function () {
+                    mrl.sendTo($scope.service.name, "updateAll");
+                }
+
+            }]);
