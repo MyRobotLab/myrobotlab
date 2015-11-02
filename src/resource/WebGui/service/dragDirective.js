5377cf3f574d8136d1739dd686d457090c51245d
LunDev <vogt-marvin@web.de>
2015-11-02 22:10:52 +0100

    WebGUI - clean up -> split serviceDirective -> clean up serviceGui - framework -> remove unused code

---
+++ src/resource/WebGui/service/dragDirective.js
@@ -0,0 +1,45 @@
+angular.module('mrlapp.service')
+        .directive('dragDirective', ['$document', 'serviceSvc', function ($document, serviceSvc) {
+                return {
+                    link: function (scope, element, attr) {
+
+                        var startX = 0;
+                        var startY = 0;
+                        var resizeX = 10;
+
+                        element.on('mousedown', function (event) {
+                            startX = event.pageX - scope.panel.posx;
+                            startY = event.pageY - scope.panel.posy;
+                            if (((!scope.panel.panelsize.sizes[scope.panel.panelsize.aktsize].freeform)
+                                    || (scope.panel.panelsize.sizes[scope.panel.panelsize.aktsize].freeform
+                                            && startX < element.width() - resizeX))
+                                    && !scope.panel.panelsize.sizes[scope.panel.panelsize.aktsize].denyMoving) {
+                                // Prevent default dragging of selected content
+                                event.preventDefault();
+
+                                serviceSvc.putPanelZIndexOnTop(scope.panel.name, scope.panel.panelname);
+
+                                element.css({
+                                    cursor: 'move'
+                                });
+                                $document.on('mousemove', mousemove);
+                                $document.on('mouseup', mouseup);
+                            }
+                        });
+
+                        function mousemove(event) {
+                            scope.panel.posy = event.pageY - startY;
+                            scope.panel.posx = event.pageX - startX;
+                            scope.panel.notifyPositionChanged();
+                        }
+
+                        function mouseup() {
+                            $document.off('mousemove', mousemove);
+                            $document.off('mouseup', mouseup);
+                            element.css({
+                                cursor: 'auto'
+                            });
+                        }
+                    }
+                };
+            }]);
