149ad09c7fc05a707500ff0d0afa959d660ecd75
Kevin Watters <kwatters@kmwllc.com>
2015-11-20 11:53:39 -0500

    adding unit test for a stage.

---
+++ test/org/myrobotlab/document/transformer/AbstractStageTest.java
@@ -0,0 +1,20 @@
+package org.myrobotlab.document.transformer;
+
+import org.junit.Test;
+import org.myrobotlab.document.Document;
+
+public abstract class AbstractStageTest {
+	
+	public abstract Document createDocument();
+	public abstract AbstractStage createStage();
+	public abstract void validate(Document doc);
+	
+	@Test
+	public void test() {
+		AbstractStage stage = createStage();
+		Document doc = createDocument();
+		stage.processDocument(doc);
+		validate(doc);
+	}
+	
+}
