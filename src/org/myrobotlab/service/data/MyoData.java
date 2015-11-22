46bf9c7bda54d2b10618c96dea2648f8bedcc803
GroG <supertick@gmail.com>
2015-11-22 07:41:46 -0800

    MyoData simplified for serialization

--- src/org/myrobotlab/service/data/MyoData.java
+++ src/org/myrobotlab/service/data/MyoData.java
@@ -2,8 +2,6 @@ package org.myrobotlab.service.data;
 
 import java.io.Serializable;
 
-import com.thalmic.myo.enums.PoseType;
-
 /**
  * @author GroG
  * 
@@ -19,50 +17,18 @@ public class MyoData implements Serializable {
 	public double roll = 0;
 	public double pitch = 0;
 	public double yaw = 0;
-	public PoseType currentPose;
+	public String currentPose = null;
 
-	// default constructor (values will be null until set)
 	public MyoData() {
 	}
 	
 	// constructor with initial values for roll/pitch/yaw
-	public MyoData(long timestamp, double roll, double pitch, double yaw, PoseType currentPose) {
+	public MyoData(long timestamp, double roll, double pitch, double yaw, String currentPose) {
 		this.timestamp = timestamp;
 		this.roll = roll;
 		this.pitch = pitch;
 		this.yaw = yaw;
 		this.currentPose = currentPose;
-	}
-	
-
-	
-	public double getRoll() {
-		return roll;
-	}
-
-	public double getPitch() {
-		return pitch;
-	}
-	
-	public double getYaw() {
-		return yaw;
-	}
-	
-	public PoseType getPose() {
-		return currentPose;
-	}
-
-
-	public void setRoll(double roll) {
-		this.roll = roll;
-	}
-
-	public void setPitch(double pitch) {
-		this.pitch = pitch;
-	}
-
-	public void setYaw(double yaw) {
-		this.yaw = yaw;
 	}
 
 	@Override
