d1ae7e4ff283846134edcd5a8b3a2621f1a1f3e4
Kevin Watters <kwatters@kmwllc.com>
2015-12-01 10:23:10 -0500

    some performance improvements for the ik3d gui.

--- src/resource/WebGui/service/js/InverseKinematics3DGui.js
+++ src/resource/WebGui/service/js/InverseKinematics3DGui.js
@@ -19,70 +19,85 @@ angular.module('mrlapp.service.InverseKinematics3DGui', [])
 
         _self.updateState($scope.service);
 
+        // Rendering area helper functions
+        function buildAxis( src, dst, colorHex, dashed ) {
+	        var geom = new THREE.Geometry(),
+            mat; 
+            if(dashed) {
+                mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+            } else {
+                mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+            }
+            geom.vertices.push( src.clone() );
+            geom.vertices.push( dst.clone() );
+            geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+            var axis = new THREE.Line( geom, mat, THREE.LinePieces );
+            return axis;
+        };
         
+		//ref: http://soledadpenades.com/articles/three-js-tutorials/drawing-the-coordinate-axes/
+		function buildAxes( length ) {
+			    var xColor = 0x990000;
+			    var yColor = 0x009900;
+			    var zColor = 0x000099;
+		        var axes = new THREE.Object3D();
+		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), xColor, false ) ); // +X
+		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), xColor, true) ); // -X
+		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), yColor, false ) ); // +Y
+		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), yColor, true ) ); // -Y
+		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), zColor, false ) ); // +Z
+		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), zColor, true ) ); // -Z
+		        return axes;
+		};
+        
+		
+		// document.body.appendChild( container );
+		//  TODO: how do we tell it to use the container?
+		var renderer = new THREE.WebGLRenderer();
+		renderer.setSize( 400, 400 );
+		// add the x,y,z axis lines to the scene.
+		var axis = buildAxes( 500 );
+		//var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+		var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+		camera.position.x = 50;
+		camera.position.y = -200;
+		camera.position.z = 500;
+        
+		
+		
+		
         this.onMsg = function(msg) {
             $log.info("On Message IK3D!");
             $log.info(msg);
             switch (msg.method) {
                 case 'onJointPositions':
-                	$log.info("On Joint Positions..");
+                	//$log.info("On Joint Positions..");
                     $scope.positions = msg.data[0];
                     $scope.$apply();
-                    
-            		// Our Javascript will go here.
+                    // Our Javascript will go here.
             		// ref: http://soledadpenades.com/articles/three-js-tutorials/drawing-the-coordinate-axes/
-            		function buildAxis( src, dst, colorHex, dashed ) {
-            		        var geom = new THREE.Geometry(),
-            		            mat; 
-            		        if(dashed) {
-            		                mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
-            		        } else {
-            		                mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
-            		        }
-            		        geom.vertices.push( src.clone() );
-            		        geom.vertices.push( dst.clone() );
-            		        geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
-            		        var axis = new THREE.Line( geom, mat, THREE.LinePieces );
-            		        return axis;
-            		};
-
-            		//ref: http://soledadpenades.com/articles/three-js-tutorials/drawing-the-coordinate-axes/
-            		function buildAxes( length ) {
-            			    var xColor = 0x990000;
-            			    var yColor = 0x009900;
-            			    var zColor = 0x000099;
-            		        var axes = new THREE.Object3D();
-            		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), xColor, false ) ); // +X
-            		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), xColor, true) ); // -X
-            		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), yColor, false ) ); // +Y
-            		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), yColor, true ) ); // -Y
-            		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), zColor, false ) ); // +Z
-            		        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), zColor, true ) ); // -Z
-            		        return axes;
-            		};
-
-            		// Initialize the scene.
-            		var scene = new THREE.Scene();
-            		var container = document.getElementById( 'canvas' );
-            		// document.body.appendChild( container );
-            		//  TODO: how do we tell it to use the container?
-            		var renderer = new THREE.WebGLRenderer();
-            		renderer.setSize( 400, 400 );
-            		//container.appendChild( renderer.domElement );
-            		// var renderer = new THREE.WebGLRenderer();
-            		//renderer.setSize( window.innerWidth, window.innerHeight );
-            		// renderer.setSize( 400,400);
+                    var container = document.getElementById( 'canvas' );
+            		// $log.info("CANVAS CONTAINER: " + container);
             		if (container.hasChildNodes()) {
             			container.removeChild( container.childNodes[0]);
             			container.appendChild( renderer.domElement );	
             		} else {
             			container.appendChild( renderer.domElement );
             		}
+
+            		// Initialize the scene.
+            		var scene = new THREE.Scene();
+            		scene.add( axis );
             		
+            		// scene.children={};
+            		//container.appendChild( renderer.domElement );
+            		// var renderer = new THREE.WebGLRenderer();
+            		//renderer.setSize( window.innerWidth, window.innerHeight );
+            		// renderer.setSize( 400,400);
             		// startpoint/stoppoint for each link as array  [ [ x1,y1,z1 ] , [ x2,y2,z2 ] ] stop x,y,z  points.
             		// This is the start/stop positions of all the joints
+            		 //console.log($scope.positions);
             		
-            		 console.log($scope.positions);
             		 var linkPoints = [];
             		 for (i = 0 ; i < $scope.positions.length-1; i++) {
             			 //console.log("Push a point!");
@@ -99,46 +114,37 @@ angular.module('mrlapp.service.InverseKinematics3DGui', [])
             		//  [[0,0,0], [10,10,10]],
             		//  [[10,10,10], [15,1,0]]
             		//];
-            		 var numLinks = linkPoints.length;
-
+            		var numLinks = linkPoints.length;
             		var colorHex = [0x0000ff, 0x00ff00];	
             		var numColors = colorHex.length;
             		for (i = 0; i < numLinks; i++) {
-            			
+            			// start and end point for this link.
             			var startPoint = linkPoints[i][0];
             			var stopPoint = linkPoints[i][1];
-            			// console.log("Here!" + startPoint + " TO " + stopPoint); 
-            			
+            			// the color to render for this link 
             			var color = colorHex[i%numColors];
+            			// create 2 vector objects representing the start/stop points for the line/
             			var start = new THREE.Vector3( startPoint[0], startPoint[1], startPoint[2] );
             			var stop = new THREE.Vector3( stopPoint[0], stopPoint[1], stopPoint[2] );
             			var lineGeom = new THREE.Geometry();
             		    var lineMat = new THREE.LineBasicMaterial({ linewidth: 5, color: color });
+            		    // push the points for the start/stop of the line.
             		    lineGeom.vertices.push( start );
             		    lineGeom.vertices.push( stop );
-            		    // lineGeom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+            		    // create a segment 
             		    var segment = new THREE.Line( lineGeom, lineMat, THREE.LinePieces );
-            			scene.add( segment.clone() );
+            		    // add the line segment to the scene.
+            			scene.add( segment );
             		}; 
 
-            		// add the x,y,z axis lines to the scene.
-            		var axis = buildAxes( 500 );
-            		scene.add( axis );
-            		// Set up the camera.
-
-            		//var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-            		var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
-            		camera.position.x = 50;
-            		camera.position.y = -200;
-            		camera.position.z = 500;
             		var render = function () {
             			requestAnimationFrame( render );
             			renderer.render(scene, camera);
             		};
+
+            		// finally render the scene.
             		render();
-            				
-                    
-                    break;
+            		break;
                 case 'onJointAngles':
                 	$log.info("On Joint Angles..");
                 	$scope.angles = msg.data[0];
