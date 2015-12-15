fd89336567cc8686a8c68778013fe894f6a2578d
GroG <supertick@gmail.com>
2015-12-14 20:33:55 -0800

    import correction

--- test/org/myrobotlab/service/ArduinoTest.java
+++ test/org/myrobotlab/service/ArduinoTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Ignore;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
