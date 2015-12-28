76dd772b145bf59ef15fdda477c9bb2a7386d4a6
LunDev <vogt-marvin@web.de>
2015-12-28 14:54:57 +0100

    WebGUI - noWorky -> noWorky more self-contained -> clean up old no worky stuff

--- src/resource/WebGui/nav/noWorkyCtrl.js
+++ src/resource/WebGui/nav/noWorkyCtrl.js
@@ -1,6 +1,26 @@
 angular.module('mrlapp.nav')
-        .controller('noWorkyCtrl', ['$scope', '$modalInstance', 'reason', 'mrl', function ($scope, $modalInstance, reason, mrl) {
+        .controller('noWorkyCtrl', ['$scope', '$modalInstance', 'reason', 'mrl', 'statusSvc', function ($scope, $modalInstance, reason, mrl, statusSvc) {
                 $scope.close = $modalInstance.close;
+        
+                $scope.status = 'waitingforinformation';
                 
-                $scope.noWorky = mrl.noWorky;
+                $scope.noWorky = function (userId) {
+                    $scope.status = 'sendingnoworky';
+                    mrl.noWorky(userId);
+                };
+                
+                var onNoWorky = function (noWorkyResultssMsg) {
+                    var status = noWorkyResultssMsg.data[0];
+//                    console.log('noWorkySvc-onNoWorky', status);
+                    if (status.level == 'error') {
+                        $scope.status = 'error';
+                        $scope.statuskey = status.key;
+                        statusSvc.addAlert('danger', 'the noWorky did not worky ! ' + status.key);
+                    } else {
+                        $scope.status = 'success';
+                        statusSvc.addAlert('success', 'noWorky sent !');
+                    }
+                };
+                mrl.subscribeToServiceMethod(onNoWorky, mrl.getRuntime().name, 'publishNoWorky');
+                mrl.subscribe(mrl.getRuntime().name, 'publishNoWorky');
             }]);
