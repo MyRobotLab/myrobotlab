357a3e91a2fc5a582b0fed326e7d2c686d8c854a
Kevin Watters <kwatters@kmwllc.com>
2016-01-06 16:20:29 -0500

    adding an initial version of OpenNLP support to the document pipeline to find peoples names mentioned in english text.

---
+++ test/org/myrobotlab/document/transformer/OpenNLPTest.java
@@ -0,0 +1,38 @@
+package org.myrobotlab.document.transformer;
+
+import org.junit.Assert;
+import org.myrobotlab.document.Document;
+
+public class OpenNLPTest extends AbstractStageTest {
+
+	@Override
+	public Document createDocument() {
+		// 
+		Document testDoc = new Document("doc_1");
+		testDoc.setField("title", "This is my document title.");
+		testDoc.setField("text", "This is my text field.  It mentions stuff about Greg Perry and Kevin Watters.");
+		return testDoc;
+	}
+
+	@Override
+	public AbstractStage createStage() {
+		// TODO Auto-generated method stub
+		String stageClass = "org.myrobotlab.document.transformer.OpenNLP";
+		String stageName = "opennlp";
+		StageConfiguration config = new StageConfiguration();
+		config.setStageClass(stageClass);
+		config.setStageName(stageName);
+		// TODO: i don't like how I implemented the config objects...
+		// TODO: make this return a config , not the stage itself?
+		OpenNLP transformer = new OpenNLP();
+		transformer.startStage(config);
+		return transformer;
+	}
+
+	@Override
+	public void validate(Document doc) {
+		Assert.assertEquals("Greg Perry", doc.getField("people").get(0).toString());
+		Assert.assertEquals("Kevin Watters", doc.getField("people").get(1).toString());
+	}
+
+}
