c61c59b7605c7d46f27a75e2c47426c24ad9aa88
PapaoutaiRobot <giova1920@hotmail.it>
2016-02-09 07:29:27 +0100

    Update AzureTranslatorGui.js

--- src/resource/WebGui/service/js/AzureTranslatorGui.js
+++ src/resource/WebGui/service/js/AzureTranslatorGui.js
@@ -1,48 +1,29 @@
-angular.module('mrlapp.service.AcapelaSpeechGui', [])
-.controller('AcapelaSpeechGuiCtrl', ['$scope', '$log', 'mrl', function($scope, $log, mrl) {
-    $log.info('AcapelaSpeechGuiCtrl');
+angular.module('mrlapp.service.AzureTranslatorGui', [])
+.controller('AzureTranslatorGuiCtrl', ['$scope', '$log', 'mrl', function($scope, $log, mrl) {
+    $log.info('AzureTranslatorGuiCtrl');
     var _self = this;
     var msg = this.msg;
     
-    $scope.text = '';
-    $scope.speakingState = '';
+    $scope.translatedText = '';
     
     // GOOD TEMPLATE TO FOLLOW
     this.updateState = function(service) {
         $scope.service = service;
-        $scope.voice = service.voice;
+        $scope.from = service.fromLanguage;
+        $scope.to = service.toLanguage;
     }
     ;
     _self.updateState($scope.service);
     
     // init scope variables
-    $scope.text = '';
+    $scope.translatedText = '';
     
     this.onMsg = function(inMsg) {
-        var data = inMsg.data[0];
-        switch (inMsg.method) {
-        case 'onState':
-            _self.updateState(data);
-            $scope.$apply();
-            break;
-        case 'onStartSpeaking':
-            $scope.speakingState = 'speaking';
-            $scope.$apply();
-            break;
-        case 'onEndSpeaking':
-            $scope.speakingState = 'finished speaking';
-            $scope.$apply();
-            break;
-        default:
-            $log.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method);
-            break;
-        }
+        
     }
     ;
     
     //mrl.subscribe($scope.service.name, 'pulse');
-    msg.subscribe('publishStartSpeaking');
-    msg.subscribe('publishEndSpeaking');
     msg.subscribe(this);
 }
 ]);
