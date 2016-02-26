de217e3f81297037c0d0d936716b0fdcf95692f3
Kevin Watters <kwatters@kmwllc.com>
2016-02-26 13:13:26 -0500

    Adding a unit test for the HtmlFilter service.

---
+++ test/org/myrobotlab/service/HtmlFilterTest.java
@@ -0,0 +1,18 @@
+package org.myrobotlab.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HtmlFilterTest {
+
+	@Test
+	public void testHtmlFilter() {
+		String html = "<div>test...</div>";
+		String expected = "test...";
+		HtmlFilter myfilter = (HtmlFilter)Runtime.createAndStart("myfilter", "HtmlFilter");
+		String actual = myfilter.stripHtml(html);
+		assertEquals(expected, actual);
+	}
+	
+}
