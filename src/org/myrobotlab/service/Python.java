438aeb0953795f45e5d25eba41cc8b1c7e2df6b4
GroG <supertick@gmail.com>
2016-03-25 11:59:06 -0700

    making uber jar .. its big .. but useful

--- src/org/myrobotlab/service/Python.java
+++ src/org/myrobotlab/service/Python.java
@@ -889,10 +889,11 @@ public class Python extends Service {
 		ServiceType meta = new ServiceType(Python.class.getCanonicalName());
 		meta.addDescription("Python ID");
 		meta.addCategory("programming", "control");
-		meta.addDependency("org.python.core", "2.7.0");
-		// sadly - the swing console for PythonGUI depends
-		// on Console class and it depends on log4j :P
-		// meta.addDependency("org.apache.log4j", "1.2.14");
+
+		// Its now part of myrobotlab.jar - unzipped in
+		// build.xml (part of myrobotlab.jar now)
+		// meta.addDependency("org.python.core", "2.7.0");
+
 		return meta;
 	}
 
