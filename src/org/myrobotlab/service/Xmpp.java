da2538e2d5d35de5efd3f5fac8e00f0e0c85c75a
GroG <supertick@gmail.com>
2016-04-01 05:57:52 -0700

    trying a different tact - printing to console and halt on error

--- src/org/myrobotlab/service/Xmpp.java
+++ src/org/myrobotlab/service/Xmpp.java
@@ -104,7 +104,7 @@ public class Xmpp extends Service implements Gateway, ChatManagerListener, ChatM
 		ServiceType meta = new ServiceType(Xmpp.class.getCanonicalName());
 		meta.addDescription("xmpp service to access the jabber network");
 		meta.addCategory("connectivity");
-		meta.addDependency("org.jivesoftware.smack", "3.3.0");
+		meta.addDependency("org.jivesoftware.smack", "4.1.6");
 		return meta;
 	}
 
