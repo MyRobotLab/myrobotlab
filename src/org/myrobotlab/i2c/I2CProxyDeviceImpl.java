4664e96f5f2eb69febc00ad62602f14273e9f26b
Mats <mats.onnerby@gmail.com>
2016-04-08 13:27:04 +0200

    Changed classpath to use pi4j 1.1-SNAPSHOT + interface changes

--- src/org/myrobotlab/i2c/I2CProxyDeviceImpl.java
+++ src/org/myrobotlab/i2c/I2CProxyDeviceImpl.java
@@ -17,7 +17,10 @@ public class I2CProxyDeviceImpl implements I2CDevice {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	@Override
+	public int read(byte[] writeBuffer, int writeOffset, int writeSize, byte[] readBuffer, int readOffset, int readSize) throws IOException {
+		return 0;
+	}
 	@Override
 	public int read(int arg0) throws IOException {
 		// TODO Auto-generated method stub
@@ -37,11 +40,20 @@ public class I2CProxyDeviceImpl implements I2CDevice {
 	}
 
 	@Override
+	public void write(int address, byte[] b) throws IOException {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
 	public void write(byte[] arg0, int arg1, int arg2) throws IOException {
 		// TODO Auto-generated method stub
 
 	}
-
+	@Override
+	public void write(byte[] b) throws IOException {
+		// TODO Auto-generated method stub
+	}
+	
 	@Override
 	public void write(int arg0, byte arg1) throws IOException {
 		// TODO Auto-generated method stub
