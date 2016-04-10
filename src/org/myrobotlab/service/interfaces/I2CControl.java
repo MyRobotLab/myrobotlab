06e496cc798eb6244aa7d009c874433afa9f4817
Mats <mats.onnerby@gmail.com>
2016-04-10 10:22:46 +0200

    Push of Raspi service to be able to test i2c on that hardware. Work in progress.

--- src/org/myrobotlab/service/interfaces/I2CControl.java
+++ src/org/myrobotlab/service/interfaces/I2CControl.java
@@ -1,6 +1,8 @@
 package org.myrobotlab.service.interfaces;
 
 import java.io.IOException;
+
+import com.pi4j.io.i2c.I2CDevice;
 /*
  * This interface is based on the methods for i2c read and write in the Pi4J project
  * It's used by RasPi and Arduino services to be able to use the same
@@ -8,59 +10,64 @@ import java.io.IOException;
  * to the ic2 bus on an Arduino. 
  */
 public interface I2CControl {
-
 /**
-* This method writes several bytes to the i2c device from given buffer at given offset.
+* This method creates a I2CDevice
 * 
+* @param busaddress
+* @param address local address in the i2c device
+* @param type 
+* 
+*/
+I2CDevice createDevice(int busAddress, int deviceAddress, String type);
+/**
+* This method returns and already existing I2CDevice
+* 
+* @param busaddress
+* @param address local address in the i2c device
+* 
+*/
+I2CDevice getDevice(int busAddress, int deviceAddress);	
+/**
+* This method writes several bytes to the i2c device from given buffer.
+* 
+* @param busaddress
 * @param address local address in the i2c device
 * @param buffer buffer of data to be written to the i2c device in one go
-* @param offset offset in buffer 
 * @param size number of bytes to be written 
 * 
-* @throws IOException thrown in case byte cannot be written to the i2c device or i2c bus
 */
-void i2cWrite(int address, byte[] buffer, int offset, int size);
+
+void i2cWrite(int busAddress, int deviceAddress, byte[] buffer, int size);
 /**
-* This method reads bytes from the i2c device to given buffer at asked offset. 
+* This method reads bytes from the i2c device to given buffer. 
 * 
+* @param busaddress
 * @param buffer buffer of data to be read from the i2c device in one go
-* @param offset offset in buffer 
 * @param size number of bytes to be read 
 * 
 * @return number of bytes read
 * 
-* @throws IOException thrown in case byte cannot be read from the i2c device or i2c bus
 */
-int i2cRead(byte[] buffer, int offset, int size);
+int i2cRead(int busAddress, int deviceAddress, byte[] buffer, int size);
 
 /**
-* This method reads bytes from the i2c device to given buffer at asked offset. 
-* 
-* @param address local address in the i2c device
-* @param buffer buffer of data to be read from the i2c device in one go
-* @param offset offset in buffer 
-* @param size number of bytes to be read 
-* 
-* @return number of bytes read
-* 
-* @throws IOException thrown in case byte cannot be read from the i2c device or i2c bus
-*/
-int i2cRead(int address, byte[] buffer, int offset, int size);
+* This method reads bytes from the i2c device to given buffer. 
+
 
 /**
 * This method writes and reads bytes to/from the i2c device in a single method call
 *
+* @param busaddress
+* @param address local address in the i2c device
 * @param writeBuffer buffer of data to be written to the i2c device in one go
-* @param writeOffset offset in write buffer
 * @param writeSize number of bytes to be written from buffer
 * @param readBuffer buffer of data to be read from the i2c device in one go
-* @param readOffset offset in read buffer
 * @param readSize number of bytes to be read
 *
 * @return number of bytes read
 *
 * @throws IOException thrown in case byte cannot be read from the i2c device or i2c bus
 */
-int i2CRead(byte[] writeBuffer, int writeOffset, int writeSize, byte[] readBuffer, int readOffset, int readSize);
+int i2CRead(int busAddress, int deviceAddress, byte[] writeBuffer, int writeSize, byte[] readBuffer, int readSize);
 	
 }
