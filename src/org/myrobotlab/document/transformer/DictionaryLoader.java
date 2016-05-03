740d4b6637864715d5198adb72928943bd274734
Dan Meehl <mr.cratos@gmail.com>
2016-05-03 11:42:13 -0400

    Keep all columns from the dictionary csv. Return as a List<String>.

--- src/org/myrobotlab/document/transformer/DictionaryLoader.java
+++ src/org/myrobotlab/document/transformer/DictionaryLoader.java
@@ -3,7 +3,9 @@ package org.myrobotlab.document.transformer;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.myrobotlab.logging.LoggerFactory;
 import org.slf4j.Logger;
@@ -22,11 +24,12 @@ public class DictionaryLoader {
 	public final static Logger log = LoggerFactory.getLogger(DictionaryLoader.class.getCanonicalName());
 	private static DictionaryLoader instance = null;
 
-	private HashMap<String, HashMap<String,String>> dictMap;
+	// csvFile -> map-of-values
+	private HashMap<String, HashMap<String,List<String>>> dictMap;
 
 	protected DictionaryLoader() {
 		// Exists only to defeat instantiation.
-		dictMap = new HashMap<String, HashMap<String,String>>();
+		dictMap = new HashMap<String, HashMap<String,List<String>>>();
 	}
 	public static DictionaryLoader getInstance() {
 		if(instance == null) {
@@ -35,24 +38,31 @@ public class DictionaryLoader {
 		return instance;
 	}
 
-	public synchronized HashMap<String,String> loadDictionary(String fileName) throws IOException {
+	public synchronized HashMap<String, List<String>> loadDictionary(String fileName) throws IOException {
 		// it's already loaded, just return it
 		if (dictMap.containsKey(fileName)) {
 			return dictMap.get(fileName);
 		}
-		// it's not loaded , load the file and put it in the dict map and return
-		// assume the file is a csv file with key/value pairs on each line
-		HashMap<String,String> dictionary = new HashMap<String,String>();
+		// It's not loaded, load the file and put it in the dict map and return.
+		// Assume the file is a csv file with key/value pairs on each line.
+		HashMap<String,List<String>> dictionary = new HashMap<String,List<String>>();
 		File dictFile = new File(fileName);
 		if (!dictFile.exists()) {
 			log.warn("Dictionary file not found {}", dictFile.getAbsolutePath());
 			return null;
 		}
 		CSVReader reader = new CSVReader(new FileReader(fileName));
-		String [] nextLine;
-		while ((nextLine = reader.readNext()) != null) {
-			// nextLine[] is an array of values from the line
-			dictionary.put(nextLine[0], nextLine[1]);
+		String [] line;
+		while ((line = reader.readNext()) != null) {
+      // line[] is an array of values from the line
+      List<String> listVal = dictionary.get(line[0]);
+      if (listVal == null) {
+        listVal = new ArrayList<String>();
+        dictionary.put(line[0], listVal);
+      }
+      for (int i = 1; i < line.length; i++ ) {
+        listVal.add(line[i]);
+      }
 		}
 		dictMap.put(fileName, dictionary);
 		reader.close();
