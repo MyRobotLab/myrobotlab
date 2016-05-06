4345a72cd1b3185256366d2bca724c5cccf90a51
Alessandruino <alessandro.didonna@gmail.com>
2016-05-06 20:37:37 +0200

    Added interfaces Point2DfPublisher and Point2DfListener

---
+++ src/org/myrobotlab/service/interfaces/Point2DfListener.java
@@ -0,0 +1,10 @@
+package org.myrobotlab.service.interfaces;
+
+import org.myrobotlab.service.data.Point2Df;
+
+public interface Point2DfListener {
+	
+	public String getName();
+	
+	public Point2Df onPoint2Df(Point2Df point);
+}
