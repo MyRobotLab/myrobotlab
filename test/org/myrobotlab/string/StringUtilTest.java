899d9bf0a1a510a9074b84c1dd40931de558e6ef
Kevin Watters <kwatters@kmwllc.com>
2016-06-04 12:29:18 -0400

    Adding a method to chunk up text into segments that are less than maxLength.  It preserves resonable word boundaries.

--- test/org/myrobotlab/string/StringUtilTest.java
+++ test/org/myrobotlab/string/StringUtilTest.java
@@ -2,6 +2,9 @@ package org.myrobotlab.string;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 public class StringUtilTest {
@@ -17,4 +20,18 @@ public class StringUtilTest {
   // should we get rid of the string util class?
   // or make it more useful
 
+  
+  @Test
+  public void testChunkString() {
+    int maxLength = 30;
+    String text = "This is a test of the emergency broadcast system. It has been done in a foo bar. With Testing of some foo. Slightly more bar. A little bit of foo though. And why shouldn't there be some sort of fooness to the air. foo for all and all for foo."; 
+    List<String> result = StringUtil.chunkText(text, maxLength);
+    //for (String s : result) {
+    //  System.out.println(s);
+    //}
+    //System.out.println("ORG:"+text);
+    //System.out.println("RES:"+StringUtils.join(result, " "));
+    assertEquals(text, StringUtils.join(result, " "));
+    
+  }
 }
