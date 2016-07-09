5b587319172888eac98682c84c5a6890221c0cfd
GroG <supertick@gmail.com>
2016-07-09 13:19:14 -0700

    updates for PinArrayControl and Arduino format info

--- src/org/myrobotlab/service/interfaces/PinArrayControl.java
+++ src/org/myrobotlab/service/interfaces/PinArrayControl.java
@@ -6,9 +6,15 @@ public interface PinArrayControl extends DeviceControl {
 
 	public List<PinDefinition> getPinList();
 	
-	public void setMode(String address, String mode);
+	public Integer read(int address);
 	
 	public Integer read(String address);
+	
+	public void setMode(int address, String mode);
+	
+	public void setMode(String address, String mode);
 
+	public void write(int address, Integer value);
+	
 	public void write(String address, Integer value);
 }
