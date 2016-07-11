d49677bc78d60fd1e4d3e07664e0a451cd4336b2
Calamity01 <christian.beliveau@gmail.com>
2016-07-11 07:58:07 -0400

    fixed neopixel service

--- src/resource/WebGui/service/js/NeoPixelGui.js
+++ src/resource/WebGui/service/js/NeoPixelGui.js
@@ -1,6 +1,6 @@
-angular.module('mrlapp.service.NeopixelGui', [])
-.controller('NeopixelGuiCtrl', ['$log', '$scope', 'mrl', function($log, $scope, mrl) {
-    $log.info('NeopixelGuiCtrl');
+angular.module('mrlapp.service.NeoPixelGui', [])
+.controller('NeoPixelGuiCtrl', ['$log', '$scope', 'mrl', function($log, $scope, mrl) {
+    $log.info('NeoPixelGuiCtrl');
     var _self = this;
     var msg = this.msg;
     
@@ -84,6 +84,6 @@ angular.module('mrlapp.service.NeopixelGui', [])
     var runtimeName = mrl.getRuntime().name;
     mrl.subscribe(runtimeName, 'getServiceNamesFromInterface');
     mrl.subscribeToServiceMethod(this.onMsg, runtimeName, 'getServiceNamesFromInterface');
-    mrl.sendTo(runtimeName, 'getServiceNamesFromInterface', 'org.myrobotlab.service.interfaces.NeopixelController');
+    mrl.sendTo(runtimeName, 'getServiceNamesFromInterface', 'org.myrobotlab.service.interfaces.NeoPixelController');
 }
 ]);
