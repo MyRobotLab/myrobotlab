8c60455ae4484de5233938a2e0e8373eb8c1e796
Mats <mats.onnerby@gmail.com>
2016-07-25 09:20:27 +0200

    I2cMux debug 8

--- src/org/myrobotlab/service/I2cMux.java
+++ src/org/myrobotlab/service/I2cMux.java
@@ -21,14 +21,15 @@ import org.slf4j.Logger;
 
 /**
  * 
- * I2CMux - This is the MyRobotLab Service that can be used if you have several i2c devices that
- * share the same address. Create one I2CMux for each of the i2c buses that you want to use.
- * It can be used with tca9548a and possibly other devices.
+ * I2CMux - This is the MyRobotLab Service that can be used if you have several
+ * i2c devices that share the same address. Create one I2CMux for each of the
+ * i2c buses that you want to use. It can be used with tca9548a and possibly
+ * other devices.
  * 
  * 
  * @author Mats Onnerby
- *  
- * More Info : https://www.adafruit.com/product/2717
+ * 
+ *         More Info : https://www.adafruit.com/product/2717
  * 
  */
 public class I2cMux extends Service implements I2CControl, I2CController {
@@ -41,16 +42,14 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 
 	public ArrayList<String> controllers = new ArrayList<String>();
 	public String controllerName;
-	
-	public List<String> deviceAddressList = Arrays.asList(
-			 "0x70","0x71","0x72","0x73","0x74","0x75","0x76","0x77");
-			
+
+	public List<String> deviceAddressList = Arrays.asList("0x70", "0x71", "0x72", "0x73", "0x74", "0x75", "0x76", "0x77");
+
 	public String deviceAddress = "0x70";
-	
-	public List<String> deviceBusList = Arrays.asList(
-			"0","1","2","3","4","5","6","7","8");	
+
+	public List<String> deviceBusList = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8");
 	public String deviceBus = "1";
-	
+
 	private boolean isAttached = false;
 
 	HashMap<String, I2CDeviceMap> i2cDevices = new HashMap<String, I2CDeviceMap>();
@@ -69,7 +68,7 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 
 	public I2cMux(String n) {
 		super(n);
-		
+
 		subscribe(Runtime.getInstance().getName(), "registered", this.getName(), "onRegistered");
 	}
 
@@ -78,48 +77,47 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 		broadcastState();
 	}
 
-   public void refreshControllers() {
-  	 
-  	controllers = Runtime.getServiceNamesFromInterface(I2CController.class);
+	public void refreshControllers() {
+
+		controllers = Runtime.getServiceNamesFromInterface(I2CController.class);
 		controllers.remove(this.getName());
-		
+
 		broadcastState();
 	}
-	
- 	public void setDeviceBus(String deviceBus){
+
+	public void setDeviceBus(String deviceBus) {
 		this.deviceBus = deviceBus;
 		broadcastState();
-  }
-	
-	public void setDeviceAddress(String deviceAddress){
+	}
+
+	public void setDeviceAddress(String deviceAddress) {
 		this.deviceAddress = deviceAddress;
 		broadcastState();
-  }
+	}
 
 	@Override
 	public void createI2cDevice(I2CControl control, int busAddress, int deviceAddress) {
 		// Create a new i2c device in case it doesn't already exists.
 		String key = String.format("%s.%d", this.deviceBus, deviceAddress);
-		if (i2cDevices.containsKey(key)){
+		if (i2cDevices.containsKey(key)) {
 			// Nothing to do, already exists
-		}
-		else
-		{
+		} else {
 			I2CDeviceMap deviceData = new I2CDeviceMap();
 			deviceData.busAddress = Integer.parseInt(this.deviceBus);
 			deviceData.deviceAddress = deviceAddress;
 			deviceData.control = this;
-			controller.createI2cDevice(this, deviceData.busAddress, deviceAddress);		
+			controller.createI2cDevice(this, deviceData.busAddress, deviceAddress);
 		}
 	}
 
 	@Override
 	public void releaseI2cDevice(I2CControl control, int busAddress, int deviceAddress) {
-		// Can't release the device at the lowest level since several devices may exist
+		// Can't release the device at the lowest level since several devices may
+		// exist
 		// with the same i2c address. It can always be reused.
 		// controller.releaseI2cDevice(this, busAddress, deviceAddress);
 	}
-	
+
 	/**
 	 * This methods sets the i2c Controller that will be used to communicate with
 	 * the i2c device
@@ -134,10 +132,11 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 		this.controllerName = controllerName;
 		return setController((I2CController) Runtime.getService(controllerName), this.deviceBus, this.deviceAddress);
 	}
-	
+
 	public boolean setController(I2CController controller) {
 		return setController(controller, this.deviceBus, this.deviceAddress);
 	}
+
 	/**
 	 * This methods sets the i2c Controller that will be used to communicate with
 	 * the i2c device
@@ -154,25 +153,25 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 		isAttached = true;
 
 		log.info(String.format("%s setController %s", getName(), controllerName));
-		
+
 		createDevice();
 		broadcastState();
 		return true;
 	}
-	
+
 	/**
 	 * This method creates the i2c device
 	 */
 	boolean createDevice() {
 		if (controller != null) {
-				controller.releaseI2cDevice(this, Integer.parseInt(deviceBus), Integer.decode(deviceAddress));
-				controller.createI2cDevice(this, Integer.parseInt(deviceBus), Integer.decode(deviceAddress));
+			controller.releaseI2cDevice(this, Integer.parseInt(deviceBus), Integer.decode(deviceAddress));
+			controller.createI2cDevice(this, Integer.parseInt(deviceBus), Integer.decode(deviceAddress));
 		}
 
 		log.info(String.format("Creating device on bus: %s address %s", deviceBus, deviceAddress));
 		return true;
 	}
-	
+
 	public void unsetController() {
 		controller = null;
 		controllerName = null;
@@ -202,12 +201,16 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 	}
 
 	public void setMuxBus(int busAddress) {
-		byte bus[] = new byte[1];
-		bus[0] = (byte) (1 << busAddress);
-		log.info(String.format("setMux this.deviceBus %s this.deviceAddress %s bus[0] %s", this.deviceBus, this.deviceAddress, bus[0]));
-		controller.i2cWrite(this, Integer.parseInt(this.deviceBus), Integer.decode(this.deviceAddress), bus, bus.length);
+		int lastBusAddress = -1;
+		if (busAddress != lastBusAddress) {
+			byte bus[] = new byte[1];
+			bus[0] = (byte) (1 << busAddress);
+			log.info(String.format("setMux this.deviceBus %s this.deviceAddress %s bus[0] %s", this.deviceBus, this.deviceAddress, bus[0]));
+			controller.i2cWrite(this, Integer.parseInt(this.deviceBus), Integer.decode(this.deviceAddress), bus, bus.length);
+			lastBusAddress = busAddress;
+		}
 	}
-	
+
 	@Override
 	public void i2cWrite(I2CControl control, int busAddress, int deviceAddress, byte[] buffer, int size) {
 		setMuxBus(busAddress);
@@ -215,21 +218,22 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 		log.debug(String.format("i2cWrite busAddress x%02X deviceAddress x%02X key %s", busAddress, deviceAddress, key));
 		controller.i2cWrite(this, Integer.parseInt(this.deviceBus), deviceAddress, buffer, size);
 	}
+
 	/**
-	 * TODO Add demuxing. i.e the route back to the caller
-	 *      The i2c will receive data that neeeds to be returned syncronous
-	 *      or asycncronus
+	 * TODO Add demuxing. i.e the route back to the caller The i2c will receive
+	 * data that neeeds to be returned syncronous or asycncronus
 	 */
 	@Override
 	public int i2cRead(I2CControl control, int busAddress, int deviceAddress, byte[] buffer, int size) {
 		setMuxBus(busAddress);
 		controller.i2cRead(this, Integer.parseInt(this.deviceBus), deviceAddress, buffer, size);
+		log.info(String.format("i2cRead. Requested %s bytes, received %s bytes", size, buffer.length));
 		return buffer.length;
 	}
+
 	/**
-	 * TODO Add demuxing. i.e the route back to the caller
-	 *      The i2c will receive data that neeeds to be returned syncronous
-	 *      or asycncronus
+	 * TODO Add demuxing. i.e the route back to the caller The i2c will receive
+	 * data that neeeds to be returned syncronous or asycncronus
 	 */
 	@Override
 	public int i2cWriteRead(I2CControl control, int busAddress, int deviceAddress, byte[] writeBuffer, int writeSize, byte[] readBuffer, int readSize) {
@@ -251,26 +255,26 @@ public class I2cMux extends Service implements I2CControl, I2CController {
 		ServiceType meta = new ServiceType(I2cMux.class.getCanonicalName());
 		meta.addDescription("Multiplexer for i2c to be able to use multiple i2c devices");
 		meta.addCategory("i2c", "control");
-    meta.setAvailable(true);
-    meta.setSponsor("Mats");
+		meta.setAvailable(true);
+		meta.setSponsor("Mats");
 		return meta;
 	}
 
 	@Override
 	public void setController(DeviceController controller) {
-		setController(controller);	
+		setController(controller);
 	}
 
 	@Override
 	public void deviceAttach(DeviceControl device, Object... conf) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deviceDetach(DeviceControl device) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
