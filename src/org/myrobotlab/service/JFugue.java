95a31d1c785f59454ca61fa5617d94114e5737ad
Mats <mats.onnerby@gmail.com>
2016-11-09 00:57:47 +0100

    Reverting JFugue to 4.0.3 because of an old compiler

--- src/org/myrobotlab/service/JFugue.java
+++ src/org/myrobotlab/service/JFugue.java
@@ -25,8 +25,8 @@
 
 package org.myrobotlab.service;
 
-import org.jfugue.player.Player;
-import org.jfugue.rhythm.Rhythm;
+import org.jfugue.Player;
+import org.jfugue.Rhythm;
 import org.myrobotlab.framework.Service;
 import org.myrobotlab.framework.ServiceType;
 import org.myrobotlab.logging.Level;
@@ -104,7 +104,7 @@ public class JFugue extends Service {
     ServiceType meta = new ServiceType(JFugue.class.getCanonicalName());
     meta.addDescription("service wrapping Jfugue, used for music and sound generation");
     meta.addCategory("sound");
-    meta.addDependency("org.jfugue.music", "5.0");
+    meta.addDependency("org.jfugue.music", "4.0.3");
     return meta;
   }
 
