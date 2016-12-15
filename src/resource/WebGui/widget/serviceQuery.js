b18ae70581520d391b8c0003d083f97ae5644035
GroG <grog@myrobotlab.org>
2016-12-15 23:15:54 +0100

    arduino test updates

--- src/resource/WebGui/widget/serviceQuery.js
+++ src/resource/WebGui/widget/serviceQuery.js
@@ -1,5 +1,5 @@
 angular.module('mrlapp.service').directive('serviceQuery', ['mrl', '$log', function(mrl, $log) {
-    var tpl = "<input type='text' ng-model='controllerName' class='form-control'\
+    var tpl = "<input type='text'  ng-model='controllerName' ng-disabled='disabled' class='form-control'\
     placeholder='attach'\
     uib-typeahead='controllerName as panel.name for panel in services | filter:{name:$viewValue}'\
     typeahead-min-length='0'\
@@ -11,27 +11,58 @@ angular.module('mrlapp.service').directive('serviceQuery', ['mrl', '$log', funct
         require: 'ngModel',
         // define isolated scope bindings with parent bindings
         scope: {
-            // serviceName: '@',
             interface: '@',
-            select: "&" // callback after a typeahead is selected
+            // interface to query
+            select: "&",
+            // callback after a typeahead is selected
+            disabled: '=ngDisabled'
         },
         controller: function($scope) {
+            if ($scope.disabled) {
+                var x = 1 + 1;
+            }
             $scope.onSelect = function(item, model, label) {
                 $scope.item = item;
                 $scope.controllerName = label;
                 $scope.model = model;
                 $scope.label = label;
                 $scope.selection = label;
+                // this concept is cool its the other side 
+                // of dependency injection - where you splat an object
+                // against a method signature and have the names sort the mess out ;)
                 $scope.select({
                     name: $scope.label
                 });
+                /*
+                 if($scope.disabled){
+                    $scope
+                }
+                */
             }
         },
         // build up typeahead with query to the registry for 
         // services which implement applicable interfaces
         link: function(scope, iElement, iAttrs, ngModelCtrl) {
+
+            // builds appropriate list of services which it can 'attach' to
             var interface = scope.interface;
             scope.services = mrl.getServicesFromInterface(interface);
+
+
+            scope.$watch(attrs.conditionalAutofocus, function(){
+                if (scope.$eval(attrs.disabled)) {
+                    element.focus();
+                }else{
+                    element.blur();
+                }
+            });
+            
+
+            if (scope.condition()) {
+                attrs.$set('autofocus', 'true');
+            }
+            if (scope.disabled) {//is disabled
+            }
         }
     };
 }
