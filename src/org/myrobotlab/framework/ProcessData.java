d78bd5ae48e4f28ea4d581aa83507e6101454954
GroG <supertick@gmail.com>
2017-05-03 11:31:05 -0700

    updates to restart

--- src/org/myrobotlab/framework/ProcessData.java
+++ src/org/myrobotlab/framework/ProcessData.java
@@ -15,219 +15,229 @@ import org.slf4j.Logger;
  *
  */
 public class ProcessData implements Serializable {
-  public final static Logger log = LoggerFactory.getLogger(ProcessData.class);
+	public final static Logger log = LoggerFactory.getLogger(ProcessData.class);
 
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  public static final String STATE_RUNNING = "running";
-  public static final String STATE_STOPPED = "stopped";
-  public static final String STATE_UNKNOWN = "unknown";
+	public static final String STATE_RUNNING = "running";
+	public static final String STATE_STOPPED = "stopped";
+	public static final String STATE_RESTARTING = "restarting";
+	public static final String STATE_UNKNOWN = "unknown";
 
-  // TODO - need to start using id
-  public Integer id;
-  public String branch;
-  public String name;
-  public String version;
-  public Long startTs = null;
-  public Long stopTs = null;
-  public String jarPath = null;
+	// TODO - need to start using id
+	public Integer id;
+	public String branch;
+	public String name;
+	public String version;
+	public Long startTs = null;
+	public Long stopTs = null;
+	public String jarPath = null;
 
-  public String javaExe = null;
+	public String javaExe = null;
 
-  public String jniLibraryPath = null;
-  public String jnaLibraryPath = null;
-  public String Xmx = null;
+	public String jniLibraryPath = null;
+	public String jnaLibraryPath = null;
+	public String Xmx = null;
 
-  boolean userDefinedServices = false;
-  
-  public String state = STATE_STOPPED; 
-  
-  transient public Process process;
-  transient public Monitor monitor;
-  static transient public Invoker service;
+	boolean userDefinedServices = false;
 
-  ArrayList<String> in = null;
+	public String state = STATE_STOPPED;
 
-  public static class Monitor extends Thread {
-    ProcessData data;
+	transient public Process process;
+	transient public Monitor monitor;
+	static transient public Invoker service;
 
-    public Monitor(ProcessData pd) {
-      super(String.format("%s.monitor", pd.name));
-      this.data = pd;
-    }
+	ArrayList<String> in = null;
 
-    @Override
-    public void run() {
-      try {
-        if (data.process != null) {
-          // data.isRunning = true;
-          data.state = STATE_RUNNING;
-          data.state = "running";
-          // don't wait if there is no agent
-          if (service != null) {
-        	  data.process.waitFor();
-          }
-        }
-      } catch (Exception e) {
-      }
+	public static class Monitor extends Thread {
+		ProcessData data;
 
-      data.state = STATE_STOPPED;
-      data.state = "stopped";
-      
-      if (ProcessData.service != null){
-    	  ProcessData.service.invoke("publishTerminated", data.id);
-      }
-      
-    }
+		public Monitor(ProcessData pd) {
+			super(String.format("%s.monitor", pd.name));
+			this.data = pd;
+		}
 
-  }
+		@Override
+		public void run() {
+			try {
+				if (data.process != null) {
+					// data.isRunning = true;
+					data.state = STATE_RUNNING;
+					data.state = "running";
+					// don't wait if there is no agent
+					if (service != null) {
+						data.process.waitFor();
+					}
+				}
+			} catch (Exception e) {
+			}
 
-  public ProcessData(Invoker service, Integer id, String branch, String version, String name, Process process) {
-    this.id = id;
-    ProcessData.service = service;
-    this.name = name;
-    this.branch = branch;
-    this.version = version;
-    this.process = process;
-  }
+			data.state = STATE_STOPPED;
+			data.state = "stopped";
 
-  /**
-   * copy of a ProcessData - threaded data will not be copied
-   * 
-   * @param pd
-   */
-  public ProcessData(ProcessData pd) {
-    this.id = pd.id;
-    this.name = pd.name;
-    this.branch = pd.branch;
-    this.version = pd.version;
+			if (ProcessData.service != null) {
+				ProcessData.service.invoke("publishTerminated", data.id);
+			}
 
-    this.javaExe = pd.javaExe;
-    this.jniLibraryPath = pd.jniLibraryPath;
-    this.version = pd.version;
-    this.jnaLibraryPath = pd.jnaLibraryPath;
+		}
 
-    this.userDefinedServices = pd.userDefinedServices;
+	}
 
-    if (pd.in != null) {
-      this.in = new ArrayList<String>();
-      for (int i = 0; i < pd.in.size(); ++i) {
-        this.in.add(pd.in.get(i));
-      }
-    }
+	public ProcessData(Invoker service, Integer id, String branch, String version, String name, Process process) {
+		this.id = id;
+		ProcessData.service = service;
+		this.name = name;
+		this.branch = branch;
+		this.version = version;
+		this.process = process;
+	}
 
-    // this.process = pd.process;
-    // this.startTs = System.currentTimeMillis();
-    // monitor = new Monitor(this);
-    // monitor.start();
-  }
+	/**
+	 * copy of a ProcessData - threaded data will not be copied
+	 * 
+	 * @param pd
+	 */
+	public ProcessData(ProcessData pd) {
+		this.id = pd.id;
+		this.name = pd.name;
+		this.branch = pd.branch;
+		this.version = pd.version;
 
-  /**
-   * FIXME - is too much catering to mrl execution ...
-   * 
-   * convert an String[] into a valid ProcessData
-   * 
-   * @param inCmdLine
-   * @param defaultBranch
-   * @param defaultVersion
-   */
-  public ProcessData(Invoker service, String jarPath, String[] inCmdLine, String defaultBranch, String defaultVersion) {
-	  ProcessData.service = service;
-	  this.jarPath = jarPath;
+		this.javaExe = pd.javaExe;
+		this.jniLibraryPath = pd.jniLibraryPath;
+		this.version = pd.version;
+		this.jnaLibraryPath = pd.jnaLibraryPath;
 
-    // String protectedDomain =
-    // URLDecoder.decode(Agent.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath(),
-    // "UTF-8");
-    // log.info("protected domain {}", protectedDomain);
+		this.userDefinedServices = pd.userDefinedServices;
 
-    // convert to ArrayList to process
-    in = new ArrayList<String>();
+		if (pd.in != null) {
+			this.in = new ArrayList<String>();
+			for (int i = 0; i < pd.in.size(); ++i) {
+				this.in.add(pd.in.get(i));
+			}
+		}
 
-    for (int i = 0; i < inCmdLine.length; ++i) {
-      String cmd = inCmdLine[i];
-      if (cmd.equals("-runtimeName")) {
-        name = inCmdLine[i + 1];
-        continue;
-      }
+		// this.process = pd.process;
+		// this.startTs = System.currentTimeMillis();
+		// monitor = new Monitor(this);
+		// monitor.start();
+	}
 
-      if (cmd.equals("-branch")) {
-        branch = inCmdLine[i + 1];
-        continue;
-      }
+	/**
+	 * FIXME - is too much catering to mrl execution ...
+	 * 
+	 * convert an String[] into a valid ProcessData
+	 * 
+	 * @param inCmdLine
+	 * @param defaultBranch
+	 * @param defaultVersion
+	 */
+	public ProcessData(Invoker service, String jarPath, String[] inCmdLine, String defaultBranch,
+			String defaultVersion) {
+		ProcessData.service = service;
+		this.jarPath = jarPath;
 
+		// String protectedDomain =
+		// URLDecoder.decode(Agent.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath(),
+		// "UTF-8");
+		// log.info("protected domain {}", protectedDomain);
 
-      if (cmd.equals("-service")) {
-        userDefinedServices = true;
-      }
+		// convert to ArrayList to process
+		in = new ArrayList<String>();
 
-      // additional parameters
-      in.add(inCmdLine[i]);
-    }
+		for (int i = 0; i < inCmdLine.length; ++i) {
+			String cmd = inCmdLine[i];
+			if (cmd.equals("-runtimeName")) {
+				name = inCmdLine[i + 1];
+				continue;
+			}
 
-    name = (name == null) ? "runtime" : name;
-    branch = (branch == null) ? defaultBranch : branch;
-    version = (version == null) ? defaultVersion : version;
+			if (cmd.equals("-branch")) {
+				branch = inCmdLine[i + 1];
+				continue;
+			}
 
-    // step 1 - get current env data
-    // String ps = File.pathSeparator;
-    String fs = File.separator;
+			if (cmd.equals("-service")) {
+				userDefinedServices = true;
+			}
 
-    Platform platform = Platform.getLocalInstance();
-    String exeName = platform.isWindows() ? "javaw" : "java";
+			// additional parameters
+			in.add(inCmdLine[i]);
+		}
 
-    javaExe = String.format("%s%sbin%s%s", System.getProperty("java.home"), fs, fs, exeName);
+		name = (name == null) ? "runtime" : name;
+		branch = (branch == null) ? defaultBranch : branch;
+		version = (version == null) ? defaultVersion : version;
 
-    jniLibraryPath = "-Djava.library.path=libraries/native";
-    jnaLibraryPath = "-Djna.library.path=libraries/native";
+		// step 1 - get current env data
+		// String ps = File.pathSeparator;
+		String fs = File.separator;
 
-  }
+		Platform platform = Platform.getLocalInstance();
+		String exeName = platform.isWindows() ? "javaw" : "java";
 
-  public boolean isRunning() {
-    return STATE_RUNNING.equals(state);
-  }
+		javaExe = String.format("%s%sbin%s%s", System.getProperty("java.home"), fs, fs, exeName);
 
-  public String[] buildCmdLine() {
-    ArrayList<String> cmd = new ArrayList<String>();
+		jniLibraryPath = "-Djava.library.path=libraries/native";
+		jnaLibraryPath = "-Djna.library.path=libraries/native";
 
-    cmd.add(javaExe);
+	}
 
-    cmd.add(jniLibraryPath);
-    cmd.add(jnaLibraryPath);
-    cmd.add("-cp");
+	public boolean isRunning() {
+		return STATE_RUNNING.equals(state);
+	}
 
-    // step 1 - get current env data
-    String ps = File.pathSeparator;
-    // bogus jython.jar added as a hack to support - jython's 'more' fragile
-    // 2.7.0 interface :(
-    // http://www.jython.org/archive/21/docs/registry.html
-    // http://bugs.jython.org/issue2355
-    String classpath = String.format("%s%s./libraries/jar/jython.jar%s./libraries/jar/*%s./bin%s./build/classes", jarPath,ps, ps, ps, ps);
-    cmd.add(classpath);
+	public String[] buildCmdLine() {
+		ArrayList<String> cmd = new ArrayList<String>();
 
-    cmd.add("org.myrobotlab.service.Runtime");
+		cmd.add(javaExe);
 
-    if (!userDefinedServices) {
-      cmd.add("-service");
-      // cmd.add("webgui");
-      // cmd.add("WebGui");
-      cmd.add("log");
-      cmd.add("Log");
-      cmd.add("cli");
-      cmd.add("Cli");
-      cmd.add("gui");
-      cmd.add("SwingGui");
-      cmd.add("python");
-      cmd.add("Python");
-    }
+		cmd.add(jniLibraryPath);
+		cmd.add(jnaLibraryPath);
+		cmd.add("-cp");
 
-    cmd.add("-fromAgent");
+		// step 1 - get current env data
+		String ps = File.pathSeparator;
+		// bogus jython.jar added as a hack to support - jython's 'more' fragile
+		// 2.7.0 interface :(
+		// http://www.jython.org/archive/21/docs/registry.html
+		// http://bugs.jython.org/issue2355
+		String classpath = String.format("%s%s./libraries/jar/jython.jar%s./libraries/jar/*%s./bin%s./build/classes",
+				jarPath, ps, ps, ps, ps);
+		cmd.add(classpath);
 
-    if (in != null) {
-      for (int i = 0; i < in.size(); ++i) {
-        cmd.add(in.get(i));
-      }
-    }
-    return cmd.toArray(new String[cmd.size()]);
-  }
+		cmd.add("org.myrobotlab.service.Runtime");
+
+		if (!userDefinedServices) {
+			cmd.add("-service");
+			// cmd.add("webgui");
+			// cmd.add("WebGui");
+			cmd.add("log");
+			cmd.add("Log");
+			cmd.add("cli");
+			cmd.add("Cli");
+			cmd.add("gui");
+			cmd.add("SwingGui");
+			cmd.add("python");
+			cmd.add("Python");
+		}
+
+		cmd.add("-fromAgent");
+
+		if (in != null) {
+			for (int i = 0; i < in.size(); ++i) {
+				cmd.add(in.get(i));
+			}
+		}
+		return cmd.toArray(new String[cmd.size()]);
+	}
+	
+	public void setRestarting(){
+		state = STATE_RESTARTING;
+	}
+
+	public boolean isRestarting() {
+		return state.equals(STATE_RESTARTING);
+	}
 
 }
