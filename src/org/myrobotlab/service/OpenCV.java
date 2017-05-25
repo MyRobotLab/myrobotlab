700bdfc1f31d205f435a314943356cd5b8dca7e4
kwatters <kwatters@kmwllc.com>
2017-05-25 17:36:39 -0400

    updating opencv to javacv 1.3

--- src/org/myrobotlab/service/OpenCV.java
+++ src/org/myrobotlab/service/OpenCV.java
@@ -893,7 +893,7 @@ public class OpenCV extends Service implements VideoSource {
 
 		meta.sharePeer("streamer", "streamer", "VideoStreamer", "Shared Video Streamer");
 		// meta.addDependency("org.bytedeco.javacpp","1.1");
-		meta.addDependency("org.bytedeco.javacv", "1.1");
+		meta.addDependency("org.bytedeco.javacv", "1.3");
 		meta.addDependency("pl.sarxos.webcam", "0.3.10");
 		return meta;
 	}
