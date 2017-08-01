ca78508cec131fd88efcdafbdb41c56351161e97
Anthony <moz4r@free.fr>
2017-08-01 02:04:46 +0200

    fix inmoov dep not updated

--- src/org/myrobotlab/service/InMoov.java
+++ src/org/myrobotlab/service/InMoov.java
@@ -1926,7 +1926,7 @@ public class InMoov extends Service {
     meta.addDescription("The InMoov service");
     meta.addCategory("robot");
     meta.addDependency("inmoov.fr", "1.0.0");
-    meta.addDependency("org.myrobotlab.inmoov", "0.4.3");
+    meta.addDependency("org.myrobotlab.inmoov", "0.4.3b");
 
     // SHARING !!! - modified key / actual name begin ------
     meta.sharePeer("head.arduino", "left", "Arduino", "shared left arduino");
