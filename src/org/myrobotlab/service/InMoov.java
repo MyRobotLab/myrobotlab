36bcd6436773368055a0273f09d6afbf59751103
Anthony <moz4r@free.fr>
2017-08-28 00:48:19 +0200

    repush without utf8 filenames inside inmoov.zip

--- src/org/myrobotlab/service/InMoov.java
+++ src/org/myrobotlab/service/InMoov.java
@@ -1872,7 +1872,7 @@ public class InMoov extends Service {
     meta.addDescription("The InMoov service");
     meta.addCategory("robot");
     meta.addDependency("inmoov.fr", "1.0.0");
-    meta.addDependency("org.myrobotlab.inmoov", "0.4.5");
+    meta.addDependency("org.myrobotlab.inmoov", "0.4.6");
 
     // SHARING !!! - modified key / actual name begin -------
     meta.sharePeer("head.arduino", "left", "Arduino", "shared left arduino");
