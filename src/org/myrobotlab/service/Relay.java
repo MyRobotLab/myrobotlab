374ce3a6b6b2f3c1de1a36cc85be024035860125
grog <grog@myrobotlab.org>
2017-09-02 19:20:26 -0700

    copy serviceData.json to deploy directory

--- src/org/myrobotlab/service/Relay.java
+++ src/org/myrobotlab/service/Relay.java
@@ -11,6 +11,11 @@ import org.slf4j.Logger;
 
 public class Relay extends Service {
 
+  public Arduino arduino;
+  public Integer pin;
+  public Integer onValue = 0;
+  public Integer offValue = 1;
+
   private static final long serialVersionUID = 1L;
 
   public final static Logger log = LoggerFactory.getLogger(Relay.class);
@@ -23,54 +28,40 @@ public class Relay extends Service {
 
     ServiceType meta = new ServiceType(Relay.class.getCanonicalName());
     meta.addDescription("Relay used by an arduino");
-    meta.setAvailable(true); 
+    meta.setAvailable(true);
     meta.addCategory("home automation");
     return meta;
+  }
+
+  public void on() {
+    if (arduino.isConnected() && pin != null && onValue != null) {
+      arduino.pinMode(pin, Arduino.OUTPUT);
+      arduino.digitalWrite(pin, onValue);
+      log.info("Relay " + this.getName() + " ON");
+    } else {
+      log.error(this.getName() + " error");
+    }
+  }
+
+  public void off() {
+    if (arduino.isConnected() && pin != null && offValue != null) {
+      arduino.pinMode(pin, Arduino.OUTPUT);
+      arduino.digitalWrite(pin, offValue);
+      log.info("Relay " + this.getName() + " OFF");
+    } else {
+      log.error(this.getName() + " error");
+    }
   }
 
   public static void main(String[] args) {
     try {
 
       LoggingFactory.init(Level.INFO);
-
       Runtime.start("relay", "Relay");
 
     } catch (Exception e) {
       Logging.logError(e);
     }
   }
-  
-  public Arduino arduino;
-  public Integer pin;
-  public Integer onValue=0;
-  public Integer offValue=1;
-  
-  
-  public void on() {
-    if (arduino.isConnected() && pin!=null && onValue!=null)
-    {
-    arduino.pinMode(pin, Arduino.OUTPUT);
-    arduino.digitalWrite(pin,onValue); 
-    log.info("Relay "+this.getName()+" ON");
-    }
-    else
-    {
-      log.error(this.getName()+" error");  
-    }
-  }
-  
-  public void off() {
-    if (arduino.isConnected() && pin!=null && offValue!=null)
-    {
-      arduino.pinMode(pin, Arduino.OUTPUT);
-      arduino.digitalWrite(pin,offValue); 
-      log.info("Relay "+this.getName()+" OFF");
-    }
-    else
-    {
-      log.error(this.getName()+" error");  
-    }
-  }  
-  
 
 }
