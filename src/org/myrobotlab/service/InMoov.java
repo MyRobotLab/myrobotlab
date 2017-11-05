448345c7003e4bd1f0cfa2c58e148b59dc9401c7
Anthony <moz4r@free.fr>
2017-11-05 13:53:29 +0100

    Update InMoov repo 0.5.9

--- src/org/myrobotlab/service/InMoov.java
+++ src/org/myrobotlab/service/InMoov.java
@@ -1904,7 +1904,7 @@ public class InMoov extends Service {
     meta.addDescription("The InMoov service");
     meta.addCategory("robot");
     meta.addDependency("inmoov.fr", "1.0.0");
-    meta.addDependency("org.myrobotlab.inmoov", "0.5.8");
+    meta.addDependency("org.myrobotlab.inmoov", "0.5.9");
 
     // SHARING !!! - modified key / actual name begin -------
     meta.sharePeer("head.arduino", "left", "Arduino", "shared left arduino");
