b7fbf438e435593a71a89d5b6bba67f8fa30e81f
Mats <mats.onnerby@gmail.com>
2017-11-06 19:02:14 +0100

    RasPi.
    Updated to support I2cMux again.
    Added basic structure to act as a PinArray.

--- src/org/myrobotlab/service/RasPi.java
+++ src/org/myrobotlab/service/RasPi.java
@@ -3,7 +3,11 @@ package org.myrobotlab.service;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.myrobotlab.framework.Platform;
 import org.myrobotlab.framework.Service;
@@ -13,11 +17,19 @@ import org.myrobotlab.logging.Level;
 import org.myrobotlab.logging.LoggerFactory;
 import org.myrobotlab.logging.Logging;
 import org.myrobotlab.logging.LoggingFactory;
+import org.myrobotlab.service.data.PinData;
 import org.myrobotlab.service.interfaces.I2CControl;
 import org.myrobotlab.service.interfaces.I2CController;
+import org.myrobotlab.service.interfaces.PinArrayControl;
+import org.myrobotlab.service.interfaces.PinArrayListener;
+import org.myrobotlab.service.interfaces.PinDefinition;
+import org.myrobotlab.service.interfaces.PinListener;
 import org.slf4j.Logger;
 
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalMultipurpose;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinMode;
@@ -27,6 +39,7 @@ import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.wiringpi.I2C;
 import com.pi4j.wiringpi.SoftPwm;
+import com.pi4j.system.*;
 
 /**
  * 
@@ -37,13 +50,22 @@ import com.pi4j.wiringpi.SoftPwm;
  * 
  */
 // TODO Ensure that only one instance of RasPi can execute on each RaspBerry PI
-public class RasPi extends Service implements I2CController {
+public class RasPi extends Service implements I2CController, PinArrayControl {
 
   public static class I2CDeviceMap {
     public I2CBus bus;
     public I2CDevice device;
     public String serviceName;
     public int deviceHandle;
+  }
+
+  public static class GpioPinListener implements GpioPinListenerDigital {
+    @Override
+    public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+      // display pin state on console
+      log.info(String.format(" --> GPIO PIN STATE CHANGE: %s = %s", event.getPin(), event.getState()));
+    }
+
   }
 
   private boolean wiringPi = false; // Defined to be able to switch between
@@ -54,11 +76,30 @@ public class RasPi extends Service implements I2CController {
                                     // Repeated start is used by Mpr121 and
                                     // may be needed by other devices
 
+  transient Map<String, PinArrayListener> pinArrayListeners = new ConcurrentHashMap<String, PinArrayListener>();
+
+  /**
+   * address index of pinList
+   */
+  Map<Integer, PinDefinition> pinIndex = null;
+
+  /**
+   * name index of pinList
+   */
+  Map<String, PinDefinition> pinMap = null;
+
+  /**
+   * map of pin listeners
+   */
+  transient Map<Integer, List<PinListener>> pinListeners = new ConcurrentHashMap<Integer, List<PinListener>>();
+
+  public static final int INPUT = 0x0;
+  public static final int OUTPUT = 0x1;
+
   private static final long serialVersionUID = 1L;
 
   public final static Logger log = LoggerFactory.getLogger(RasPi.class.getCanonicalName());
 
-  // the 2 pins for I2C on the raspberry
   GpioController gpio;
 
   GpioPinDigitalOutput gpio01;
@@ -111,20 +152,22 @@ public class RasPi extends Service implements I2CController {
     if ("arm".equals(platform.getArch()) || "armv7.hfp".equals(platform.getArch())) {
       log.info("Executing on Raspberry PI");
       // init gpio
+      log.info("Initiating GPIO");
+      gpio = GpioFactory.getInstance();
+      log.info("GPIO Initiated");
+      // TODO Check if the is correct. I don't think it is /Mats // GPIO pins
       /*
-       * log.info("Initiating GPIO"); gpio = GpioFactory.getInstance();
-       * log.info("GPIO Initiated");
-       *
-       * 
-       * // TODO Check if the is correct. I don't think it is /Mats // GPIO pins
-       * should be provisioned in the CreateDevice /* gpio01 =
-       * gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01); gpio03 =
+       * should be provisioned in the CreateDevice /* gpio01
+       * =gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01); gpio03 =
        * gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03);
        */
     } else {
       // we should be running on a Raspberry Pi
       log.error("architecture is not arm");
     }
+
+    getPinList();
+
   }
 
   @Override
@@ -214,13 +257,14 @@ public class RasPi extends Service implements I2CController {
 
   @Override
   public void i2cWrite(I2CControl control, int busAddress, int deviceAddress, byte[] buffer, int size) {
-    String key = String.format("%d.%d", busAddress, deviceAddress);
+    
+    String key = String.format("%d.%d", busAddress, deviceAddress);    
     I2CDeviceMap devicedata = i2cDevices.get(key);
     if (devicedata == null) {
-      log.error(String.format("No device data found for key %s", key));
-      log.error(String.format("Available devices: %s", i2cDevices.toString()));
-    }
-
+      createI2cDevice(busAddress, deviceAddress, control.getName());
+      devicedata = i2cDevices.get(key);
+    } 
+    
     if (wiringPi) {
       int reg = buffer[0] & 0xFF;
       for (int i = 1; i < size; i++) {
@@ -243,6 +287,11 @@ public class RasPi extends Service implements I2CController {
     int bytesRead = 0;
     String key = String.format("%d.%d", busAddress, deviceAddress);
     I2CDeviceMap devicedata = i2cDevices.get(key);
+    if (devicedata == null) {
+      createI2cDevice(busAddress, deviceAddress, control.getName());
+      devicedata = i2cDevices.get(key);
+    }
+    
     if (wiringPi) {
       for (int i = 0; i < size; i++) {
         buffer[i] = (byte) (I2C.wiringPiI2CRead(devicedata.deviceHandle) & 0xFF);
@@ -266,6 +315,11 @@ public class RasPi extends Service implements I2CController {
     }
     String key = String.format("%d.%d", busAddress, deviceAddress);
     I2CDeviceMap devicedata = i2cDevices.get(key);
+    if (devicedata == null) {
+      createI2cDevice(busAddress, deviceAddress, control.getName());
+      devicedata = i2cDevices.get(key);
+    }
+    
     if (wiringPi) {
       for (int i = 0; i < readSize; i++) {
         readBuffer[i] = (byte) (I2C.wiringPiI2CReadReg8(devicedata.deviceHandle, (writeBuffer[0] + i) & 0xFF));
@@ -310,33 +364,42 @@ public class RasPi extends Service implements I2CController {
     // This part adds the service to the mapping between
     // busAddress||DeviceAddress
     // and the service name to be able to send data back to the invoker
+    
     String key = String.format("%d.%d", Integer.parseInt(control.getDeviceBus()), Integer.decode(control.getDeviceAddress()));
-    I2CDeviceMap devicedata = new I2CDeviceMap();
+
     if (i2cDevices.containsKey(key)) {
       log.error(String.format("Device %s %s %s already exists.", control.getDeviceBus(), control.getDeviceAddress(), control.getName()));
     } else {
+      createI2cDevice(Integer.parseInt(control.getDeviceBus()), Integer.decode(control.getDeviceAddress()), control.getName());
+      control.attachI2CController(this);
+    }
+  }
+  
+  void createI2cDevice(int bus, int address, String serviceName){
+    
+    String key = String.format("%d.%d", bus, address);
+    I2CDeviceMap devicedata = new I2CDeviceMap();
+    if (!i2cDevices.containsKey(key)) {
       try {
         if (wiringPi) {
-          int deviceHandle = I2C.wiringPiI2CSetup(Integer.decode(control.getDeviceAddress()));
-          devicedata.serviceName = control.getName();
+          int deviceHandle = I2C.wiringPiI2CSetup(address);
+          devicedata.serviceName = serviceName;
           devicedata.bus = null;
           devicedata.device = null;
           devicedata.deviceHandle = deviceHandle;
         } else {
-          I2CBus bus = I2CFactory.getInstance(Integer.parseInt(control.getDeviceBus()));
-          I2CDevice device = i2c.getDevice(Integer.decode(control.getDeviceAddress()));
-          devicedata.serviceName = control.getName();
-          devicedata.bus = bus;
+          I2CBus i2cBus = I2CFactory.getInstance(bus);
+          I2CDevice device = i2cBus.getDevice(address);
+          devicedata.serviceName = serviceName;
+          devicedata.bus = i2cBus;
           devicedata.device = device;
           devicedata.deviceHandle = -1;
         }
-        log.info(String.format("Created device for %s key %s", control.getName(), key));
+        i2cDevices.put(key, devicedata);
+        log.info(String.format("Created device for %s key %s", serviceName, key));
       } catch (NumberFormatException | IOException e) {
         Logging.logError(e);
       }
-
-      i2cDevices.put(key, devicedata);
-      control.attachI2CController(this);
     }
   }
 
@@ -373,5 +436,203 @@ public class RasPi extends Service implements I2CController {
    */
   public boolean getWiringPi() {
     return wiringPi;
+  }
+
+  /*
+   * Start of PinArray methods
+   */
+
+  @Override
+  public List<PinDefinition> getPinList() {
+
+    // If the pinIndex is populated already, return it's values
+    if (pinIndex != null) {
+      return new ArrayList<PinDefinition>(pinIndex.values());
+    }
+
+    pinMap = new TreeMap<String, PinDefinition>();
+    pinIndex = new TreeMap<Integer, PinDefinition>();
+    List<PinDefinition> pinList = new ArrayList<PinDefinition>();
+
+    try {
+      // if (SystemInfo.getBoardType() == SystemInfo.BoardType.RaspberryPi_3B) {
+      if (true) {
+        for (int i = 0; i < 32; ++i) {
+          PinDefinition pindef = new PinDefinition(getName(), i);
+          String pinName = null;
+          if (i == 16) {
+            pindef.setRx(true);
+          }
+          if (i == 15) {
+            pindef.setTx(true);
+          }
+          if (i <= 16 || i >= 21) {
+            pinName = String.format("GPIO%d", i);
+            pindef.setDigital(true);
+          } else {
+            pinName = String.format("Unused%d", i);
+            pindef.setDigital(false);
+          }
+          pindef.setPinName(pinName);
+          pindef.setAddress(i);
+          pinIndex.put(i, pindef);
+          pinMap.put(pinName, pindef);
+          pinList.add(pindef);
+        }
+      } else {
+        log.error(String.format("Unknown boardtype %s", SystemInfo.getBoardType()));
+      }
+    } catch (UnsupportedOperationException | IOException | InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return pinList;
+  }
+
+  @Override
+  public PinDefinition getPin(String pinName) {
+    if (pinMap.containsKey(pinName)) {
+      return pinMap.get(pinName);
+    }
+    return null;
+  }
+
+  @Override
+  public PinDefinition getPin(Integer address) {
+    if (pinIndex.containsKey(address)) {
+      return pinIndex.get(address);
+    }
+    return null;
+  }
+
+  @Override
+  public int read(Integer address) {
+    return pinIndex.get(address).getValue();
+  }
+
+  @Override
+  public int read(String pinName) {
+    return read(pinNameToAddress(pinName));
+  }
+
+  public Integer pinNameToAddress(String pinName) {
+    return pinMap.get(pinName).getAddress();
+  }
+
+  @Override
+  public void pinMode(Integer address, String mode) {
+    
+    if (mode != null && mode.equalsIgnoreCase("INPUT")) {
+      pinMode(address, INPUT);
+    } else {
+      pinMode(address, OUTPUT);
+    }
+  }
+
+  public void pinMode(int pin, int mode) {
+
+    PinDefinition pinDef = pinIndex.get(pin);
+    if (mode == INPUT) {
+      pinDef.setGpioPin(gpio.provisionDigitalMultipurposePin(RaspiPin.getPinByAddress(pin), PinMode.DIGITAL_INPUT));
+    } else {
+      pinDef.setGpioPin(gpio.provisionDigitalMultipurposePin(RaspiPin.getPinByAddress(pin), PinMode.DIGITAL_OUTPUT));
+    }
+    invoke("publishPinDefinition", pinDef);
+  }
+
+  @Override
+  public void write(Integer address, Integer value) {
+
+    PinDefinition pinDef = pinIndex.get(address);
+    pinMode(address, Arduino.OUTPUT);
+    digitalWrite(address, value);
+    // cache value
+    pinDef.setValue(value);
+  }
+
+  public void digitalWrite(int pin, int value) {
+
+    log.info("digitalWrite {} {}", pin, value);
+    // msg.digitalWrite(pin, value);
+    PinDefinition pinDef = pinIndex.get(pin);
+    if (value == 0) {
+      pinDef.getGpioPin().low();
+    } else {
+      pinDef.getGpioPin().high();
+    }
+    invoke("publishPinDefinition", pinDef);
+  }
+
+  @Override
+  public PinData publishPin(PinData pinData) {
+    // TODO Make sure this method is invoked when a pin value interrupt is
+    // received
+    // caching last value
+    pinIndex.get(pinData.address).setValue(pinData.value);
+    return pinData;
+  }
+
+  @Override
+  public PinData[] publishPinArray(PinData[] pinData) {
+    return pinData;
+  }
+
+  @Override
+  public void attach(PinListener listener, Integer pinAddress) {
+    String name = listener.getName();
+
+    if (listener.isLocal()) {
+      List<PinListener> list = null;
+      if (pinListeners.containsKey(pinAddress)) {
+        list = pinListeners.get(pinAddress);
+      } else {
+        list = new ArrayList<PinListener>();
+      }
+      list.add(listener);
+      pinListeners.put(pinAddress, list);
+
+    } else {
+      addListener("publishPin", name, "onPin");
+    }
+  }
+
+  @Override
+  public void attach(PinArrayListener listener) {
+    pinArrayListeners.put(listener.getName(), listener);
+  }
+
+  @Override
+  public void enablePin(Integer address) {
+    enablePin(address, 0);
+  }
+
+  @Override
+  public void disablePin(Integer address) {
+
+    PinDefinition pin = pinIndex.get(address);
+    pin.getGpioPin().removeListener();
+    PinDefinition pinDef = pinIndex.get(address);
+    invoke("publishPinDefinition", pinDef);
+  }
+
+  @Override
+  public void disablePins() {
+    for (int i = 0; i < pinIndex.size(); i++){
+      PinDefinition pin = pinIndex.get(i);
+      if (pin.getGpioPin() != null){
+        pin.getGpioPin().removeListener();
+        pin.setEnabled(false);
+      }
+    }
+  }
+
+  @Override
+  public void enablePin(Integer address, Integer rate) {
+    
+    PinDefinition pin = pinIndex.get(address);
+    pin.getGpioPin().addListener(new GpioPinListener());
+    pin.setEnabled(true);
+    invoke("publishPinDefinition", pin); // broadcast pin change
   }
 }
