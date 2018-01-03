2208bd3f9492abdde729f3f61398d132cb091510
kwatters <kwatters@kmwllc.com>
2018-01-03 17:08:13 -0500

    cleaning up logger dependencies

--- src/main/java/org/myrobotlab/logging/LoggingSLF4J.java
+++ src/main/java/org/myrobotlab/logging/LoggingSLF4J.java
@@ -2,7 +2,6 @@ package org.myrobotlab.logging;
 
 import java.nio.charset.StandardCharsets;
 
-import org.apache.log4j.DailyRollingFileAppender;
 import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.Level;
