39ead02f30a150a4af847964dd641854f9cbba14
U-DESKTOP-CLDMSHG\kwatters <kwatters@kmwllc.com>
2018-02-17 22:06:21 -0500

    fixing compilation error against different myo thalamic dependency

--- src/main/java/org/myrobotlab/service/MyoThalmic.java
+++ src/main/java/org/myrobotlab/service/MyoThalmic.java
@@ -470,12 +470,10 @@ public class MyoThalmic extends Service implements DeviceListener, MyoDataListen
     }
   }
 
-@Override
-public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection, float rotation,
-		WarmupState warmupState) {
-	log.info("onArmSync {}", arm);
-    whichArm = arm;
-    invoke("publishArmSync", arm);
-}
-
+  @Override
+  public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection, WarmupState warmupState) {
+	  log.info("onArmSync {}", arm);
+	  whichArm = arm;
+	  invoke("publishArmSync", arm);
+  }
 }
