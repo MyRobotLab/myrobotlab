0379ad11928468f5a32157ca08a4cadb11c0892d
U-DESKTOP-CLDMSHG\kwatters <kwatters@kmwllc.com>
2018-03-02 20:27:50 -0500

    attach yolo / opencv to solr for memory.  this conflicts with the inbox/outbox approach.. i know.. we'll refactor as this stuff gets more mature.

--- src/test/java/org/myrobotlab/integration/YoloFilterTest.java
+++ src/test/java/org/myrobotlab/integration/YoloFilterTest.java
@@ -1,14 +1,18 @@
 package org.myrobotlab.integration;
 
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrServerException;
 import org.myrobotlab.logging.Level;
 import org.myrobotlab.logging.LoggingFactory;
 import org.myrobotlab.opencv.OpenCVFilterYolo;
 import org.myrobotlab.service.OpenCV;
 import org.myrobotlab.service.Runtime;
+import org.myrobotlab.service.Solr;
 
 public class YoloFilterTest {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SolrServerException, IOException {
     // TODO Auto-generated method stub
 
     
@@ -16,14 +20,22 @@ public class YoloFilterTest {
     org.apache.log4j.BasicConfigurator.configure();
     LoggingFactory.getInstance().setLevel(Level.INFO);
     
+    
+    Solr solr = (Solr)Runtime.createAndStart("solr", "Solr");
+    solr.startEmbedded();
+    
     OpenCV opencv = (OpenCV) Runtime.start("opencv", "OpenCV");
     opencv.setStreamerEnabled(false);
+    opencv.setCameraIndex(0);
+    
 
     OpenCVFilterYolo yolo = new OpenCVFilterYolo("yolo");
     opencv.addFilter(yolo);
 
+    solr.attach(opencv);
     
-    opencv.setCameraIndex(0);
+    
+    
     opencv.capture();
     
   }
