adfd3905180923aa524ec363fc6e50d4096865c4
supertick <GRPERRY@CTNAL0051220979.us164.corpintra.net>
2018-05-23 06:44:24 -0700

    stealing all of kwatters work for my own encoder ! :)

--- src/main/java/org/myrobotlab/service/interfaces/EncoderController.java
+++ src/main/java/org/myrobotlab/service/interfaces/EncoderController.java
@@ -6,9 +6,13 @@ import org.myrobotlab.sensor.EncoderData;
 public interface EncoderController extends Attachable {
 	// > encoderAttach/deviceId/pin
 	public void attach(EncoderControl control, Integer pin) throws Exception;
-  // > setZeroPoint/deviceId
-  public void setZeroPoint(Integer deviceId);
-  // < publishEncoderPosition/deviceId/b16 position
+
+	// > setZeroPoint/deviceId
+	// public void setZeroPoint(Integer deviceId);
+
+	// < publishEncoderPosition/deviceId/b16 position
 	public EncoderData publishEncoderPosition(EncoderData data);
+
+	public void setZeroPoint(EncoderController encoderController);
 
 }
