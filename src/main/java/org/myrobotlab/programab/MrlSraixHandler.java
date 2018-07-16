9f3295a97bd7203d939c5faacebf7b6a56d39ee5
U-DESKTOP-CLDMSHG\kwatters <kwatters@kmwllc.com>
2018-07-15 21:18:48 -0400

    ok.. this commit changes programab behavior.  now, MRL is in control of all SRAIX tags as defined/implemented in the MrlSraixHandler.java class.

---
+++ src/main/java/org/myrobotlab/programab/MrlSraixHandler.java
@@ -0,0 +1,13 @@
+package org.myrobotlab.programab;
+
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.SraixHandler;
+
+public class MrlSraixHandler implements SraixHandler {
+
+  @Override
+  public String sraix(Chat chatSession, String input, String defaultResponse, String hint, String host, String botid, String apiKey, String limit) {
+    return "Not implemented yet";
+  }
+
+}
