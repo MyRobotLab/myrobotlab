b2f0567e5be4a5c80af4fc27589ed2caeb04f624
grperry <grperry@CTNAL0051220979.us164.corpintra.net>
2018-09-18 06:39:41 -0700

    Low level Services should be unaware of high level Services #264

--- src/main/java/org/myrobotlab/service/InMoov.java
+++ src/main/java/org/myrobotlab/service/InMoov.java
@@ -1174,8 +1174,6 @@ public class InMoov extends Service {
     }
 
     arm.arduino.setBoard(type);
-    arm.arduino.usedByInmoov = true;
-    arm.arduino.serial.usedByInmoov = true;
     arm.connect(port);
     arduinos.put(port, arm.arduino);
 
@@ -1222,8 +1220,6 @@ public class InMoov extends Service {
     }
 
     hand.arduino.setBoard(type);
-    hand.arduino.serial.usedByInmoov = true;
-    hand.arduino.usedByInmoov = true;
     hand.connect(port);
     arduinos.put(port, hand.arduino);
     return hand;
@@ -1254,8 +1250,6 @@ public class InMoov extends Service {
     }
 
     head.arduino.setBoard(type);
-    head.arduino.usedByInmoov = true;
-    head.arduino.serial.usedByInmoov = true;
     head.connect(port, headYPin, headXPin, eyeXPin, eyeYPin, jawPin, rollNeckPin);
     arduinos.put(port, head.arduino);
     return head;
@@ -1501,8 +1495,6 @@ public class InMoov extends Service {
     }
 
     torso.arduino.setBoard(type);
-    torso.arduino.usedByInmoov = true;
-    torso.arduino.serial.usedByInmoov = true;
     torso.connect(port);
     arduinos.put(port, torso.arduino);
 
@@ -1534,8 +1526,6 @@ public class InMoov extends Service {
     }
 
     eyelidsArduino.setBoard(type);
-    eyelidsArduino.usedByInmoov = true;
-    eyelidsArduino.serial.usedByInmoov = true;
     eyelidsArduino.connect(port);
     if (!eyelidsArduino.isConnected()) {
       error("arduino %s not connected", eyelidsArduino.getName());
