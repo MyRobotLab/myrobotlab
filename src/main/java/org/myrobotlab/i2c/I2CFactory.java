8dac14bdbbf8b9e8187d37a7f2f7a5be29373d90
Anthony <moz4r@free.fr>
2018-09-21 00:28:54 +0200

    desperate fixes to get raspi + i2c worky again https://github.com/MyRobotLab/myrobotlab/issues/262

--- src/main/java/org/myrobotlab/i2c/I2CFactory.java
+++ src/main/java/org/myrobotlab/i2c/I2CFactory.java
@@ -1,12 +1,21 @@
 package org.myrobotlab.i2c;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.myrobotlab.framework.Platform;
 
 import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
+import com.pi4j.io.i2c.I2CFactoryProvider;
+import com.pi4j.io.i2c.impl.I2CProviderImpl;
 
 public class I2CFactory {
+  
+  public static final long DEFAULT_LOCKAQUIRE_TIMEOUT = 1000;
+  public static final TimeUnit DEFAULT_LOCKAQUIRE_TIMEOUT_UNITS = TimeUnit.MILLISECONDS;
+  
+  volatile static I2CFactoryProvider provider = new I2CProviderImpl();
 
   /**
    * Create new I2CBus instance
@@ -42,7 +51,14 @@ public class I2CFactory {
       // TODO: fix this!!!
       //log.warn("This probable doesn't work for ARM / RasPI now.. update the code!");
       //return I2CBusImpl.getBus(busNumber);
-      return null;
+      //return null;
+      try {
+        return provider.getBus(busNumber, DEFAULT_LOCKAQUIRE_TIMEOUT, DEFAULT_LOCKAQUIRE_TIMEOUT_UNITS);
+      } catch (UnsupportedBusNumberException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+        return null;
+      }
     } else {
       return I2CProxyImpl.getBus(busNumber);
     }
