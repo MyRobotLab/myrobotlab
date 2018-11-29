e561d15044aa87e39a12119b6ffd7a5efa78e122
Anthony <moz4r@free.fr>
2018-11-29 16:47:04 +0100

    polish

--- src/main/java/org/myrobotlab/inmoov/Utils.java
+++ src/main/java/org/myrobotlab/inmoov/Utils.java
@@ -2,7 +2,7 @@ package org.myrobotlab.inmoov;
 
 import java.io.File;
 import java.io.IOException;
-
+import org.apache.commons.io.FilenameUtils;
 import org.myrobotlab.io.FileIO;
 import org.myrobotlab.logging.LoggerFactory;
 import org.myrobotlab.service.Python;
@@ -16,21 +16,30 @@ public class Utils {
   /**
    * This method will load a python file into the python interpreter. 
    */
-  public static boolean loadPythonFile(String file) {
+  public static boolean loadFile(String file, String type) {
+    if (!FilenameUtils.getExtension(file).equalsIgnoreCase(type)) {
+      log.warn("{} is not a {} file", file, type);
+      return false;
+    }
     File f = new File(file);
     Python p = (Python) Runtime.getService("python");
     log.info("Loading  Python file {}", f.getAbsolutePath());
+    if (p == null) {
+      log.error("Python instance not found");
+      return false;
+    }
     String script = null;
     try {
       script = FileIO.toString(f.getAbsolutePath());
     } catch (IOException e) {
-      log.error("IO Error loading file");
+      log.error("IO Error loading file : {}",e);
       return false;
     }
     // evaluate the scripts in a blocking way.
     boolean result = p.exec(script, true, true);
     if (!result) {
       log.error("Error while loading file {}", f.getAbsolutePath());
+      return false;
     } else {
       log.info("Successfully loaded {}", f.getAbsolutePath());
     }
