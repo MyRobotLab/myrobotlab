969efbea2f12b4cbd8d9ec5463b1b7a98106baa6
kwatters <kwatters@kmwllc.com>
2019-01-29 16:00:27 -0500

    fixing dependencies to support leap motion on 64 bit.  (the libraries will overwrite each other if we unzip both 32 and 64 bit at the same time.)

--- src/main/java/org/myrobotlab/service/LeapMotion.java
+++ src/main/java/org/myrobotlab/service/LeapMotion.java
@@ -210,6 +210,16 @@ public class LeapMotion extends Service implements LeapDataListener, LeapDataPub
     meta.addDescription("Leap Motion Service");
     meta.addCategory("sensor", "telerobotics");
     meta.addDependency("leapmotion", "leap", "2.1.3");
+
+    // TODO: These will overwrite each other!  we need to be selective for the platform of what we deploy.
+    // I believe the 32bit libraries would overwrite the 64bit libraries. 
+    // meta.addDependency("leapmotion", "leap-linux32", "2.1.3", "zip");
+    // meta.addDependency("leapmotion", "leap-win32", "2.1.3", "zip");
+    // 64 bit support only for now.  until we can switch out dependencies based on the current platform.
+    meta.addDependency("leapmotion", "leap-win64", "2.1.3", "zip");
+    meta.addDependency("leapmotion", "leap-mac64", "2.1.3", "zip");
+    meta.addDependency("leapmotion", "leap-linux64", "2.1.3", "zip");
+    
     return meta;
   }
 
