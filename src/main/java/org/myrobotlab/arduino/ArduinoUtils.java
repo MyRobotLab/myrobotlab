9b86be634f642a0b292e3853538e98608fdc6801
Kevin Watters <kwatters@kmwllc.com>
2019-02-23 14:56:16 -0500

    more cleanup of file paths

--- src/main/java/org/myrobotlab/arduino/ArduinoUtils.java
+++ src/main/java/org/myrobotlab/arduino/ArduinoUtils.java
@@ -77,11 +77,11 @@ public class ArduinoUtils {
    */
   public static boolean uploadSketch(String port, String boardKey, String arduinoPath) throws IOException, InterruptedException {
     FileIO.extractResources();
-    String sketchFilename = Util.getResourceDir() + "/Arduino/MRLComm/MRLComm.ino";
+    String sketchFilename = Util.getResourceDir() + File.separator + "Arduino"+File.separator+"MRLComm"+File.separator+"MRLComm.ino";
     File sketch = new File(sketchFilename);
     if (!sketch.exists()) {
       // trying to use development version
-      sketchFilename = "src/main/resources/resource/Arduino/MRLComm/MRLComm.ino";
+      sketchFilename = "src"+File.separator+"main"+File.separator+"resources"+File.separator+"resource"+File.separator+"Arduino"+File.separator+"MRLComm"+File.separator+"MRLComm.ino";
       sketch = new File(sketchFilename);
     }
     // Create the command to run (and it's args.)
