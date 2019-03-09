1a3c5c94d2b2aa8206c2b432cf2b8c2b32a16a6f
GroG <grog@myrobotlab.org>
2019-03-08 22:05:11 -0800

    trying to diagnose service smoke test error

--- src/main/java/org/myrobotlab/service/WebGui.java
+++ src/main/java/org/myrobotlab/service/WebGui.java
@@ -1,5 +1,6 @@
 package org.myrobotlab.service;
 
+import java.awt.GraphicsEnvironment;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -63,11 +64,6 @@ import org.myrobotlab.service.interfaces.AuthorizationProvider;
 import org.myrobotlab.service.interfaces.Gateway;
 //import org.myrobotlab.webgui.WebGUIServlet;
 import org.slf4j.Logger;
-
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.AsyncHttpClientConfig;
-import com.ning.http.client.providers.netty.NettyAsyncHttpProvider;
-import com.ning.http.client.providers.netty.NettyAsyncHttpProviderConfig;
 
 /**
  * 
@@ -971,7 +967,7 @@ public class WebGui extends Service implements AuthorizationProvider, Gateway, H
       subscribe(runtime.getName(), "registered");
       subscribe(runtime.getName(), "released");
 
-      if (autoStartBrowser) {
+      if (autoStartBrowser && GraphicsEnvironment.isHeadless()) {
         log.info("auto starting default browser");
         BareBonesBrowserLaunch.openURL(String.format(startURL, port));
       }
