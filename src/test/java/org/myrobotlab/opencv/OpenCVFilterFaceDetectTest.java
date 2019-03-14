64e571e653078d7107c81f2d887921f2fd470e83
kwatters <kwatters@kmwllc.com>
2019-03-14 11:58:34 -0400

    some initial commits for cv filter unit testing.

---
+++ src/test/java/org/myrobotlab/opencv/OpenCVFilterFaceDetectTest.java
@@ -0,0 +1,39 @@
+package org.myrobotlab.opencv;
+
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.junit.Before;
+import org.junit.Assert;
+
+public class OpenCVFilterFaceDetectTest extends AbstractOpenCVFilterTest {
+
+  
+  @Before
+  public void before() {
+    debug = false;
+  }
+  
+  @Override
+  public OpenCVFilter createFilter() {
+    // create our test filter.
+    OpenCVFilterFaceDetect filter = new OpenCVFilterFaceDetect("facedetect");
+    return filter;
+  }
+
+  @Override
+  public IplImage createTestImage() {
+    // TODO Auto-generated method stub
+    // let's use lena.png.
+    IplImage input = defaultImage();
+    return input;
+  }
+
+  @Override
+  public void verify(OpenCVFilter filter, IplImage input, IplImage output) {
+    // it should have found 1 face
+    OpenCVFilterFaceDetect f = (OpenCVFilterFaceDetect)filter;
+    // expected vs actual
+    Assert.assertEquals(1, f.bb.size());
+    
+  }
+  
+}
