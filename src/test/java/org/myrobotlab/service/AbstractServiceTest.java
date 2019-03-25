6f20f5ded5ae967abb57b46f69d0093fdc0fa985
Kevin Watters <kwatters@kmwllc.com>
2019-03-24 22:55:31 -0400

    putting the programab auto-testing elves back to work.  They should never have been ignored.

---
+++ src/test/java/org/myrobotlab/service/AbstractServiceTest.java
@@ -0,0 +1,50 @@
+package org.myrobotlab.service;
+
+import org.junit.Test;
+import org.myrobotlab.framework.Service;
+import org.myrobotlab.logging.LoggingFactory;
+import org.myrobotlab.test.AbstractTest;
+import org.junit.Ignore;
+
+/**
+ * A base generic test class that can start a service, call a test method on it,
+ * and then release the service The idea is you can subclass this and implement
+ * the createService() and testService() methods to instrument a unit test for
+ * any service in MRL.
+ * 
+ * @author kwatters
+ *
+ */
+@Ignore
+public abstract class AbstractServiceTest extends AbstractTest {
+
+  // The service object that is created for testing
+  private Service service;
+
+  // This method should be subclassed and it should return a service object.
+  public abstract Service createService();
+
+  // This is the internal testing method that sequences through the life cycle
+  // of the service in the unit test
+  @Test
+  public void testServiceInternal() throws Exception {
+    // set up the logging level ?
+    // LoggingFactory.init("WARN");
+    service = createService();
+    testService();
+    releaseService();
+  };
+
+  // The implementing class will do all it's validation against the "service"
+  // object
+  public abstract void testService() throws Exception;
+
+  // finally release the service after the unit test has completed.
+  public void releaseService() {
+    // TODO: what's the normal way to destroy a service?
+    service.releaseService();
+    // we could potentially do some post checks to make sure the service
+    // actually shutdown
+  }
+
+}
