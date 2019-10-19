c4804cc8c5c4e53a258f0a9e8f5a06054061d406
GroG <grog@myrobotlab.org>
2019-10-19 16:52:46 -0700

    making LeapData Serializable

--- src/main/java/org/myrobotlab/service/data/LeapData.java
+++ src/main/java/org/myrobotlab/service/data/LeapData.java
@@ -1,8 +1,11 @@
 package org.myrobotlab.service.data;
 
+import java.io.Serializable;
+
 import com.leapmotion.leap.Frame;
 
-public class LeapData {
+public class LeapData implements Serializable {
+  private static final long serialVersionUID = 1L;
   transient public Frame frame;
   public LeapHand leftHand;
   public LeapHand rightHand;
