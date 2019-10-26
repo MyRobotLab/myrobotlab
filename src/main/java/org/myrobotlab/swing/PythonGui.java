3b1dbf97e5015eb4eccb2eae1141003ff4a269a6
GroG <grog@myrobotlab.org>
2019-10-26 07:41:56 -0700

    wow couple long time console initialization fixes

--- src/main/java/org/myrobotlab/swing/PythonGui.java
+++ src/main/java/org/myrobotlab/swing/PythonGui.java
@@ -164,7 +164,7 @@ public class PythonGui extends ServiceGui implements ActionListener, MouseListen
     if (o == stop) {
       stop.activate();
       send("stop");
-      send("attachPythonConsole");
+      //send("attachPythonConsole");
       return;
     } else if (o == execute) {
       execute.activate();
@@ -244,7 +244,7 @@ public class PythonGui extends ServiceGui implements ActionListener, MouseListen
     subscribe("publishStdOut");
     subscribe("appendScript");
     // subscribe("publishLoadedScript", "addNewEditorPanel");
-    send("attachPythonConsole");
+    //send("attachPythonConsole");
   }
 
   @Override
