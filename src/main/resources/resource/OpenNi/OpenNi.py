6cdfb9eaf68213935d11f321cf7d9afa8b94a989
Kevin Watters <kwatters@kmwllc.com>
2019-11-02 14:40:17 -0400

    some updates to scripts.

--- src/main/resources/resource/OpenNi/OpenNi.py
+++ src/main/resources/resource/OpenNi/OpenNi.py
@@ -6,4 +6,20 @@
 # very minimal script to start
 
 openni = Runtime.start("openni", "OpenNi")
+python = Runtime.getService("python")
+
+# define a method for your callback..  here it passed the full openni data object that contains the skeleton info.
+def onOpenNIData(data):
+  # python handy method for printing all members on an object.
+  print "Full data object"
+  print dir(data)
+  print "Skeleton object"
+  print dir(data.skeleton)  
+  # example of printout of the head x,y,z coordinates.
+  print str(data.skeleton.head.x) + " " + str(data.skeleton.head.y) + " " + str(data.skeleton.head.z)
+
+# attach to the callback in python for the data
+openni.addOpenNIData(python)
+
 openni.startUserTracking()
+
