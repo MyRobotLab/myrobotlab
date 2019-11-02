b89fc59e2bb0cd4a8828ab6367eb2534443d4931
Kevin Watters <kwatters@kmwllc.com>
2019-11-02 14:40:17 -0400

    some updates to scripts.

--- src/main/resources/resource/RSSConnector/RSSConnector.py
+++ src/main/resources/resource/RSSConnector/RSSConnector.py
@@ -1,2 +1,12 @@
 # start the service
 rssconnector = Runtime.start("rssconnector","RSSConnector")
+
+rssconnector.setRssUrl("http://www.myrobotlab.org/rss.xml")
+
+rssconnector.addListener("publishDocument", "python", "onDocument");
+
+def onDocument(doc):
+    # The articles are published as documents.
+    print doc
+
+rssconnector.startCrawling()
