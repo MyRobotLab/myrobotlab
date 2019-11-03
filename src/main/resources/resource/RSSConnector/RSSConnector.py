2ee39f356491b5bc70c0b6b6098a3d297b1cedae
kwatters <kwatters@kmwllc.com>
2019-11-03 13:45:24 -0500

    a little more polish on the rss example script.

--- src/main/resources/resource/RSSConnector/RSSConnector.py
+++ src/main/resources/resource/RSSConnector/RSSConnector.py
@@ -1,12 +1,23 @@
-# start the service
+##################################################################################
+# RSSConnector.py
+# description: A connector that can attach to an RSS news feed and publish those documents
+# categories: 
+# more info @: http://myrobotlab.org/service/RSSConnector
+##################################################################################
+
+# Start the connector
 rssconnector = Runtime.start("rssconnector","RSSConnector")
 
+# set the url for the rss feed
 rssconnector.setRssUrl("http://www.myrobotlab.org/rss.xml")
 
-rssconnector.addListener("publishDocument", "python", "onDocument");
-
+# Define a method that will be called when the rss connector crawls a document.
 def onDocument(doc):
     # The articles are published as documents.
     print doc
 
+# register the python service as a listener for documents.
+rssconnector.addListener("publishDocument", "python", "onDocument");
+
+# tell the rss connecto to crawl the feed.
 rssconnector.startCrawling()
