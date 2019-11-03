0c26ff19a860b52a2e0e3a673edfb9b91675b274
Kevin Watters <kwatters@kmwllc.com>
2019-11-02 22:24:40 -0400

    more updates to scripts.. something is better than nothing .. i think..

--- src/main/resources/resource/Deeplearning4j/Deeplearning4j.py
+++ src/main/resources/resource/Deeplearning4j/Deeplearning4j.py
@@ -7,3 +7,12 @@
 
 # start the service
 deeplearning4j = Runtime.start('deeplearning4j','Deeplearning4j')
+
+# load the VGG16 model from the zoo
+deeplearning4j.loadVGG16()
+
+# run an image file through the model and get the classifications / confidence
+classifications = deeplearning4j.classifyImageFileVGG16("myimage.jpg")
+
+# print them out... it's a dictionary/map
+print classifications
--- src/main/resources/resource/TesseractOcr/TesseractOcr.py
+++ src/main/resources/resource/TesseractOcr/TesseractOcr.py
@@ -1,2 +1,6 @@
 # start the service
 tesseractocr = Runtime.start("tesseractocr","TesseractOcr")
+
+text = tesseract.ocr("traffic.sign.jpg");
+
+print "Found Text :" + text
