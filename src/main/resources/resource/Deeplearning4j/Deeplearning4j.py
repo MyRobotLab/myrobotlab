3c1f08cf331fc0a477098505ac893f8a139fd302
kwatters <kwatters@kmwllc.com>
2019-11-03 13:38:49 -0500

    updating some print statements.

--- src/main/resources/resource/Deeplearning4j/Deeplearning4j.py
+++ src/main/resources/resource/Deeplearning4j/Deeplearning4j.py
@@ -1,18 +1,20 @@
-#########################################
+##################################################################################
 # Deeplearning4j.py
 # description: A wrapper service for the Deeplearning4j framework.
 # categories: ai
 # more info @: http://myrobotlab.org/service/Deeplearning4j
-#########################################
+##################################################################################
 
-# start the service
+# start the deeplearning4j service
 deeplearning4j = Runtime.start('deeplearning4j','Deeplearning4j')
 
 # load the VGG16 model from the zoo
 deeplearning4j.loadVGG16()
 
 # run an image file through the model and get the classifications / confidence
-classifications = deeplearning4j.classifyImageFileVGG16("myimage.jpg")
+classifications = deeplearning4j.classifyImageFileVGG16("image0-1.png")
 
-# print them out... it's a dictionary/map
-print classifications
+# print them out... it's a dictionary/map of label to confidence level (between 0-1)
+for label in classifications:
+  print(label + " : " + str(classifications.get(label)))
+
--- src/main/resources/resource/OpenNi/OpenNi.py
+++ src/main/resources/resource/OpenNi/OpenNi.py
@@ -14,9 +14,9 @@ python = Runtime.getService("python")
 # define a method for your callback..  here it passed the full openni data object that contains the skeleton info.
 def onOpenNIData(data):
   # python handy method for printing all members on an object.
-  print "Head Position x,y,z"
+  print("Head Position x,y,z")
   # example of printout of the head x,y,z coordinates.
-  print str(data.skeleton.head.x) + " " + str(data.skeleton.head.y) + " " + str(data.skeleton.head.z)
+  print(str(data.skeleton.head.x) + " " + str(data.skeleton.head.y) + " " + str(data.skeleton.head.z))
 
 # attach to the callback in python for the data
 openni.addOpenNIData(python)
