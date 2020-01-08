9ecaf71d2f31247ef0f83e2c97188174085bb716
GroG <grog@myrobotlab.org>
2020-01-08 12:48:54 -0800

    fixed select bot for new session in webgui

--- src/main/resources/resource/WebGui/app/service/js/ProgramABGui.js
+++ src/main/resources/resource/WebGui/app/service/js/ProgramABGui.js
@@ -1,92 +1,92 @@
 angular.module('mrlapp.service.ProgramABGui', [])
 .controller('ProgramABGuiCtrl', ['$scope', '$log', 'mrl', '$uibModal', '$sce', function($scope, $log, mrl, $uibModal, $sce) { // $modal ????
-    $log.info('ProgramABGuiCtrl');
+    $log.info('ProgramABGuiCtrl')
     // grab the self and message
-    var _self = this;
-    var startDialog = null;
-    var msg = this.msg;
+    var _self = this
+    var startDialog = null
+    var msg = this.msg
     
     // use $scope only when the variable
     // needs to interract with the display
-    $scope.currentUserName =  '';
-    $scope.currentBotName = '';
-    $scope.utterance = '';
+    $scope.currentUserName =  ''
+    $scope.currentBotName = ''
+    $scope.utterance = ''
 
     // grab defaults.
-    $scope.newUserName = $scope.service.currentUserName;
-    $scope.newBotName = $scope.service.currentBotName;
+    $scope.newUserName = $scope.service.currentUserName
+    $scope.newBotName = $scope.service.currentBotName
 
     // start info status
-    $scope.rows = [];
+    $scope.rows = []
     
     // following the template.
     this.updateState = function(service) {
         // use another scope var to transfer/merge selection
         // from user - service.currentSession is always read-only
         // all service data should never be written to, only read from
-        $scope.currentUserName = service.currentUserName;
-        $scope.currentBotName = service.currentBotName;
-        $scope.service = service;
+        $scope.currentUserName = service.currentUserName
+        $scope.currentBotName = service.currentBotName
+        $scope.service = service
     }
-    ;
+    
     
     this.onMsg = function(inMsg) {
-        $log.info("Program AB Msg !");
+        $log.info("Program AB Msg !")
         switch (inMsg.method) {
         case 'onState':
-            _self.updateState(inMsg.data[0]);
-            $scope.$apply();
-            break;
+            _self.updateState(inMsg.data[0])
+            $scope.$apply()
+            break
         case 'onRequest':
-            var textData = inMsg.data[0];
+            var textData = inMsg.data[0]
             $scope.rows.unshift({
                 name: $scope.currentUserName,
                 text: $sce.trustAsHtml(textData)
-            });
-            $log.info('currRequest', textData);
-            $scope.$apply();
-            break;
+            })
+            $log.info('currRequest', textData)
+            $scope.$apply()
+            break
         case 'onText':
-            var textData = inMsg.data[0];
+            var textData = inMsg.data[0]
             $scope.rows.unshift({
                 name: $scope.currentBotName,
                 text: $sce.trustAsHtml(textData)
-            });
-            $log.info('currResponse', textData);
-            $scope.$apply();
-            break;
+            })
+            $log.info('currResponse', textData)
+            $scope.$apply()
+            break
         case 'onOOBText':
-            var textData = inMsg.data[0];
+            var textData = inMsg.data[0]
             $scope.rows.unshift({
                 name: " > oob <",
                 text: $sce.trustAsHtml(textData)
-            });
-            $log.info('currResponse', textData);
-            $scope.$apply();
-            break;
+            })
+            $log.info('currResponse', textData)
+            $scope.$apply()
+            break
         default:
-            $log.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method);
-            break;
+            $log.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method)
+            break
         }
-        ;
+        
     }
-    ;
+    
 
     $scope.test = function(session, utterance) {
-        msg.send("getCategories","hello");
+        msg.send("getCategories","hello")
     }
     
     
     $scope.getSessionResponse = function(utterance) {
-    	$log.info("SESSION GET RESPONSE (" + $scope.currentUserName + " " + $scope.currentBotName + ")");
-    	$scope.getResponse($scope.currentUserName, $scope.currentBotName, utterance);
+    	$log.info("SESSION GET RESPONSE (" + $scope.currentUserName + " " + $scope.currentBotName + ")")
+    	$scope.getResponse($scope.currentUserName, $scope.currentBotName, utterance)
     }
     
     $scope.getResponse = function(username, botname, utterance) {
-    	$log.info("USER BOT RESPONSE (" + username + " " + botname + ")");
-        msg.send("getResponse", username, botname, utterance);
-        $scope.utterance = "";
-    };
+    	$log.info("USER BOT RESPONSE (" + username + " " + botname + ")")
+        msg.send("getResponse", username, botname, utterance)
+        $scope.utterance = ""
+    }
     
     $scope.startDialog = function() {
         startDialog = $uibModal.open({
@@ -94,32 +94,32 @@ angular.module('mrlapp.service.ProgramABGui', [])
             scope: $scope,
             controller: function($scope) {
                 $scope.cancel = function() {
-                    startDialog.dismiss();
+                    startDialog.dismiss()
                 }
-                ;
+                
             
             }
-        });
+        })
     }
     
     $scope.startSession = function(username, botname) {
-        $scope.currentUserName = username;
-        $scope.currentBotName = botname;
-    	$scope.rows.unshift("Reload Session for Bot " + botname);
-        $scope.startSessionLabel = 'Reload Session';
-        msg.send("startSession", username, botname);
-        startDialog.dismiss();
-    };
+        $scope.currentUserName = username
+        $scope.currentBotName = botname
+    	$scope.rows.unshift("Reload Session for Bot " + botname)
+        $scope.startSessionLabel = 'Reload Session'
+        msg.send("startSession", username, botname)
+        startDialog.dismiss()
+    }
     
     $scope.savePredicates = function() {
-        $scope.service = mrl.getService($scope.service.name);
-        mrl.sendTo($scope.service.name, "savePredicates");
-    };
+        $scope.service = mrl.getService($scope.service.name)
+        mrl.sendTo($scope.service.name, "savePredicates")
+    }
     
     // subscribe to the response from programab.
-    msg.subscribe('publishRequest');
-    msg.subscribe('publishText');
-    msg.subscribe('publishOOBText');
-    msg.subscribe(this);
+    msg.subscribe('publishRequest')
+    msg.subscribe('publishText')
+    msg.subscribe('publishOOBText')
+    msg.subscribe(this)
 }
-]);
+])
