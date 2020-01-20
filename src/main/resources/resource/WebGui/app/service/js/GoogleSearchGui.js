026603f7a5fca7e7e60f907dce46d1562dcd3438
GroG <grog@myrobotlab.org>
2020-01-20 11:57:53 -0800

    google search with images

--- src/main/resources/resource/WebGui/app/service/js/GoogleSearchGui.js
+++ src/main/resources/resource/WebGui/app/service/js/GoogleSearchGui.js
@@ -1,37 +1,35 @@
-angular.module('mrlapp.service.GoogleSearchGui', [])
-.controller('GoogleSearchGuiCtrl', ['$scope', '$log', 'mrl', '$uibModal', '$sce', function($scope, $log, mrl, $uibModal, $sce) { // $modal ????
+angular.module('mrlapp.service.GoogleSearchGui', []).controller('GoogleSearchGuiCtrl', ['$scope', '$log', 'mrl', '$uibModal', '$sce', '$compile', function($scope, $log, mrl, $uibModal, $sce, $compile) {
+    // $modal ????
     $log.info('GoogleSearchGuiCtrl')
     // grab the self and message
     let _self = this
     let msg = this.msg
-    
+
     $scope.rows = []
     $scope.searchText = null
-
 
     // following the template.
     this.updateState = function(service) {
         // use another scope var to transfer/merge selection
         // from user - service.currentSession is always read-only
         // all service data should never be written to, only read from
-      
+
         $scope.service = service
     }
 
-    $scope.search = function(){
+    $scope.search = function() {
 
-        $scope.rows.unshift($scope.searchText)
+        // $scope.rows.unshift($scope.searchText)
         msg.send('search', $scope.searchText)
         $scope.searchText = ''
     }
-    
-    
+
     this.onMsg = function(inMsg) {
-        $log.info("GoogleSearchGui.onMsg(" + inMsg.method +')')
+        $log.info("GoogleSearchGui.onMsg(" + inMsg.method + ')')
         let data = inMsg.data[0]
 
         switch (inMsg.method) {
-        
+
         case 'onState':
             _self.updateState(data)
             $scope.properties = mrl.getProperties(data)
@@ -39,22 +37,32 @@ angular.module('mrlapp.service.GoogleSearchGui', [])
             break
 
         case 'onText':
-            var textData = data
             $scope.rows.unshift(data)
             $scope.$apply()
             break
-       
+
+        case 'onImage':
+            $scope.rows.unshift(data)
+            $scope.$apply()
+            break
+
+        case 'onResults':
+            $scope.rows.unshift(data)
+            $scope.$apply()
+            break
+
         default:
             $log.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method)
             break
         }
-        
+
     }
-    
-    
+
     // subscribe to the response from programab.
-    
-    msg.subscribe('publishText')
+
+    // msg.subscribe('publishText')
+    // msg.subscribe('publishImages')
+    msg.subscribe('publishResults')
     msg.subscribe(this)
 }
 ])
