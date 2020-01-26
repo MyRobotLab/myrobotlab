af5b809e13befa493ffb405a935b4089095b131f
GroG <grog@myrobotlab.org>
2020-01-26 15:35:37 -0800

    search interface

---
+++ src/main/java/org/myrobotlab/service/interfaces/SearchPublisher.java
@@ -0,0 +1,22 @@
+package org.myrobotlab.service.interfaces;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.myrobotlab.service.GoogleSearch.SearchResults;
+
+public interface SearchPublisher extends TextPublisher {
+
+  SearchResults search(String searchText) throws IOException;
+
+  List<String> imageSearch(String searchText);
+
+  SearchResults publishResults(SearchResults results);
+
+  String publishImage(String image);
+
+  List<String> publishImages(List<String> images);
+
+  int setMaxImages(int cnt);
+
+} 
