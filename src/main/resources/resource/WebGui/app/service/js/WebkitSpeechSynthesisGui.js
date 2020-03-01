b7d8904ecb5133fa68b11705eb52144bbc50ae73
GroG <grog@myrobotlab.org>
2020-02-29 17:57:06 -0800

    webkitspeechsynthesis

--- src/main/resources/resource/WebGui/app/service/js/WebkitSpeechSynthesisGui.js
+++ src/main/resources/resource/WebGui/app/service/js/WebkitSpeechSynthesisGui.js
@@ -1,27 +1,11 @@
-angular.module('mrlapp.service.WebkitSpeechSynthesisGui', []).controller('WebkitSpeechSynthesisGuiCtrl', ['$scope', '$log', 'mrl', '$uibModal', function($scope, $log, mrl, $uibModal) {
-    $log.info('WebkitSpeechSynthesisGuiCtrl')
+angular.module('mrlapp.service.WebkitSpeechSynthesisGui', []).controller('WebkitSpeechSynthesisGuiCtrl', ['$scope', 'mrl', function($scope, mrl) {
+    console.info('WebkitSpeechSynthesisGuiCtrl')
     var _self = this
     var msg = this.msg
 
-    
-    $scope.speak = function() {
-        if ('speechSynthesis'in window) {
-            // Synthesis support. Make your web apps talk!
-            var synth = window.speechSynthesis;
+    $scope.voiceToIndexMap = {}
 
-            voices = synth.getVoices();
-            for (var i = 0; i < voices.length; i++) {
-                console.log("Voice " + i.toString() + ' ' + voices[i].name + ' ' + voices[i].uri);
-            }
 
-            var utterThis = new SpeechSynthesisUtterance("Hello this is a test, can you hear me ... why have i been so quite for so long");
-            utterThis.voice = voices[2];
-            utterThis.pitch = 1.0
-            utterThis.rate = 1.0
-            synth.speak(utterThis)
-
-        }
-    }
 
     this.updateState = function(service) {
 		$scope.service = service
@@ -35,8 +19,11 @@ angular.module('mrlapp.service.WebkitSpeechSynthesisGui', []).controller('Webkit
             _self.updateState(inMsg.data[0])
             $scope.$apply()
             break
+         case 'onVoiceIndex':
+            break
+            
         default:
-            $log.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method)
+            console.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method)
             break
         }
     }
@@ -45,16 +32,37 @@ angular.module('mrlapp.service.WebkitSpeechSynthesisGui', []).controller('Webkit
     // I suspect speak is not "setup" like other functions and is not accessable like others in the
     // theml e.g. msg.speak - so got to figure that out or temporarily create a $scope.speak kludge
     $scope.speak = function(text){
-        msg.send("speak", text)
-        //console.log($scope.service.voice.name)
+         if ('speechSynthesis'in window) {
+            // Synthesis support. Make your web apps talk!
+            var synth = window.speechSynthesis;
+
+            var utterThis = new SpeechSynthesisUtterance(text);
+            utterThis.voice = voices[2];
+            utterThis.pitch = 1.0
+            utterThis.rate = 1.0
+            synth.speak(utterThis)
+
+        }
     }
 
-    $scope.setVoice  = function(text){
-        console.log($scope.service.voice.name)
+
+    $scope.setVoice  = function(text){        
         msg.send("setVoice", text.name)
-        // msg.send("broadcastState")
     }
 
+     if ('speechSynthesis'in window) {
+            // Synthesis support. Make your web apps talk!
+            var synth = window.speechSynthesis;
+
+            voices = synth.getVoices();
+            
+            for (var i = 0; i < voices.length; i++) {
+                console.log("Voice " + i.toString() + ' ' + voices[i].name + ' ' + voices[i].uri);
+                msg.send("addWebKitVoice", i, voices[i].name, voices[i].lang, voices[i].default)
+            }
+     }
+
+    msg.subscribe('publishVoiceIndex')
     msg.subscribe(this)
 }
 ])
