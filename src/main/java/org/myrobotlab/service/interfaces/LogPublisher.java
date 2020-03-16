63036d84ce32ca1f356c54e3a86859950f204b26
GroG <grog@myrobotlab.org>
2020-03-15 18:19:11 -0700

    formatter correction

--- src/main/java/org/myrobotlab/service/interfaces/LogPublisher.java
+++ src/main/java/org/myrobotlab/service/interfaces/LogPublisher.java
@@ -6,25 +6,28 @@ import org.myrobotlab.framework.interfaces.NameProvider;
  * 
  * @author GroG
  *
- * A LogPublisher can publish its own logging messages
+ *         A LogPublisher can publish its own logging messages
  * 
  */
 public interface LogPublisher extends NameProvider {
-		
-		/**
-		 * A String is currently used as the log entry - but it could be an object in its 
-		 * constituent parts instead e.g. timestamp, formatted message, level etc.  - if this
-		 * is desired make a public LogEntry publishLogEntry interface method 
-		 * @param msg
-		 * @return
-		 */
-		public String publishLog(String msg);
-		
-		/**
-		 * a way to publish the log messages and log entries
-		 * @param method
-		 * @param params
-		 * @return
-		 */
-		public Object invoke(String method, Object... params);
+
+  /**
+   * A String is currently used as the log entry - but it could be an object in
+   * its constituent parts instead e.g. timestamp, formatted message, level etc.
+   * - if this is desired make a public LogEntry publishLogEntry interface
+   * method
+   * 
+   * @param msg
+   * @return
+   */
+  public String publishLog(String msg);
+
+  /**
+   * a way to publish the log messages and log entries
+   * 
+   * @param method
+   * @param params
+   * @return
+   */
+  public Object invoke(String method, Object... params);
 }
