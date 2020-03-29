5944258b1b0d35f2da38dc83df952dbd0f1c0713
GroG <grog@myrobotlab.org>
2020-03-29 08:24:59 -0700

    changeTab functionality and removal of viewType

--- src/main/resources/resource/WebGui/app/service/js/SecurityGui.js
+++ src/main/resources/resource/WebGui/app/service/js/SecurityGui.js
@@ -2,6 +2,10 @@ angular.module('mrlapp.service.SecurityGui', []).controller('SecurityGuiCtrl', [
     $log.info('SecurityGuiCtrl')
     var _self = this
     var msg = this.msg
+    
+    // FIXME - need better way to have multiple panels, currently this is just for testing
+    $scope.mrl = mrl
+    $scope.panel = mrl.getPanel('runtime')
 
     $scope.keyNames = [];
 
