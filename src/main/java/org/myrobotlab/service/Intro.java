44025c66a2e8edac1889fc0a27080b383049439c
Langevin Gael <hairygael@gmail.com>
2020-04-09 23:07:00 +0200

    add startServo to Intro

--- src/main/java/org/myrobotlab/service/Intro.java
+++ src/main/java/org/myrobotlab/service/Intro.java
@@ -6,6 +6,7 @@ import org.myrobotlab.logging.Level;
 import org.myrobotlab.logging.LoggerFactory;
 import org.myrobotlab.logging.LoggingFactory;
 import org.slf4j.Logger;
+import org.myrobotlab.service.interfaces.ServoControl;
 
 public class Intro extends Service {
 
@@ -31,6 +32,7 @@ public class Intro extends Service {
     meta.addDescription("Introduction to MyRobotlab");
     meta.setAvailable(true); 
     meta.addCategory("general");
+    meta.addPeer("servo", "Servo", "servo");
     return meta;
   }
 
@@ -47,4 +49,47 @@ public class Intro extends Service {
       log.error("main threw", e);
     }
   }
+
+  transient Servo servo;
+
+  boolean isServoActivated = false;
+
+  public boolean isServoActivated() {
+        return isServoActivated;
+        }
+
+
+
+  public Servo startServo(String port) {
+    return startServo(port, 3);
+  }
+
+  public Servo startServo(String port, int pin) {
+  
+    if (servo == null) {
+      speakBlocking("starting servo");
+      isServoActivated = true;
+
+      servo = (Servo) startPeer("servo");
+
+      if (port != null) {
+        try {
+          speakBlocking(port);
+          Arduino controller = (Arduino) startPeer("controller");
+          controller.connect(port);
+          controller.attach(controller, pin);
+        } catch (Exception e) {
+          error(e);
+        }
+      }
+    }
+    return servo;
+  }
+
+  public void stopServo() {
+    speakBlocking("stopping servo");
+    releasePeer("servo");
+    isServoActivated = false;
+  }
+
 }
