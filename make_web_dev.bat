cdf8325349a281977601a0fb9f11377500651eb1
GroG <grog@myrobotlab.org>
2020-04-10 13:01:43 -0700

    Update make_web_dev.bat

--- make_web_dev.bat
+++ make_web_dev.bat
@@ -6,5 +6,5 @@ cd myrobotlab
 git pull
 git status
 curl -LOJ http://build.myrobotlab.org:8080/job/myrobotlab/job/develop/lastSuccessfulBuild/artifact/target/myrobotlab.jar
-java -jar myrobotlab.jar --install
-java -jar myrobotlab.jar -s webgui WebGui python Python i01 InMoov2 intro Intro
+java -jar myrobotlab.jar --install && java -jar myrobotlab.jar -s webgui WebGui python Python i01 InMoov2 intro Intro
+
