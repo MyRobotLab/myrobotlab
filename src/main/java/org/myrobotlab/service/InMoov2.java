b7df4b322553c249da2168cf68a77bb44be9357c
Langevin Gael <hairygael@gmail.com>
2020-04-18 15:09:22 +0200

    checking pir version11

--- src/main/java/org/myrobotlab/service/InMoov2.java
+++ src/main/java/org/myrobotlab/service/InMoov2.java
@@ -1684,15 +1684,16 @@ public class InMoov2 extends Service implements TextListener, TextPublisher, Joy
 	
 	public void publishPin(Pin pin) {
 		log.info("{} - {}", pin.pin, pin.value);
-		if (pin.value == 1) {
-		  lastPIRActivityTime = System.currentTimeMillis();
-		}
+		//if (pin.value == 1) {
+		  //lastPIRActivityTime = System.currentTimeMillis();
+		//}
 		// if its PIR & PIR is active & was sleeping - then wake up !
-		if (pirPin == pin.pin && pin.value == 1) {
+		//if (pirPin == pin.pin && startSleep != null && pin.value == 1) {
 		  // attach(); // good morning / evening / night... asleep for % hours
-		  powerUp();
-		}
+		  //powerUp();
+		//}
 	}
+	
 	public void startServos(String leftPort, String rightPort) throws Exception {
 		startHead(leftPort);
 		startLeftArm(leftPort);
