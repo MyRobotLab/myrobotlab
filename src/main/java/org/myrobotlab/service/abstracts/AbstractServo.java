639d66d9e01b7ecdc5f97a6abe00f852d7dd7a1f
kwatters <kwatters@kmwllc.com>
2020-05-21 12:12:56 -0400

    making a bunch of stuff primitive.

--- src/main/java/org/myrobotlab/service/abstracts/AbstractServo.java
+++ src/main/java/org/myrobotlab/service/abstracts/AbstractServo.java
@@ -73,12 +73,7 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
    * "command" methods such as moveTo - its always status information, and its
    * typically updated from an encoder of some form
    */
-  protected Double currentPos;
-
-  /**
-   * set by encoders
-   */
-  protected Double currentPosInput = null;
+  protected double currentPos;
 
   /**
    * if enabled then a pwm pulse is keeping the servo at the current position,
@@ -164,7 +159,7 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
   /**
    * default rest is 90 default target position will be 90 if not specified
    */
-  protected Double rest = 90.0;
+  protected double rest = 90.0;
 
   /**
    * speed of the servo - null is no speed control
@@ -195,7 +190,7 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
   /**
    * the requested INPUT position of the servo
    */
-  protected Double targetPos;
+  protected double targetPos;
 
   /**
    * if true - a single moveTo command will be published for servo controllers
@@ -269,7 +264,6 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
     }
     if (enc.equals(encoder)) {
       log.info("encoder {} already attached", enc.getName());
-      return;
     }
     encoder = enc;
     enc.attach(this);
@@ -484,12 +478,12 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
   }
 
   @Override
-  public Double getTargetOutput() {
+  public double getTargetOutput() {
     return mapper.calcOutput(targetPos);    
   }
 
   @Override
-  public Double getTargetPos() {
+  public double getTargetPos() {
     return targetPos;
   }
 
@@ -540,7 +534,7 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
   }
 
   @Override
-  public void map(Double minX, Double maxX, Double minY, Double maxY) {
+  public void map(double minX, double maxX, double minY, double maxY) {
     mapper = new MapperLinear(minX, maxX, minY, maxY);
     broadcastState();
   }
