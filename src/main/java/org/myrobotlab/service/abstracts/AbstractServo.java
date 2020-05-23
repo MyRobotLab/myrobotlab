70ffc429e43380f94ee78068b0922f55cdf18e80
GroG <grog@myrobotlab.org>
2020-05-23 14:44:53 -0700

    fixed sweep

--- src/main/java/org/myrobotlab/service/abstracts/AbstractServo.java
+++ src/main/java/org/myrobotlab/service/abstracts/AbstractServo.java
@@ -598,7 +598,7 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
       isMoving = false;
       
       invoke("publishJointAngle", new AngleData(getName(), data.angle));
-      invoke("publishServoEvent", ServoStatus.SERVO_STOPPED, currentInputPos);
+//      invoke("publishServoEvent", ServoStatus.SERVO_STOPPED, currentInputPos);
       invoke("publishServoStopped", this);
 
       // if currently configured to autoDisable - the timer starts now
@@ -611,8 +611,6 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
       
     } else {
       isMoving = true;
-      // invoke("publishMoveTo", this); grog: WRONG ! on every encoder update .. no !
-      // invoke("publishServoEvent", ServoStatus.SERVO_POSITION_UPDATE, currentInputPos); grog: WRONG !
     }
   }
 
@@ -638,28 +636,31 @@ public abstract class AbstractServo extends Service implements ServoControl, Enc
     return sc;
   }
 
-  @Override
-  @Deprecated /*
-               * not used nor wanted - subscribers should be either
-               * EncoderListener or ServoController
-               */
   public ServoEvent publishServoEvent(ServoStatus status, Double currentPosUs) {
-    double pos = microsecondsToDegree(currentPosUs);
-    ServoEvent sd = new ServoEvent(status, getName(), pos);
+    double posDegrees = microsecondsToDegree(currentPosUs);
+    ServoEvent sd = new ServoEvent(status, getName(), posDegrees);
     lastActivityTimeTs = System.currentTimeMillis();
-    // log.debug("status {} pos {}", status, pos);
 
     if (isSweeping) {
-      if (sweepingToMax && pos >= sweepMax - 1) { // handle overshoot ?
-        sweepingToMax = false;
-        moveTo(sweepMin);
-      }
-      if (!sweepingToMax && pos <= sweepMin + 1) { // handle overshoot ?
-        sweepingToMax = true;
-        moveTo(sweepMax);
+    if (status == ServoStatus.SERVO_STOPPED) {
+      double inputPos = mapper.calcInput(currentOutputPos);
+      
+      // We got a stop event from the servo - which "should" be 
+      // the end of a sweep.  "Should" be.
+      // If our current position from the encoder says we
+      // are closer to one side vs the other .. we go to the
+      // opposite side.
+      
+      double deltaMin = Math.abs(inputPos - sweepMin);
+      double deltaMax = Math.abs(inputPos - sweepMax);
+      
+      if (deltaMin < deltaMax ) {
+        send(getName(), "moveTo", sweepMax);
+      } else {
+        send(getName(), "moveTo", sweepMin);
       }
     }
-
+    }
     return sd;
   }
   
