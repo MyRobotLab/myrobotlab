c57e5273244349fbecd13dd97c93081d00f39df3
Langevin Gael <hairygael@gmail.com>
2020-06-02 00:30:49 +0200

    add execScript and loadFile

--- src/main/java/org/myrobotlab/service/InMoov2Arm.java
+++ src/main/java/org/myrobotlab/service/InMoov2Arm.java
@@ -1,12 +1,15 @@
 package org.myrobotlab.service;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.commons.io.FilenameUtils;
 import org.myrobotlab.framework.Service;
 import org.myrobotlab.framework.ServiceType;
+import org.myrobotlab.io.FileIO;
 import org.myrobotlab.kinematics.DHLink;
 import org.myrobotlab.kinematics.DHRobotArm;
 import org.myrobotlab.logging.LoggerFactory;
@@ -304,6 +307,50 @@ public class InMoov2Arm extends Service implements IKJointAngleListener {
     omoplate.save();
     return true;
   }
+  
+  /**
+   * execute a resource script
+   * @param someScriptName
+   */
+  public void execScript(String someScriptName) {
+    try {
+      Python p = (Python)Runtime.start("python", "Python");
+      String script = getResourceAsString(someScriptName);
+      p.exec(script, true);
+    } catch (Exception e) {
+      error("unable to execute script %s", someScriptName); 
+    }
+  }
+
+	/**
+	 * This method will load a python file into the python interpreter.
+	 */
+  @Deprecated /* use execScript - this doesn't handle resources correctly */
+	public static boolean loadFile(String file) {
+		File f = new File(file);
+		Python p = (Python) Runtime.getService("python");
+		log.info("Loading  Python file {}", f.getAbsolutePath());
+		if (p == null) {
+			log.error("Python instance not found");
+			return false;
+		}
+		String script = null;
+		try {
+			script = FileIO.toString(f.getAbsolutePath());
+		} catch (IOException e) {
+			log.error("IO Error loading file : ", e);
+			return false;
+		}
+		// evaluate the scripts in a blocking way.
+		boolean result = p.exec(script, true);
+		if (!result) {
+			log.error("Error while loading file {}", f.getAbsolutePath());
+			return false;
+		} else {
+			log.debug("Successfully loaded {}", f.getAbsolutePath());
+		}
+		return true;
+	}  
 
   public void setAutoDisable(Boolean idleTimeoutMs) {
     bicep.setAutoDisable(idleTimeoutMs);
