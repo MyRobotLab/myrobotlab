370ef23cfa9fdb5bc6963f46225150d8283b8c07
Gaël Langevin <hairygael@gmail.com>
2020-06-01 23:35:14 +0200

    loadFile for dedicated config saving

--- src/main/java/org/myrobotlab/service/InMoov2Head.java
+++ src/main/java/org/myrobotlab/service/InMoov2Head.java
@@ -328,6 +328,32 @@ public class InMoov2Head extends Service {
     return true;
   }
 
+	public static boolean loadFile(String file) {
+		File f = new File(file);
+		Python p = (Python) Runtime.getService("python");
+		log.info("Loading  Python file {}", f.getAbsolutePath());
+		if (p == null) {
+			log.error("Python instance not found");
+			return false;
+		}
+		String script = null;
+		try {
+			script = FileIO.toString(f.getAbsolutePath());
+		} catch (IOException e) {
+			log.error("IO Error loading file : ", e);
+			return false;
+		}
+		// evaluate the scripts in a blocking way.
+		boolean result = p.exec(script, true);
+		if (!result) {
+			log.error("Error while loading file {}", f.getAbsolutePath());
+			return false;
+		} else {
+			log.debug("Successfully loaded {}", f.getAbsolutePath());
+		}
+		return true;
+	}  
+
   public void setAutoDisable(Boolean rotheadParam, Boolean neckParam, Boolean rollNeckParam) {
     rothead.setAutoDisable(rotheadParam);
     rollNeck.setAutoDisable(rollNeckParam);
