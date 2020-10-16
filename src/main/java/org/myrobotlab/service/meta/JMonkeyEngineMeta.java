271c72b064bcb4c6ecc2605bf0c22e37edcf2bd5
kwatters <kwatters@kmwllc.com>
2020-10-16 10:56:03 -0400

    fixing up jme meta

--- src/main/java/org/myrobotlab/service/meta/JMonkeyEngineMeta.java
+++ src/main/java/org/myrobotlab/service/meta/JMonkeyEngineMeta.java
@@ -25,7 +25,7 @@ public class JMonkeyEngineMeta extends MetaData {
     addDependency("org.jmonkeyengine", "jme3-desktop", jmeVersion);
     // addDependency("org.jmonkeyengine", "jme3-lwjgl", jmeVersion);
     // nev version of lwjgl3 which works with java 11
-    addDependency("org.jmonkeyengine", "jme3-lwjgl3", jmeVersion);
+    addDependency("org.jmonkeyengine", "jme3-lwjgl", "2.9.3");
     addDependency("org.jmonkeyengine", "jme3-jogg", jmeVersion);
     // addDependency("org.jmonkeyengine", "jme3-test-data", jmeVersion);
     addDependency("com.simsilica", "lemur", "1.11.0");
