0223a12c1570f7fc2479e94be6cdbdbedb0acffc
greg <greg.perry@daimler.com>
2021-05-06 20:41:39 -0700

    removed jcenter and bintray from runtime deps

--- src/main/java/org/myrobotlab/service/InMoov2.java
+++ src/main/java/org/myrobotlab/service/InMoov2.java
@@ -61,29 +61,6 @@ public class InMoov2 extends Service implements TextListener, TextPublisher, Joy
       error("unable to execute script %s", someScriptName);
     }
   }
-  
-  
-  /**
-   * Part of service life cycle - a new servo has been started
-   */
-  public void onStarted(String fullname) {
-    log.info("{} started");
-    try {
-      ServiceInterface si = Runtime.getService(fullname);
-      if ("Servo".equals(si.getSimpleName())){
-        log.info("sending setAutoDisable true to {}", fullname);
-        send(fullname, "setAutoDisable", true);
-        // ServoControl sc = (ServoControl)Runtime.getService(name);
-      }
-    } catch (Exception e) {
-      log.error("onStarted threw", e);
-    }
-  }
-  
-  public void onCreated(String fullname) {
-    log.info("{} created", fullname);
-  }
-
 
   /**
    * Part of service life cycle - a new servo has been started
@@ -309,17 +286,6 @@ public class InMoov2 extends Service implements TextListener, TextPublisher, Joy
     // by default all servos will auto-disable
     // Servo.setAutoDisableDefault(true); //until peer servo services for
     // InMoov2 have the auto disable behavior, we should keep this
-    
-    // same as created in runtime - send asyc message to all
-    // registered services, this service has started
-    // find all servos - set them all to autoDisable(true)
-    // onStarted(name) will handle all future created servos
-    List<ServiceInterface> services = Runtime.getServices();
-    for (ServiceInterface si : services) {
-      if ("Servo".equals(si.getSimpleName())) {
-        send(si.getFullName(), "setAutoDisable", true);
-      }
-    } 
 
     // same as created in runtime - send asyc message to all
     // registered services, this service has started
