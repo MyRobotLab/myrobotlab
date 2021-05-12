e648bf455a473f94a13ff6a568e9749ec193cb7a
greg <greg.perry@daimler.com>
2021-05-12 07:01:43 -0700

    making noWorky dialog worky

--- src/main/resources/resource/WebGui/app/nav/noWorkyCtrl.js
+++ src/main/resources/resource/WebGui/app/nav/noWorkyCtrl.js
@@ -1,23 +1,30 @@
-angular.module('mrlapp.nav')
-        .controller('noWorkyCtrl', ['$scope', '$uibModalInstance', 'reason', 'mrl', function ($scope, $uibModalInstance, reason, mrl) {
-                $scope.close = $uibModalInstance.close;
-        
-                $scope.status = 'waitingforinformation';
-                
-                $scope.noWorky = function (userId) {
-                    $scope.status = 'sendingnoworky';
-                    mrl.noWorky(userId);
-                };
-                
-                var onNoWorky = function (noWorkyResultssMsg) {
-                    var status = noWorkyResultssMsg.data[0];
-                    if (status.level == 'error') {
-                        $scope.status = 'error';
-                        $scope.statuskey = status.key;
-                    } else {
-                        $scope.status = 'success';
-                    }
-                };
-// FIXME - a noWorky is Runtime specific ... WHICH RUNTIME !!!
-//                mrl.subscribeToServiceMethod(onNoWorky, mrl.getRuntime().name, 'publishNoWorky');
-            }]);
+angular.module('mrlapp.nav').controller('noWorkyCtrl', ['$scope', '$uibModalInstance', 'reason', 'mrl', function($scope, $uibModalInstance, reason, mrl) {
+    let _self = this
+    $scope.close = $uibModalInstance.close
+    $scope.status = 'waitingforinformation'
+
+    $scope.noWorky = function(userId) {
+        $scope.status = 'sendingnoworky'
+        mrl.noWorky(userId)
+    }
+
+    _self.onNoWorky = function(status) {
+        if (status.level == 'error') {
+            $scope.status = 'error'
+            $scope.statuskey = status.key
+        } else {
+            $scope.status = 'success'
+        }
+        $scope.$apply()
+    }
+
+    // mrl.subscribeTo('runtime', 'noWorky')
+    // defaulted to runtime@{remoteId} callback to runtime@{jsId}
+    mrl.subscribeTo('runtime', 'noWorky', _self.onNoWorky)
+
+    // mrl.subscribeToServiceMethod(onNoWorky, 'runtime@' + mrl.getRemoteId(), 'publishNoWorky')
+    // jsRuntimeMethodCallbackMap[fullname + '.onDescribe'] = _self.onDescribe
+    // mrl.sendTo('runtime@' + mrl.getRemoteId(), "addListener", "noWorky", 'runtime@' + mrl.getId())
+    // mrl.subscribeTo(this, 'runtime@' + mrl.getRemoteId(), 'noWorky', 'runtime@' + mrl.getId())
+}
+])
