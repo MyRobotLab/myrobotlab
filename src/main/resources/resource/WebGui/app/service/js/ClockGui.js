8363f51e0b863e5e2e77e121bf1fb7aef173cdb2
greg <greg.perry@daimler.com>
2021-05-19 06:18:53 -0700

    clock polish

--- src/main/resources/resource/WebGui/app/service/js/ClockGui.js
+++ src/main/resources/resource/WebGui/app/service/js/ClockGui.js
@@ -1,50 +1,39 @@
-angular.module('mrlapp.service.ClockGui', [])
-        .controller('ClockGuiCtrl', ['$scope', '$log', 'mrl', '$timeout', function ($scope, $log, mrl, $timeout) {
-                $log.info('ClockGuiCtrl');
-                var _self = this;
-                var msg = this.msg;
+angular.module('mrlapp.service.ClockGui', []).controller('ClockGuiCtrl', ['$scope', 'mrl', function($scope, mrl) {
+    console.info('ClockGuiCtrl')
+    var _self = this
+    var msg = this.msg
 
-                // GOOD TEMPLATE TO FOLLOW
-                this.updateState = function (service) {
-                    $scope.service = service;
-                };
+    // GOOD TEMPLATE TO FOLLOW
+    this.updateState = function(service) {
+        $scope.service = service
+    }
 
+    // init scope variables
+    $scope.onTime = null
+    $scope.onEpoch = null
 
+    this.onMsg = function(inMsg) {
+        switch (inMsg.method) {
+        case 'onState':
+            _self.updateState(inMsg.data[0])
+            $scope.$apply()
+            break
+        case 'onTime':
+            $scope.onTime = inMsg.data[0]
+            $scope.$apply()
+            break
+        case 'onEpoch':
+            $scope.onEpoch = inMsg.data[0]
+            $scope.$apply()
+            break
+        default:
+            console.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method)
+            break
+        }
+    }
 
-                // init scope variables
-                $scope.pulseData = '';
-
-                this.onMsg = function (inMsg) {
-                    switch (inMsg.method) {
-                        case 'onState':
-                            $timeout(function () {
-                                _self.updateState(inMsg.data[0]);
-                            });
-                            break;
-                        case 'onPulse':
-                            $timeout(function () {
-                                $scope.pulseData = inMsg.data[0];
-                            });
-                            break;
-                        default:
-                            $log.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method);
-                            break;
-                    }
-                };
-
-                $scope.toNumber = function (val) {
-                    if (angular.isUndefined(val)) {
-                        var val = '0';
-                    }
-                    val = val.toString();
-                    var clean = val.replace(/[^0-9\.]/g, '').replace('.', '').replace(' ', '');
-                    if (clean == '') {
-                        clean = '0';
-                    }
-                    return clean;
-                };
-
-                msg.subscribe('pulse');
-                msg.subscribe(this);
-            }
-        ])
+    msg.subscribe('publishTime')
+    msg.subscribe('publishEpoch')
+    msg.subscribe(this)
+}
+])
