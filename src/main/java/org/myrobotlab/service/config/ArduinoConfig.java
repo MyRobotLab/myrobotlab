923c7df4ca757ea255e5060b8f400a35793ea1ff
Greg Perry <greg.perry@torc.ai>
2021-06-20 09:34:15 -0700

    starting ...

---
+++ src/main/java/org/myrobotlab/service/config/ArduinoConfig.java
@@ -0,0 +1,5 @@
+package org.myrobotlab.service.config;
+
+public class ArduinoConfig extends Config {
+
+}
---
+++ src/main/java/org/myrobotlab/service/config/ServoConfig.java
@@ -0,0 +1,22 @@
+package org.myrobotlab.service.config;
+
+public class ServoConfig extends Config {
+
+  public Boolean autoDisable;
+  public String controller;
+  public Double currentOutputPos;
+  public Boolean enabled;
+  public Boolean idleDisabled;  
+  public Integer idleTimeout;
+  public Boolean isBlocking;
+  
+  public String pin;
+  public Double rest;
+  public Double speed;
+
+  public Double sweepMax;
+  public Double sweepMin;
+  
+  public Double targetPos;
+  
+}
