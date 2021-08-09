21aaa43448698e0f42d4fee9b453eca24c06ced0
Greg Perry <greg.perry@torc.ai>
2021-08-08 22:42:30 -0700

    mpu worky

--- src/main/resources/resource/WebGui/app/service/js/Mpu6050Gui.js
+++ src/main/resources/resource/WebGui/app/service/js/Mpu6050Gui.js
@@ -1,151 +1,139 @@
-angular.module('mrlapp.service.Mpu6050Gui', [])
-.controller('Mpu6050GuiCtrl', ['$log', '$scope', 'mrl', function($log, $scope, mrl) {
-    $log.info('Mpu6050GuiCtrl');
-    var _self = this;
-    var msg = this.msg;
-    
-    // Init
-    // Don't think init is necessary for data that is bound
-    $scope.controllerName = '';
-    $scope.controllers = [];  
-    
+angular.module('mrlapp.service.Mpu6050Gui', []).controller('Mpu6050GuiCtrl', ['$scope', 'mrl', function($scope, mrl) {
+    console.info('Mpu6050GuiCtrl')
+    var _self = this
+    var msg = this.msg
+
+    $scope.controllers = []
+
+    // published Mpu6050 data
+    $scope.data = null
+    $scope.orientation = {
+        "pitch": 0,
+        "roll": 0,
+        "yaw": 0
+    }
+
+    $scope.sampleRates = []
+
+    for (let i = 1; i < 20; ++i) {
+        $scope.sampleRates.push(i)
+    }
+
     // Start of three.js scene and object creation
     // Create the scene
-	$log.info("Creating scene..");
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 25, 1, 0.5, 1000 );
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setSize(400,400);
-	// Lightning
-	var ambientLight, light;
-	ambientLight = new THREE.AmbientLight( 0x333333 );	// 0.2
-	light = new THREE.DirectionalLight( 0xFFFFFF, 1.0 );
-	// Materials
-	var teapotColor = new THREE.Color(0xdd00dd);
-	phongMaterial = new THREE.MeshPhongMaterial( { color: teapotColor, shading: THREE.SmoothShading, side: THREE.DoubleSide } );
-    var gridHelper = new THREE.GridHelper( 50, 5 );
+    console.info("Creating scene..")
+    var scene = new THREE.Scene()
+    var camera = new THREE.PerspectiveCamera(25,1,0.5,1000)
+    var renderer = new THREE.WebGLRenderer()
+    renderer.setSize(400, 400)
+    // Lightning
+    var ambientLight, light
+    ambientLight = new THREE.AmbientLight(0x333333)
+    // 0.2
+    light = new THREE.DirectionalLight(0xFFFFFF,1.0)
+    // Materials
+    var teapotColor = new THREE.Color(0xdd00dd)
+    phongMaterial = new THREE.MeshPhongMaterial({
+        color: teapotColor,
+        shading: THREE.SmoothShading,
+        side: THREE.DoubleSide
+    })
+    var gridHelper = new THREE.GridHelper(50,5)
     //
-    var color1 = new THREE.Color(0xdd00dd);
-    var color2 = new THREE.Color(0x00ff00);
-    gridHelper.setColors(color1,color2);
-    
-    var teapotSize = 10;
-    var tess = 15;
-    var bottom = true;
-    var lid = true;
-    var body = true;
-    var fitLid = true;
-    var blinn = true;
+    var color1 = new THREE.Color(0xdd00dd)
+    var color2 = new THREE.Color(0x00ff00)
+    gridHelper.setColors(color1, color2)
 
-    /* Don't know why this is missing from THREE - but it is...
-    var teapotGeometry = new THREE.TeapotBufferGeometry( teapotSize,
-			tess,
-			bottom,
-		    lid,
-			body,
-			fitLid,
-			blinn);
-    
-    var teapot = new THREE.Mesh(teapotGeometry, phongMaterial);   
-    */
+    var teapotSize = 10
+    var tess = 15
+    var bottom = true
+    var lid = true
+    var body = true
+    var fitLid = true
+    var blinn = true
 
+    const geometry = new THREE.BoxGeometry(10,10,10)
+    // const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} )
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        vertexColors: true
+    })
+    const teapot = new THREE.Mesh(geometry,material)
 
-	const geometry = new THREE.BoxGeometry( 10, 10, 10 );
-	// const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-	const material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: true }  );
-	const teapot = new THREE.Mesh( geometry, material );
-
-	for ( var i = 0; i < geometry.faces.length; i ++ ) {
-        geometry.faces[ i ].color.setHex( Math.random() * 0xffffff );
+    for (var i = 0; i < geometry.faces.length; i++) {
+        geometry.faces[i].color.setHex(Math.random() * 0xffffff)
     }
 
-	var quarternion = new THREE.Quaternion(); 
-		
-    scene.add(ambientLight);
-	scene.add(light);
-    scene.add(teapot);
-    // scene.add(gridHelper);
+    var quarternion = new THREE.Quaternion()
 
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 40;
+    scene.add(ambientLight)
+    scene.add(light)
+    scene.add(teapot)
+    // scene.add(gridHelper)
 
-    renderer.render( scene, camera );
+    camera.position.x = 0
+    camera.position.y = 0
+    camera.position.z = 40
+
+    renderer.render(scene, camera)
     // End of three.js scene creation and object creation  
-    
+
     // GOOD TEMPLATE TO FOLLOW
     this.updateState = function(service) {
-        $scope.service = service;
-        $scope.controllers = service.controllers;
-        $scope.controllerName = service.controllerName;
-        $scope.deviceBusList = service.deviceBusList;
-        $scope.deviceBus = service.deviceBus;
-        $scope.deviceAddressList = service.deviceAddressList;
-        $scope.deviceAddress = service.deviceAddress;
-        
-        $scope.isAttached = service.isAttached;
-        
-        $scope.accelGX = service.accelGX;
-        $scope.accelGY = service.accelGY;
-        $scope.accelGZ = service.accelGZ;
-        
-        $scope.temperatureC = service.temperatureC;
-
-        $scope.gyroDegreeX = service.gyroDegreeX;
-        $scope.gyroDegreeY = service.gyroDegreeY;
-        $scope.gyroDegreeZ = service.gyroDegreeZ;
-
-        $scope.filtered_x_angle = service.filtered_x_angle;
-        $scope.filtered_y_angle = service.filtered_y_angle;
-        $scope.filtered_z_angle = service.filtered_z_angle;
+        $scope.service = service
     }
-    ;
-   
-    
+
     this.onMsg = function(inMsg) {
-        var data = inMsg.data[0];
+        var data = inMsg.data[0]
         switch (inMsg.method) {
         case 'onState':
-            _self.updateState(data);
-            $scope.$apply();
-            var container = document.getElementById( 'canvas' );
-    		// $log.info("CANVAS CONTAINER: " + container);
-    		if (container.hasChildNodes()) {
-    			container.removeChild( container.childNodes[0]);
-    			container.appendChild( renderer.domElement );	
-    		} else {
-    			container.appendChild( renderer.domElement );
-    		}
-    		// Rotate the teapot based on the filtered x, y, z values
-        	teapot.rotation.x = $scope.filtered_x_angle;
-        	teapot.rotation.y = $scope.filtered_y_angle;
-        	teapot.rotation.z = $scope.filtered_z_angle;
-        	
-            renderer.render( scene, camera );
-            break;
+            _self.updateState(data)
+            $scope.$apply()
+            var container = document.getElementById('canvas')
+            // console.info("CANVAS CONTAINER: " + container)
+            if (container.hasChildNodes()) {
+                container.removeChild(container.childNodes[0])
+                container.appendChild(renderer.domElement)
+            } else {
+                container.appendChild(renderer.domElement)
+            }
+            // Rotate the teapot based on the filtered x, y, z values
+            teapot.rotation.x = $scope.filtered_x_angle
+            teapot.rotation.y = $scope.filtered_y_angle
+            teapot.rotation.z = $scope.filtered_z_angle
+
+            renderer.render(scene, camera)
+            break
+        case 'onMpu6050Data':
+            var container = document.getElementById('canvas')
+            // console.info("CANVAS CONTAINER: " + container)
+            if (container.hasChildNodes()) {
+                container.removeChild(container.childNodes[0])
+                container.appendChild(renderer.domElement)
+            } else {
+                container.appendChild(renderer.domElement)
+            }
+
+            // Rotate the teapot based on the filtered x, y, z values
+            teapot.rotation.x = data.orientation.pitch
+            teapot.rotation.y = data.orientation.roll
+            teapot.rotation.z = data.orientation.yaw
+
+            $scope.data = data
+
+            $scope.$apply()
+            renderer.render(scene, camera)
+            break
+        case 'onStatus':
         default:
-            $log.info("ERROR - unhandled method " + $scope.name + " Method " + inMsg.method);
-            break;
+            console.info("ERROR - unhandled method " + $scope.name + " Method " + inMsg.method)
+            break
         }
-        ;
-    
-    }
-    ;
-    
-    $scope.setControllerName = function(name) {
-        $scope.controllerName = name;
-    }
-    
-    $scope.setDeviceBus = function(bus) {
-        $scope.deviceBus = bus;
-    }
-    
-    $scope.setDeviceAddress = function(address) {
-        $scope.deviceAddress = address;
-    }
-    
 
-    msg.subscribe(this);
+    }
+
+    msg.subscribe('publishMpu6050Data')
+    // msg.subscribe('publishOrientation')
+    msg.subscribe(this)
 }
-]);
-
+])
