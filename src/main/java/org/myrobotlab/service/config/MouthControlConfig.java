e2952159f7d739a64e23a53692af49108358626f
Langevin Gael <hairygael@gmail.com>
2021-08-30 19:17:32 +0200

    Add files via upload

---
+++ src/main/java/org/myrobotlab/service/config/MouthControlConfig.java
@@ -0,0 +1,9 @@
+package org.myrobotlab.service.config;
+
+public class MouthControlConfig extends ServiceConfig {
+
+  public int mouthClosedPos;
+  public int mouthOpenedPos;
+  public int delaytime;
+  public int delaytimestop;
+  public int delaytimeletter;
