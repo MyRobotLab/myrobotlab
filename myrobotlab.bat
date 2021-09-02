3fc49a073953f65806b02023f954b00de6e112f1
supertick <grog@myrobotlab.org>
2021-09-02 13:11:58 -0700

    duh jars now to jar

--- myrobotlab.bat
+++ myrobotlab.bat
@@ -1,11 +1,12 @@
+@ECHO OFF
+
 rem TODO move vars to top
 rem TODO have --id as an optional var
-rem @ECHO OFF
 
 set APPDIR=%CD%
 echo APPDIR=%APPDIR%
 
-set CLASSPATH="%CLASSPATH%;%APPDIR%\target\classes\*;%APPDIR%\libraries\jars\*;%APPDIR%\myrobotlab.jar"
+set CLASSPATH="%CLASSPATH%;%APPDIR%\target\classes\*;%APPDIR%\libraries\jar\*;%APPDIR%\myrobotlab.jar"
 
 echo CLASSPATH=%CLASSPATH%
 
@@ -15,5 +16,5 @@ set JAVA=java
 rem Processing/Arduino handle this in an array - no need for now
 set JAVA_OPTIONS="-Djava.library.path=libraries/native -Djna.library.path=libraries/native -Dfile.encoding=UTF-8"
 
-"%JAVA%" %JAVA_OPTIONS% org.myrobotlab.service.Runtime --from-launcher --log-level info -s webgui WebGui intro Intro python Python
+"%JAVA%" %JAVA_OPTIONS% -cp %CLASSPATH% org.myrobotlab.service.Runtime --from-launcher --log-level info -s webgui WebGui intro Intro python Python
 
