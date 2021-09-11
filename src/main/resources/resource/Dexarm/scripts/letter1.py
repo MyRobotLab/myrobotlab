ecc0da3d1b5aaf38f9a936f07249f441b189efe0
Langevin Gael <hairygael@gmail.com>
2021-09-11 23:37:15 +0200

    Add files via upload

--- src/main/resources/resource/Dexarm/scripts/letter1.py
+++ src/main/resources/resource/Dexarm/scripts/letter1.py
@@ -5,10 +5,17 @@ import time
 import os, sys
 
 '''windows'''
-dexarm = Dexarm(port="COM3")
-
+dexarm1 = Dexarm1(port="COM3")
+dexarm2 = Dexarm2(port="COM4")
 '''mac & linux'''
 # device = Dexarm(port="/dev/tty.usbmodem3086337A34381")
-
-dexarm.go_home()
-serial.writeFile('resource/Dexarm/gcode/letter1.gcode')
+sleep(2)
+dexarm1.go_home1()
+dexarm2.go_home2()
+#sleep(2)
+#dexarm.set_module_type(0)
+#dexarm.writeFile('resource/Dexarm/gcode/letter1.gcode')
+#sleep(2)
+#dexarm.go_home()
+#sleep(1)
+#dexarm.go_home()
