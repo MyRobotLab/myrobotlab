206d78a940132dd4140fa985ff795a5e677c750d
Langevin Gael <hairygael@gmail.com>
2021-09-11 23:35:28 +0200

    modified

--- src/main/resources/resource/Dexarm/scripts/launchdexarm.py
+++ src/main/resources/resource/Dexarm/scripts/launchdexarm.py
@@ -1,11 +1,15 @@
 ## launching dexarm
 
 from java.lang import String
+import time
 import os, sys
 
 log = Runtime.start("log","Log")
 RuningFolder="resource"
 RuningFolder=os.getcwd().replace("\\", "/")+"/"+RuningFolder+"/"
-execfile(RuningFolder+'/Dexarm/scripts/pydexarm.py')
-#execfile(RuningFolder+'/Dexarm/scripts/example.py')
+execfile(RuningFolder+'/Dexarm/scripts/pydexarm1.py')
+execfile(RuningFolder+'/Dexarm/scripts/pydexarm2.py')
+execfile(RuningFolder+'/Dexarm/scripts/swingGui.py')
+execfile(RuningFolder+'/Dexarm/scripts/joystick.py')
+#execfile(RuningFolder+'/Dexarm/scripts/example2.py')
 execfile(RuningFolder+'/Dexarm/scripts/letter1.py')
