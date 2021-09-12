6ffef237ac63d1fb5fcb273393b1877c099bbb9c
Langevin Gael <hairygael@gmail.com>
2021-09-13 00:33:13 +0200

    modified the example

--- src/main/resources/resource/Dexarm/scripts/example.py
+++ src/main/resources/resource/Dexarm/scripts/example.py
@@ -5,41 +5,54 @@ from java.lang import String
 import time
 import os, sys
 
-'''windows'''
-#log = Runtime.start("log","Log")
-#RuningFolder="scripts"
-#RuningFolder=os.getcwd().replace("\\", "/")+"/"+RuningFolder+"/"
-#execfile(RuningFolder+'/pydexarm.py')
-#dexarm = Dexarm(port="COM3")
+# We move dexarm1 away
+dexarm1.move_to1(140, 180, 50)
 
-'''mac & linux'''
-# device = Dexarm(port="/dev/tty.usbmodem3086337A34381")
-
+# We start dexarm2 to set the paper
 dexarm2.set_module_type2(2)
-#dexarm2.go_home2()
-
-dexarm2.move_to2(50, 300, 0)
-dexarm2.move_to2(50, 300, -50)
+dexarm2.set_module_type2(6)
+dexarm2.move_to2(170, 330, 10)
+dexarm2.move_to2(170, 330, 0)
 dexarm2.air_picker_pick2()
-dexarm2.move_to2(50, 300, 0)
-sleep(1)
-dexarm2.move_to2(-50, 300, 0)
-dexarm2.move_to2(-50, 300, -50)
+dexarm2.rotate_wrist2()
+dexarm2.move_to2(170, 330, 10)
+dexarm2.move_to2(0, 330, 10)
+dexarm2.move_to2(0, 330, 0)
 #dexarm2.air_picker_place2()
 dexarm2.air_picker_nature2()
-dexarm2.air_picker_stop2()
-dexarm2.move_to2(-50, 300, 0)
-
-dexarm2.go_home2()
-
-## we start dexarm1
-dexarm1.set_module_type1(0)
-dexarm1.move_to1(50, 300, -30)
-#dexarm1.move_to1(G0, X-80.33 Y337.72)
-#dexarm1.read_Gcode1()
-dexarm1.read_test1()
 sleep(1)
-dexarm1.go_home1()
+dexarm2.air_picker_stop2()
+dexarm2.rotate_wrist2()
+dexarm2.move_to2(0, 330, 10)
+dexarm2.move_to2(140, 180, 50)
+
+
+
+# We start dexarm1 to write on paper
+dexarm1.set_module_type1(0)
+#dexarm1.move_to1(50, 300, -30)
+#dexarm1.send_gcode1()
+dexarm1.read_test1()
+
+# We move dexarm1 away
+dexarm1.move_to1(140, 180, 50)
+
+# We start dexarm2 to remove the written paper
+dexarm2.set_module_type2(2)
+dexarm2.set_module_type2(6)
+dexarm2.move_to2(0, 330, 10)
+dexarm2.move_to2(0, 330, 0)
+dexarm2.air_picker_pick2()
+dexarm2.rotate_wrist2()
+dexarm2.move_to2(0, 330, 10)
+dexarm2.move_to2(-170, 330, 10)
+dexarm2.move_to2(-170, 330, 0)
+#dexarm2.air_picker_place2()
+dexarm2.air_picker_nature2()
+sleep(1)
+dexarm2.air_picker_stop2()
+dexarm2.rotate_wrist2()
+dexarm2.move_to2(-170, 330, 10)
 
 
 
