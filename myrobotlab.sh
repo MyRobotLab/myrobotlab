def706f5e7f3cee5ec389ac19a3a06f33637efb5
Greg Perry <greg.perry@torc.ai>
2021-09-15 20:28:14 -0700

    things to allow proper startup from shell scripts

--- myrobotlab.sh
+++ myrobotlab.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+REPO_FILE=libraries/repo.json
+
 # fancy way to get real cwd ?
 APPDIR="$(dirname -- "$(readlink -f -- "${0}")" )"
 
@@ -22,5 +24,13 @@ fi
 
 # Processing/Arduino handle this in an array - no need for now
 JAVA_OPTIONS="-Djava.library.path=libraries/native -Djna.library.path=libraries/native -Dfile.encoding=UTF-8"
+
+
+if test -f "$REPO_FILE"; then
+    echo "$REPO_FILE exists."
+else 
+    echo "$REPO_FILE does not exist."
+    "${JAVA}" ${JAVA_OPTIONS} org.myrobotlab.service.Runtime --from-launcher --install
+fi
 
 "${JAVA}" ${JAVA_OPTIONS} org.myrobotlab.service.Runtime --from-launcher --log-level info -s webgui WebGui intro Intro python Python
