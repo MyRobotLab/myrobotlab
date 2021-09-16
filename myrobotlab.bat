6e58c4e4015792d9ea6132983dbc8a6eadb389ca
Greg Perry <greg.perry@torc.ai>
2021-09-15 20:53:04 -0700

    stoopid windows scripting

--- myrobotlab.bat
+++ myrobotlab.bat
@@ -1,7 +1,5 @@
 @ECHO OFF
 
-REPO_FILE=libraries/repo.json
-
 rem TODO move vars to top
 rem TODO have --id as an optional var
 
@@ -18,11 +16,11 @@ set JAVA=java
 rem Processing/Arduino handle this in an array - no need for now
 set JAVA_OPTIONS="-Djava.library.path=libraries/native -Djna.library.path=libraries/native -Dfile.encoding=UTF-8"
 
-IF EXIST %REPO_FILE% (
-    echo "$REPO_FILE exists."
+IF EXIST "libraries/repo.json" (
+    echo "libraries/repo.json exists."
 ) ELSE (
-    echo "$REPO_FILE does not exist."
-    "${JAVA}" ${JAVA_OPTIONS} org.myrobotlab.service.Runtime --from-launcher --install
+    echo "libraries/repo.json does not exist."
+    "%JAVA%" %JAVA_OPTIONS% -cp %CLASSPATH% org.myrobotlab.service.Runtime --from-launcher --install
 )
 
 "%JAVA%" %JAVA_OPTIONS% -cp %CLASSPATH% org.myrobotlab.service.Runtime --from-launcher --log-level info -s webgui WebGui intro Intro python Python
