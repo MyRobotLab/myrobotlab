74d9762a9f6cb6f82d58c438cffdf264901bed02
GroG <grog@myrobotlab.org>
2021-10-07 07:55:28 -0700

    Update Hd44780.py

--- src/main/resources/resource/Hd44780/Hd44780.py
+++ src/main/resources/resource/Hd44780/Hd44780.py
@@ -7,7 +7,7 @@
 #################################################################
 # If your using the Arduino Nano, comment out this line and
 # uncomment the ArDuino Nano lines
-raspi = Runtime.createAndStart("raspi","RasPi")
+raspi = Runtime.start("raspi","RasPi")
 #################################################################
 # Start the Arduino Nano connected using /dev/ttyUSB0           #
 #################################################################
@@ -18,11 +18,14 @@ raspi = Runtime.createAndStart("raspi","RasPi")
 #################################################################
 # Next start the PCF8574 service                                #
 #################################################################
-pcf = Runtime.createAndStart("Pcf","Pcf8574")
+pcf = Runtime.start("Pcf","Pcf8574")
 # Then attach it to the I2C Bus Master
 # When attaching, we specify the Bus Master Device,
 # the I2C Bus Number
 # and the I2C address
+# FIXME - avoid these attaches if possible - the pcf should be configured
+# first with bus and address - then just pcf.attach(raspi)
+# important for simple service lifecycles ... configuration first, attach next
 pcf.attach(raspi,"1","0x27")
  
 #pcf.attach(arduinoNano,"0","0x27")
@@ -30,21 +33,21 @@ pcf.attach(raspi,"1","0x27")
 #################################################################
 # Next start the Hd44780 service                                #
 #################################################################
-Hd44780 = Runtime.createAndStart("Hd44780","Hd44780")
+hd44780 = Runtime.start("hd44780","Hd44780")
  
 # Once the service has been started, we need to attach it to
 # the PCF service
-Hd44780.attach(pcf)
+hd44780.attach(pcf)
  
 # this will initalise the display.
-Hd44780.init()
+hd44780.init()
  
 # when we want to clear the screen call this
-Hd44780.clear()
+hd44780.clear()
  
 # You can turn the backlight on or off.
 # True will turn it on, False will turn it off.
-Hd44780.stBackLight(True)
+hd44780.stBackLight(True)
  
 # Filally to send text to the display
-Hd44780.display("Hello World", 0)
+hd44780.display("Hello World", 0)
