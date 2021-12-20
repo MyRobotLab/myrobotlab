0c7cf471ba9d41a7c760917b5eb35da05640412c
kwatters <kwatters@kmwllc.com>
2021-12-20 14:01:48 -0500

    upgrading to javacpp 1.5.6

--- src/main/java/org/myrobotlab/service/meta/TesseractOcrMeta.java
+++ src/main/java/org/myrobotlab/service/meta/TesseractOcrMeta.java
@@ -23,8 +23,8 @@ public class TesseractOcrMeta extends MetaData {
     Platform platform = Platform.getLocalInstance();
     addDescription("Optical character recognition - the ability to read");
     addCategory("ai", "vision");
-    addDependency("org.bytedeco", "tesseract", "4.1.1-1.5.5");
-    addDependency("org.bytedeco", "tesseract-platform", "4.1.1-1.5.5");
+    addDependency("org.bytedeco", "tesseract", "4.1.1-1.5.6");
+    addDependency("org.bytedeco", "tesseract-platform", "4.1.1-1.5.6");
     addDependency("tesseract", "tessdata", "0.0.2", "zip");
 
   }
