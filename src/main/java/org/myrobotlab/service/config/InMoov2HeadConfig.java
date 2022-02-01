7d59da36818976bc613f520100e44d430e08cbdc
Langevin Gael <hairygael@gmail.com>
2022-02-01 11:44:05 +0100

    add InMoov2HeadConfig

---
+++ src/main/java/org/myrobotlab/service/config/InMoov2HeadConfig.java
@@ -0,0 +1,9 @@
+package org.myrobotlab.service.config;
+
+public class InMoov2TorsoConfig extends ServiceConfig {
+
+  public String topStom;
+  public String midStom;
+  public String lowStom;
+
+}
