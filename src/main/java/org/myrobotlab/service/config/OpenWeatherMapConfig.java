f4081aaffba263210f1ad25ea5285eea5fdd1b38
Langevin Gael <hairygael@gmail.com>
2022-02-18 22:24:25 +0100

    Create OpenWeatherMapConfig.java

---
+++ src/main/java/org/myrobotlab/service/config/OpenWeatherMapConfig.java
@@ -0,0 +1,11 @@
+package org.myrobotlab.service.config;
+
+import java.util.List;
+import java.util.Map;
+
+public class OpenWeatherMapConfig extends ServiceConfig {
+
+  public String currentUnits;
+  public String currentTown;
+
+}
