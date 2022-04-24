cf0bf17210f2cad0363e3de3c003d594ad1d08b9
Greg Perry <greg.perry@torc.ai>
2022-04-23 19:43:31 -0700

    partial programab update

--- src/main/java/org/myrobotlab/service/config/ProgramABConfig.java
+++ src/main/java/org/myrobotlab/service/config/ProgramABConfig.java
@@ -6,6 +6,7 @@ public class ProgramABConfig extends ServiceConfig {
   public String currentUserName;
   public String[] textListeners;
   public String[] utteranceListeners;
+  public String botDir;
   
   // search engine - currently google search is default peer
   public String search;
