2ca82395c1c2335ae42d9966ccc6900fce159994
Branden Butler <bwtbutler@hotmail.com>
2022-07-30 18:56:06 -0500

    Optimize imports in CodecUtils

--- src/main/java/org/myrobotlab/codec/CodecUtils.java
+++ src/main/java/org/myrobotlab/codec/CodecUtils.java
@@ -2,7 +2,6 @@ package org.myrobotlab.codec;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
