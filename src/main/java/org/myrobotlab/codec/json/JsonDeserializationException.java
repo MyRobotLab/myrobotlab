770b6d9bf9e862c40bba94681a0d80d59f6b84a6
Branden Butler <bwtbutler@hotmail.com>
2022-07-30 18:47:36 -0500

    Add documentation to json exceptions

--- src/main/java/org/myrobotlab/codec/json/JsonDeserializationException.java
+++ src/main/java/org/myrobotlab/codec/json/JsonDeserializationException.java
@@ -1,5 +1,11 @@
 package org.myrobotlab.codec.json;
 
+/**
+ * Represents a failed Json deserialization operation.
+ * This exception is always caused by other exceptions.
+ *
+ * @author AutonomicPerfectionist
+ */
 public class JsonDeserializationException extends RuntimeException{
     public JsonDeserializationException(Throwable cause) {
         super(cause);
--- src/main/java/org/myrobotlab/codec/json/JsonSerializationException.java
+++ src/main/java/org/myrobotlab/codec/json/JsonSerializationException.java
@@ -1,5 +1,11 @@
 package org.myrobotlab.codec.json;
 
+/**
+ * Represents a failed JSON serialization operation.
+ * This exception is always caused by other exceptions.
+ *
+ * @author AutonomicPerfectionist
+ */
 public class JsonSerializationException extends RuntimeException{
     public JsonSerializationException(Throwable cause) {
         super(cause);
