b3ceef8c8a24324c5c592d5097d03e9ccdcc0480
Cyber-One <ray.edgley@gmail.com>
2022-10-30 09:15:02 +1100

    Update to JavaDocs and update a m,ethod form using a depreciated call to the newer version

--- src/main/java/org/myrobotlab/service/interfaces/RangingControl.java
+++ src/main/java/org/myrobotlab/service/interfaces/RangingControl.java
@@ -4,12 +4,28 @@ import org.myrobotlab.framework.interfaces.NameProvider;
 
 public interface RangingControl extends NameProvider {
 
+  /**
+   * This method starts the ranging process. 
+   * Ranging can be done as a single ping or as a continuous stream of pings.
+   */
   public void startRanging();
 
+  /**
+   * This method stops the ranging process. 
+   * Ranging can be done as a single ping or as a continuous stream of pings.
+   */
   public void stopRanging();
 
+  /**
+   * The measured distance can be either Metric (centimeters) or imperial (inches).
+   * This method sets the returned ranging value to Metric centimeters.
+   */
   public void setUnitCm();
 
+  /**
+   * The measured distance can be either Metric (centimeters) or imperial (inches).
+   * This method sets the returned ranging value to Imperial Inches.
+   */
   public void setUnitInches();
 
 }
