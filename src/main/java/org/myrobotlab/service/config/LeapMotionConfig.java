217beceea4fc5d16141df4647ee022c2862483e9
GroG <grog@myrobotlab.org>
2023-01-01 10:31:30 -0800

    make leap scale via config

---
+++ src/main/java/org/myrobotlab/service/config/LeapMotionConfig.java
@@ -0,0 +1,17 @@
+package org.myrobotlab.service.config;
+
+public class LeapMotionConfig extends ServiceConfig {
+
+  public float leftThumb = 1.0f;
+  public float leftIndex = 1.0f;
+  public float leftMiddle = 1.0f;
+  public float leftRing = 1.0f;
+  public float leftPinky = 1.0f;
+
+  public float rightThumb = 1.0f;
+  public float rightIndex = 1.0f;
+  public float rightMiddle = 1.0f;
+  public float rightRing = 1.0f;
+  public float rightPinky = 1.0f;
+
+}
