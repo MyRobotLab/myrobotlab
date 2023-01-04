20bb8ef223b9e9b32d7bdc732c45de75427e9a84
GroG <grog@myrobotlab.org>
2023-01-03 17:02:51 -0800

    merged

--- src/main/java/org/myrobotlab/service/Runtime.java
+++ src/main/java/org/myrobotlab/service/Runtime.java
@@ -1907,7 +1907,6 @@ public class Runtime extends Service implements MessageListener, ServiceLifeCycl
     }
 
     // FIXME - release autostarted peers ?
-
     // last step - remove from registry
     registry.remove(name);
 
