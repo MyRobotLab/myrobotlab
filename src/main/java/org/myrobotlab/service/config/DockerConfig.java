aaa866a9f43f3311b7fa97f4b582e988e0f6f798
GroG <grog@myrobotlab.org>
2023-02-15 19:03:26 -0800

    cleared out config for docker

--- src/main/java/org/myrobotlab/service/config/DockerConfig.java
+++ src/main/java/org/myrobotlab/service/config/DockerConfig.java
@@ -2,26 +2,6 @@ package org.myrobotlab.service.config;
 
 public class DockerConfig extends ServiceConfig {
 
-  public boolean autoDisable = false;
-  // public String controller;
-  public boolean enabled = true;
-  public Integer idleTimeout;
-
-  public String pin;
-  public Double rest = 90.0;
-  public Double speed;
-
-  // mapper values
-  public boolean inverted = false;
-  public boolean clip = true;
-  public Double minIn = 0.0;
-  public Double maxIn = 180.0;
-  public Double minOut = 0.0;
-  public Double maxOut = 180.0;
-
-  public Double sweepMax;
-  public Double sweepMin;
-  
-  public String controller;
+  // FIXME - fill out stuff here
 
 }
