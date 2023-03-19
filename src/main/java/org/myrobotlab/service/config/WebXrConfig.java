57cce0684123b4e10135cb30f2770b75ff0bae39
supertick <grog@myrobotlab.org>
2023-03-18 20:37:11 -0700

    reverting vertx and webxr

--- src/main/java/org/myrobotlab/service/config/WebXrConfig.java
+++
@@ -1,9 +0,0 @@
-package org.myrobotlab.service.config;
-
-public class WebXrConfig extends ServiceConfig {
-
-  public Integer port = 8888;
-  public boolean autoStartBrowser = true;
-  public boolean enableMdns = false;
-
-}
