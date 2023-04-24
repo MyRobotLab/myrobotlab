4f3b1744b1b08ab9116957ad0ccdc942c26e181a
Cyber-One <ray.edgley@gmail.com>
2023-04-25 01:46:45 +1000

    Change default Bus and Address

--- src/main/java/org/myrobotlab/service/config/Pcf8574Config.java
+++ src/main/java/org/myrobotlab/service/config/Pcf8574Config.java
@@ -2,8 +2,8 @@ package org.myrobotlab.service.config;
 
 public class Pcf8574Config extends ServiceConfig {
 
-  public String address = "0x38";
-  public String bus = "1";
+  public String address = "0x20";
+  public String bus = "0";
   public String controller;
 
 }
