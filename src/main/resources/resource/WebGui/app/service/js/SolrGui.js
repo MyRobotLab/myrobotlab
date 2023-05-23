8d24cc87061830204f4c83a5ccbd668576bbf134
kwatters <kwatters@kmwllc.com>
2023-05-23 13:33:22 -0400

    beginning refactor of solr web gui.

--- src/main/resources/resource/WebGui/app/service/js/SolrGui.js
+++ src/main/resources/resource/WebGui/app/service/js/SolrGui.js
@@ -1,25 +1,48 @@
-angular.module('mrlapp.service.SolrGui', [])
-  .controller('SolrGuiCtrl', ['$scope', '$log', 'mrl', function ($scope, $log, mrl) {
-                $log.info('SolrGuiCtrl');
-                // TODO: something useful?!
-                this.onMsg = function (msg) {
-                    $log.info("Solr Msg ! - ");  
-                    $log.info(msg);
-                    if (msg.method == "onResults") {
-                    	// Results!
-                    	var solrResults = msg.data[0];
-                    	$scope.service.solrResults = solrResults;
-                    	$scope.$apply();
-                    }
-                };
-                $scope.search = function(querystring) {
-                    $log.info('SolrGuiCtrl - Search Clicked!' + querystring);
-                	mrl.sendTo($scope.service.name, "search", querystring);
-                };
-                
+angular.module('mrlapp.service.SolrGui', []).controller('SolrGuiCtrl', ['$scope', 'mrl', function($scope, mrl) {
+    console.info('SolrGuiCtrl');
+    var _self = this
+    var msg = this.msg
+    // TODO: something useful?!
+    $scope.solrResults = '';
+    $scope.queryString = '*:*';
+    	
+    // GOOD TEMPLATE TO FOLLOW
+    this.updateState = function(service) {
+      $scope.service = service
+    }
 
-                
-                mrl.subscribe($scope.service.name, 'publishResults', $scope.service.results);
-//                $scope.panel.initDone();
+    this.onMsg = function(inMsg) {
+      let data = inMsg.data[0]
+      switch (inMsg.method) {
+        case 'onResults': 
+    	  var solrResults = data;
+    	  console.info("On Results!");
+          $scope.solrResults = solrResults;
+          $scope.$apply();
+          break
+        case 'onState':
+          _self.updateState(data)
+          $scope.$apply()
+          break	
+        case 'onStatus':
+          $scope.status = data;
+          $scope.$apply()
+          break
+        default:
+    	  console.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method)
+    	  break
+      }
+    
+    };
+    
+    $scope.search = function(querystring) {
+      console.info('SolrGuiCtrl - Search Clicked!' + querystring);
+      mrl.sendTo($scope.service.name, "search", querystring);
+    };
+    
+    msg.subscribe('publishResults');
+    msg.subscribe(this);
+    // mrl.subscribe($scope.service.name, 'publishResults', $scope.service.results);
+    // $scope.panel.initDone();
                 
   }]);
