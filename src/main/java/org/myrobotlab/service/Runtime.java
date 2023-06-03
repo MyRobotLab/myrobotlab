f8e8677cb263916d288fd78ba767fa6caae492c8
grog <grog@myrobotlab.org>
2023-06-03 10:10:29 -0700

    filter out runtime

--- src/main/java/org/myrobotlab/service/Runtime.java
+++ src/main/java/org/myrobotlab/service/Runtime.java
@@ -2111,7 +2111,9 @@ public class Runtime extends Service implements MessageListener, ServiceLifeCycl
     // probably be just remove their references - do not
     // ask for them to be released remotely ..
     for(String remoteService: registry.keySet()) {
-      registry.remove(remoteService);
+      if (!remoteService.equals(runtime.getFullName())) {
+        registry.remove(remoteService);
+      }
     }
 
     if (runtime != null && releaseRuntime) {
