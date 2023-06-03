7edd1f36b317c34c928254a2206d6653376a043b
GroG <grog@myrobotlab.org>
2023-06-02 18:01:08 -0700

    Py4j Worky !!! (#1292)
    * Py4j Worky !!!
    
    * requested updates
    
    * updated override

--- src/main/java/org/myrobotlab/io/StreamGobbler.java
+++ src/main/java/org/myrobotlab/io/StreamGobbler.java
@@ -6,11 +6,38 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
+import org.myrobotlab.logging.LoggerFactory;
+import org.slf4j.Logger;
+/**
+ * A general stream gobbler, useful when starting processes and handling their streams
+ * @author GroG
+ *
+ */
 public class StreamGobbler extends Thread {
   protected transient InputStream processOut;
   protected transient OutputStream processIn;
   protected String name;
+  public final static Logger log = LoggerFactory.getLogger(StreamGobbler.class);
 
+  /**
+   * When we do not need to redirect the stream to another stream. E.g.
+   * when we create a process, and are already redirecting std:out and std:err
+   * 
+   * @param name
+   * @param processOut
+   */
+  public StreamGobbler(String name, InputStream processOut) {
+    this(name, processOut, null);
+  }
+
+  /**
+   * This is a general stream gobbler that will consume and input stream and move 
+   * the data to an output stream.
+   * 
+   * @param name
+   * @param processOut
+   * @param processIn
+   */
   public StreamGobbler(String name, InputStream processOut, OutputStream processIn) {
     super(name);
     this.processOut = processOut;
@@ -24,14 +51,14 @@ public class StreamGobbler extends Thread {
       InputStreamReader isr = new InputStreamReader(processOut);
       BufferedReader br = new BufferedReader(isr);
       String line = null;
-      while ((line = br.readLine()) != null)
-        if (line != null) {
+      while ((line = br.readLine()) != null) {
+        log.info("gobbler - {}", line);
+        if (line != null && processIn != null) {
           processIn.write(String.format("%s\n", line).getBytes());
           processIn.flush();
-        }
+        }}
     } catch (IOException ioe) {
-      System.out.println("gobbler leaving");
-      ioe.printStackTrace();
+      log.info("{} gobbler leaving", name);
     }
   }
 }
---
+++ src/main/java/org/myrobotlab/service/interfaces/Executor.java
@@ -0,0 +1,32 @@
+package org.myrobotlab.service.interfaces;
+
+import org.myrobotlab.framework.interfaces.Invoker;
+
+/**
+ * Interface to a Executor - currently only utilized by Py4j to
+ * represent an interface for python from java-land, implemented
+ * by MessageHandler in /resource/Py4j/Py4j.py
+ * 
+ * @author GroG
+ *
+ */
+public interface Executor extends Invoker {
+
+  /**
+   * exec in Python - executes arbitrary code
+   * @param code
+   * @return
+   */
+  public Object exec(String code);
+  
+  /**
+   * To use the service framework appropriately, you only need
+   * a reference to runtime and the service name.  This method
+   * is required in Java-land to set the MessageHandler's name in
+   * Python-land
+   * @param name
+   * @return
+   */
+  public String setName(String name);
+
+}
