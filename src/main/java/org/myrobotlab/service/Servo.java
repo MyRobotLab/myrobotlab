5cd089571635aac6c3a3744a86c8e88c4a56a389
grog <grog@myrobotlab.org>
2023-06-14 10:49:11 -0700

    servo update

--- src/main/java/org/myrobotlab/service/Servo.java
+++ src/main/java/org/myrobotlab/service/Servo.java
@@ -25,7 +25,6 @@
 
 package org.myrobotlab.service;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import org.myrobotlab.logging.Level;
@@ -36,7 +35,6 @@ import org.myrobotlab.sensor.EncoderData;
 import org.myrobotlab.sensor.TimeEncoder;
 import org.myrobotlab.service.abstracts.AbstractServo;
 import org.myrobotlab.service.config.ServiceConfig;
-import org.myrobotlab.service.config.ServiceConfig.Listener;
 import org.myrobotlab.service.config.ServoConfig;
 import org.myrobotlab.service.data.ServoMove;
 import org.myrobotlab.service.interfaces.ServiceLifeCycleListener;
@@ -87,6 +85,25 @@ public class Servo extends AbstractServo implements ServiceLifeCycleListener {
    */
   @Override
   protected boolean processMove(Double newPos, boolean blocking, Long timeoutMs) {
+    
+    if (newPos == null) {
+      log.info("servo processMove(null) not valid position");
+      return false;
+    }
+    
+    double minLimit = (mapper.minX < mapper.maxX)?mapper.minX:mapper.maxX;
+    double maxLimit = (mapper.maxX > mapper.minX)?mapper.maxX:mapper.minX;
+    
+    if (newPos < minLimit) {
+      log.warn("requested position {} is less than minimum {}", newPos, minLimit);
+      return false;
+    }
+    
+    if (newPos > maxLimit) {
+      log.warn("requested position {} is more than maximum {}", newPos, maxLimit);
+      return false;
+    }
+
 
     // This is to allow attaching disabled
     // then delay enabling until the first moveTo command
@@ -174,18 +191,17 @@ public class Servo extends AbstractServo implements ServiceLifeCycleListener {
     // movement
     // usually knowing about encoder type is "bad" but the timer encoder is the
     // default native encoder
-    Long blockingTimeMs = null;
+    long blockingTimeMs = 0;
     if (encoder != null && encoder instanceof TimeEncoder) {
       TimeEncoder timeEncoder = (TimeEncoder) encoder;
       // calculate trajectory calculates and processes this move
-      // blockingTimeMs = timeEncoder.calculateTrajectory(getCurrentOutputPos(),
-      // getTargetOutput(), getSpeed());
       blockingTimeMs = timeEncoder.calculateTrajectory(getCurrentInputPos(), getTargetPos(), getSpeed());
     }
 
     if (isBlocking) {
       // our thread did a blocking call - we will wait until encoder notifies us
-      // to continue or timeout (if supplied) has been reached
+      // to continue or timeout (if supplied) has been reached - "cheesy" need to
+      // re-work for real monitor callbacks from real encoders
       sleep(blockingTimeMs);
       isBlocking = false;
       isMoving = false;
