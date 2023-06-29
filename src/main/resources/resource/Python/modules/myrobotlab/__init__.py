3e3e8435759bb8d0c1a6e47889f4652d9d76dbac
grog <grog@myrobotlab.org>
2023-06-29 07:21:41 -0700

    removed not ready  python module

--- src/main/resources/resource/Python/modules/myrobotlab/__init__.py
+++
@@ -1,8 +0,0 @@
-# inmoov2/__init__.py
-# from . import myrobotlab
-
-"""MyRobotLab - a Python library for robotics."""
-
-from .myrobotlab import connect
-__version__ = '0.9'
-__doc__ = """Open Source Framework for Robotics and Creative Machine Control"""
