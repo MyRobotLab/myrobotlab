06783cb70fc1747d4540e1e71fe94a2b14774842
grog <grog@myrobotlab.org>
2023-07-16 19:42:34 -0700

    raspi updates

--- src/main/java/org/myrobotlab/service/config/RasPiConfig.java
+++ src/main/java/org/myrobotlab/service/config/RasPiConfig.java
@@ -1,5 +1,10 @@
 package org.myrobotlab.service.config;
 
 public class RasPiConfig extends ServiceConfig {
-
+    /**
+     * reading poll rate for all enabled GPIO pins
+     * this "should" not be an int but a float, but at this time
+     * its better to follow the PinDefinition pollRateHz type
+     */
+    public int pollRateHz = 1;
 }
