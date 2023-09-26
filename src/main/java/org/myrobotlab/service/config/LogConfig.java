b38784c3bde402ac6ff15a6eb81a1a56799619f1
grog <grog@myrobotlab.org>
2023-09-26 10:58:16 -0700

    LogConfig removed copypasta

--- src/main/java/org/myrobotlab/service/config/LogConfig.java
+++ src/main/java/org/myrobotlab/service/config/LogConfig.java
@@ -2,8 +2,6 @@ package org.myrobotlab.service.config;
 
 public class LogConfig extends ServiceConfig {
 
-  public String currentUserName;
-
   /**
    * level of log error, warn, info, debug
    */
