7fdb56137cb04c3ba8cc377f48ea5d130fac8689
Langevin Gael <hairygael@gmail.com>
2023-10-13 11:12:58 +0200

    chatBot set botState

--- src/main/java/org/myrobotlab/service/InMoov2.java
+++ src/main/java/org/myrobotlab/service/InMoov2.java
@@ -925,7 +925,7 @@ public class InMoov2 extends Service<InMoov2Config> implements ServiceLifeCycleL
     led.interval = 500;
     // FIXME flash on config.flashOnBoot
     invoke("publishFlash");
-    String botState = fsm.getCurrent();
+    String botState = chatBot.getPredicate("botState");
     if ("sleeping".equals(botState)) {
       invoke("publishEvent", "WAKE");
     }
