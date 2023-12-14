842fcf48d91131c6d9b538fcbea377c0539d6a13
grog <grog@myrobotlab.org>
2023-12-13 23:09:10 -0800

    required to not interfere with jenkins

--- src/main/java/org/myrobotlab/service/config/MqttBrokerConfig.java
+++ src/main/java/org/myrobotlab/service/config/MqttBrokerConfig.java
@@ -6,6 +6,6 @@ public class MqttBrokerConfig extends ServiceConfig {
   public String password;
   public String address = "0.0.0.0";
   public Integer mqttPort = 1883;
-  public Integer wsPort = 8080;
+  public Integer wsPort = 8081;
 
 }
