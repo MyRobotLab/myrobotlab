4ca4fee98ab99909b11f64523647c3bc6c04d1ad
grog <grog@myrobotlab.org>
2023-12-22 10:26:05 -0500

    statechange fixes

--- src/main/java/org/myrobotlab/service/Emoji.java
+++ src/main/java/org/myrobotlab/service/Emoji.java
@@ -17,15 +17,16 @@ import org.myrobotlab.service.FiniteStateMachine.StateChange;
 import org.myrobotlab.service.config.EmojiConfig;
 import org.myrobotlab.service.data.ImageData;
 import org.myrobotlab.service.interfaces.ImagePublisher;
-import org.myrobotlab.service.interfaces.StateChangeHandler;
+import org.myrobotlab.service.interfaces.StateChangeListener;
 import org.myrobotlab.service.interfaces.TextListener;
 import org.myrobotlab.service.interfaces.TextPublisher;
 import org.slf4j.Logger;
 
+
 // emotionListener
 // Links
 // - http://googleemotionalindex.com/
-public class Emoji extends Service<EmojiConfig> implements TextListener, StateChangeHandler, ImagePublisher {
+public class Emoji extends Service<EmojiConfig> implements TextListener, StateChangeListener, ImagePublisher {
 
   private static final long serialVersionUID = 1L;
 
@@ -189,7 +190,7 @@ public class Emoji extends Service<EmojiConfig> implements TextListener, StateCh
 
   // FIXME - publish events if desired...
   @Override
-  public void handleStateChange(StateChange event) {
+  public void onStateChange(StateChange event) {
     log.info("handleEvent {}", event);
     EmojiData emoji = new EmojiData();
     emoji.name = event.current;
