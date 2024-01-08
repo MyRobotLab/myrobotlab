1119890bcb71549a03df4da6d14bcee87e73912c
grog <grog@myrobotlab.org>
2024-01-08 13:24:46 -0800

    small updates

--- src/main/java/org/myrobotlab/service/config/VertxConfig.java
+++ src/main/java/org/myrobotlab/service/config/VertxConfig.java
@@ -5,15 +5,47 @@ import java.util.List;
 
 public class VertxConfig extends ServiceConfig {
 
+  /**
+   * <pre>
+      # legacy
+      - resource/WebGui/app
+      - resource/InMoov2/peers/WebGui/app
+      - resource
+      
+      # react 
+      - src/main/resources/resource/Vertx/build
+  
+      # debug legacy
+      - src/main/resources/resource/WebGui/app
+      - src/main/resources/resource/InMoov2/peers/WebGui/app
+      - src/main/resources/resource
+  
+      # debug react port 5000 proxy
+   * </pre>
+   */
+
   public Integer port = 8443;
-  public Integer workerCount = 1;
   public boolean ssl = true;
   public boolean autoStartBrowser = true;
 
   public List<String> root = new ArrayList<>();
 
   public VertxConfig() {
-    root.add("./resource/Vertx/build");
+    // legacy
+    root.add("resource/WebGui/app");
+    root.add("resource/InMoov2/peers/WebGui/app");
+    root.add("resource");
+    /**
+    <pre>
+    // debug legacy
+    root.add("src/main/resources/resource/WebGui/app");
+    root.add("src/main/resources/resource/InMoov2/peers/WebGui/app");
+    root.add("src/main/resources/resource");
+
+    // debug react app
+    root.add("resource/Vertx/build");
+    </pre>
+    */
   }
 
 }
