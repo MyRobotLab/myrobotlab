5773e3fcddfa92e452ad6698e0f67f4603c4aa19
grog <grog@myrobotlab.org>
2024-01-15 13:22:58 -0800

    adding default bots to inmoov2 config

--- src/main/java/org/myrobotlab/service/config/InMoov2Config.java
+++ src/main/java/org/myrobotlab/service/config/InMoov2Config.java
@@ -144,6 +144,24 @@ public class InMoov2Config extends ServiceConfig {
     mouthControl.mouth = i01Name + ".mouth";
 
     ProgramABConfig chatBot = (ProgramABConfig) plan.get(getPeerName("chatBot"));
+    chatBot.botDir = "resource/ProgramAB";
+        
+    chatBot.bots.add("resource/ProgramAB/Alice");
+    chatBot.bots.add("resource/ProgramAB/Dr.Who");
+    chatBot.bots.add("resource/ProgramAB/Ency");
+    chatBot.bots.add("resource/ProgramAB/Mr. Turing");
+    chatBot.bots.add("resource/ProgramAB/de-DE");
+    chatBot.bots.add("resource/ProgramAB/en-US");
+    chatBot.bots.add("resource/ProgramAB/es-ES");
+    chatBot.bots.add("resource/ProgramAB/fi-FI");
+    chatBot.bots.add("resource/ProgramAB/fr-FR");
+    chatBot.bots.add("resource/ProgramAB/hi-IN");
+    chatBot.bots.add("resource/ProgramAB/it-IT");
+    chatBot.bots.add("resource/ProgramAB/nl-NL");
+    chatBot.bots.add("resource/ProgramAB/pt-PT");
+    chatBot.bots.add("resource/ProgramAB/ru-RU");
+    chatBot.bots.add("resource/ProgramAB/tr-TR");
+
     Runtime runtime = Runtime.getInstance();
     String[] bots = new String[] { "cn-ZH", "en-US", "fi-FI", "hi-IN", "nl-NL", "ru-RU", "de-DE", "es-ES", "fr-FR", "it-IT", "pt-PT", "tr-TR" };
     String tag = runtime.getLocaleTag();
