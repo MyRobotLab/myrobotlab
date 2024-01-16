3909a06b03a87c524117a627e9fa42e07ad3fb1e
grog <grog@myrobotlab.org>
2024-01-16 08:03:52 -0800

    oakd and mockgateway updates

--- src/main/java/org/myrobotlab/service/OakD.java
+++ src/main/java/org/myrobotlab/service/OakD.java
@@ -1,9 +1,13 @@
 package org.myrobotlab.service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
+import org.myrobotlab.framework.Message;
 import org.myrobotlab.framework.Service;
 import org.myrobotlab.framework.Status;
 import org.myrobotlab.logging.Level;
@@ -74,8 +78,63 @@ public class OakD extends Service<OakDConfig> {
     // py4j.exec("");
   }
   
+  public void startRecognition() {
+    
+  }
+  
+  public void stopRecognition() {
+    
+  }
+  
+  /**
+   * FIXME - turn into interface
+   * Will publish processing messages to the processor(s) currently 
+   * subscribed.
+   * @param method
+   * @param data
+   */
+  public void processMessage(String method, Object data) {
+    String processor = getPeerName("py4j");
+    Message msg = Message.createMessage(getName(), processor, method, data);
+    invoke("publishProcessMessage", msg);
+  }
+
+  /**
+   * FIXME - turn into interface
+   * Processing publishing point, where everything InMoov2 wants to be processed
+   * is turned into a message and published.
+   * 
+   * @param msg
+   * @return
+   */
+  public Message publishProcessMessage(Message msg) {
+    return msg;
+  }
+
+  
   public Classification publishClassification(Classification classification) {
+    classification.src = getName();
+    // we have a detection or recognition event ... publish the associated image
+    config.displayWeb = true;
+    if (config.displayWeb) {
+      // imageToWeb("classification.png");
+      invoke("imageToWeb", "classification.png");
+    }
+    
     return classification;
+  }
+  
+  public String imageToWeb(String filePath) {
+    byte[] content = null;
+    try (FileInputStream fileInputStream = new FileInputStream(new File(filePath))) {
+        content = new byte[(int) new File(filePath).length()];
+        fileInputStream.read(content);
+        String img = Base64.getEncoder().encodeToString(content);
+        return String.format("data:image/png;base64,%s", img);
+    } catch (IOException e) {
+        error(e);
+    }
+    return null;
   }
 
   public static void main(String[] args) {
