13323366c5ef4e1304545fab57945c1af83951e5
grog <grog@myrobotlab.org>
2024-02-04 07:23:58 -0800

    added test

--- src/test/java/org/myrobotlab/service/RuntimeTest.java
+++ src/test/java/org/myrobotlab/service/RuntimeTest.java
@@ -1,6 +1,7 @@
 package org.myrobotlab.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
@@ -103,6 +104,15 @@ public class RuntimeTest extends AbstractTest {
     assertEquals("fr-FR", l.toString());
 
   }
+  
+  @Test 
+  public void testRuntimeIsAvailable() {
+    Runtime runtime = Runtime.getInstance();
+    assertTrue(Runtime.isAvailable());
+    Runtime.releaseAll(true, true);
+    assertFalse(Runtime.isAvailable());
+  }
+  
 
   @Test
   public void testGetDescribeMessage() {
