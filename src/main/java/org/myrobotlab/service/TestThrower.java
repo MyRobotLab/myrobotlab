ad05d2633baf88aedcfb3487ac1e7486d4d58ce8
GroG <grog@myrobotlab.org>
2024-02-06 06:18:36 -0800

    adding config to all services missing config (#1403)

--- src/main/java/org/myrobotlab/service/TestThrower.java
+++ src/main/java/org/myrobotlab/service/TestThrower.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import org.myrobotlab.framework.Service;
 import org.myrobotlab.framework.interfaces.Attachable;
 import org.myrobotlab.logging.LoggerFactory;
+import org.myrobotlab.service.config.TestThrowerConfig;
 import org.slf4j.Logger;
 
 /**
@@ -37,7 +38,7 @@ import org.slf4j.Logger;
  * TestThrower - used for testing.
  *
  */
-public class TestThrower extends Service {
+public class TestThrower extends Service<TestThrowerConfig> {
 
   private static final long serialVersionUID = 1L;
   public final static Logger log = LoggerFactory.getLogger(TestThrower.class);
---
+++ src/main/java/org/myrobotlab/service/config/HttpClientConfig.java
@@ -0,0 +1,5 @@
+package org.myrobotlab.service.config;
+
+public class HttpClientConfig extends ServiceConfig {
+
+}
