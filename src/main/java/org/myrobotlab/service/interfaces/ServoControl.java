445af3e3a6b9d0c0137ccb59cd0e5a1341037c53
grog <grog@myrobotlab.org>
2024-02-16 10:20:20 -0800

    forgot file

--- src/main/java/org/myrobotlab/service/interfaces/ServoControl.java
+++ src/main/java/org/myrobotlab/service/interfaces/ServoControl.java
@@ -404,7 +404,12 @@ public interface ServoControl extends AbsolutePositionControl, EncoderListener, 
   /**
    * disable speed control and move the servos at full speed.
    */
-  @Deprecated /* implement setSpeed(null) */
+  @Deprecated
   void fullSpeed();
-
+  
+  /**
+   * 
+   */
+  void setMaxSpeed();
+  
 }
