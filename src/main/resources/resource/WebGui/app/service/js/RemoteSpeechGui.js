cb643b7c18a3331034a8bfccf2896316b689e9f4
supertick <grog@myrobotlab.org>
2024-05-19 15:03:12 -0700

    init checkin

--- src/main/resources/resource/WebGui/app/service/js/RemoteSpeechGui.js
+++ src/main/resources/resource/WebGui/app/service/js/RemoteSpeechGui.js
@@ -8,13 +8,17 @@ angular.module("mrlapp.service.RemoteSpeechGui", []).controller("RemoteSpeechGui
 
     this.updateState = function (service) {
       $scope.service = service
-      $scope.$apply()
     }
 
     this.onMsg = function (inMsg) {
+      let data = inMsg.data[0]
       switch (inMsg.method) {
         case "onState":
           _self.updateState(inMsg.data[0])
+          $scope.$apply()
+          break
+        case "onStatus":
+          console.info("state", data)
           break
         default:
           console.error("ERROR - unhandled method " + $scope.name + " " + inMsg.method)
@@ -22,8 +26,8 @@ angular.module("mrlapp.service.RemoteSpeechGui", []).controller("RemoteSpeechGui
       }
     }
 
-    $scope.setType = function (type) {
-      msg.send("set" + type)
+    $scope.setType = function () {
+      msg.send("setSpeechType", $scope.speechType)
     }
 
     $scope.speak = function (text) {
@@ -33,6 +37,22 @@ angular.module("mrlapp.service.RemoteSpeechGui", []).controller("RemoteSpeechGui
     $scope.setVoice = function () {
       console.log($scope.service.voice.name)
       msg.send("setVoice", $scope.service.voice.name)
+    }
+
+    $scope.getUrl = function (service) {
+      return service.config.endpoint?.url
+    }
+
+    $scope.getVerb = function (service) {
+      return service.config.endpoint?.verb
+    }
+
+    $scope.getTemplate = function (service) {
+      return service.config.endpoint?.template
+    }
+
+    $scope.getAuthToken = function (service) {
+      return service.config.endpoint?.authToken
     }
 
     msg.subscribe(this)
