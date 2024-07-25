d4440fd37543cb4802d72846cdb9aa85680f88af
Langevin Gael <hairygael@gmail.com>
2024-07-25 16:00:44 +0200

    Update InMoov2Config.java

--- src/main/java/org/myrobotlab/service/config/InMoov2Config.java
+++ src/main/java/org/myrobotlab/service/config/InMoov2Config.java
@@ -271,10 +271,11 @@ public class InMoov2Config extends ServiceConfig {
     chatBot.bots.add("resource/ProgramAB/pl-PL");
     chatBot.bots.add("resource/ProgramAB/pt-PT");
     chatBot.bots.add("resource/ProgramAB/ru-RU");
+    chatBot.bots.add("resource/ProgramAB/ta-TA");
     chatBot.bots.add("resource/ProgramAB/tr-TR");
 
     Runtime runtime = Runtime.getInstance();
-    String[] bots = new String[] { "cn-ZH", "en-US", "fi-FI", "hi-IN", "nl-NL", "pl-PL","ru-RU", "de-DE", "es-ES", "fr-FR", "it-IT", "pt-PT", "tr-TR" };
+    String[] bots = new String[] { "cn-ZH", "en-US", "fi-FI", "hi-IN", "nl-NL", "pl-PL","ru-RU", "de-DE", "es-ES", "fr-FR", "it-IT", "pt-PT", "ta-TA", "tr-TR" };
     String tag = runtime.getLocaleTag();
     if (tag != null) {
       String[] tagparts = tag.split("-");
